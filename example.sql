0659488809d7596d7db613f1c6ea573ac18ec6eb
shodan <shodan@406a0c4d-033a-0410-8de8-e80135713968>
2006-02-26 20:33:48 +0000

    - better for testing
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@235 406a0c4d-033a-0410-8de8-e80135713968

--- example.sql
+++ example.sql
@@ -1,6 +1,4 @@
-USE test;
-
-CREATE TABLE IF NOT EXISTS documents
+CREATE TABLE IF NOT EXISTS test.documents
 (
 	id			INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	group_id	INTEGER NOT NULL,
@@ -9,7 +7,8 @@ CREATE TABLE IF NOT EXISTS documents
 	content		TEXT NOT NULL
 );
 
-INSERT INTO documents ( id, group_id, date_added, title, content ) VALUES
+REPLACE INTO test.documents ( id, group_id, date_added, title, content ) VALUES
 	( 1, 1, NOW(), 'test one', 'this is my test document number one' ),
 	( 2, 1, NOW(), 'test two', 'this is my test document number two' ),
-	( 3, 2, NOW(), 'another doc', 'another group' );
+	( 3, 2, NOW(), 'another doc', 'this is another group' ),
+	( 4, 2, NOW(), 'doc number four', 'this is to test groups' );

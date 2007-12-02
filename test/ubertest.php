3b92c420c1ee93a4534904e3ac4d0879814cbac3
shodan <shodan@406a0c4d-033a-0410-8de8-e80135713968>
2007-12-02 18:16:36 +0000

    minor style and output changes
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@950 406a0c4d-033a-0410-8de8-e80135713968

--- test/ubertest.php
+++ test/ubertest.php
@@ -1,5 +1,9 @@
 <?php
 
+//
+// $Id$
+//
+
 $sd_address 		= "localhost";
 $sd_port 			= 6712;
 $sd_log				= "searchd.log";
@@ -47,7 +51,7 @@ if ( !is_array($args) || empty($args) )
 	print ( "t, test\t\t\trun tests and compare results to reference\n" );
 	print ( "\nOptions are:\n" );
 	print ( "-u, --user <USER>\tuse 'USER' as MySQL user\n" );
-	print ( "-P, --password <PASS>\tuse 'PASS' as MySQL password\n" );
+	print ( "-p, --password <PASS>\tuse 'PASS' as MySQL password\n" );
 	print ( "-i, --indexer <PATH>\tpath to indexer\n" );
 	print ( "-s, --searchd <PATH>\tpath to searchd\n" );
 	print ( "\nExamples:\n" );
@@ -65,7 +69,7 @@ for ( $i=0; $i<count($args); $i++ )
 	else if ( $arg=="g" || $arg=="gen" )			{ $g_model = true; $run = true; }
 	else if ( $arg=="t" || $arg=="test" )			{ $g_model = false; $run = true; }
 	else if ( $arg=="-u" || $arg=="--user" )		$db_user = $args[++$i];
-	else if ( $arg=="-P" || $arg=="--password" )	$db_pwd = $args[++$i];
+	else if ( $arg=="-p" || $arg=="--password" )	$db_pwd = $args[++$i];
 	else if ( $arg=="-i" || $arg=="--indexer" )		$indexer_path = $args[++$i];
 	else if ( $arg=="-s" || $arg=="--searchd" )		$searchd_path = $args[++$i];
 	else
@@ -90,14 +94,20 @@ else
 	print ( "PERFORMING AUTOMATED TESTING\n\n" );
 
 
+$t = MyMicrotime ();
+
 $dh = opendir ( "." );
 while ( $entry = readdir($dh) )
-{
-	if ( substr ( $entry,0,4 )!="test" )
-		continue;
+	if ( substr ( $entry,0,4 )=="test" )
+		RunTest ( $entry );
 
-	RunTest ( $entry );
-}
+@unlink ( "error.txt" );
+@unlink ( "config.conf" );
 
-unlink ( "error.txt" );
+printf ( "%.2f sec elapsed\n", MyMicrotime()-$t );
+
+//
+// $Id$
+//
+
 ?>

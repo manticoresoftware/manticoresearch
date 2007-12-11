fb803391a77f5fa7df133c6a1ff0c6b01f5bd730
shodan <shodan@406a0c4d-033a-0410-8de8-e80135713968>
2007-12-11 17:22:42 +0000

    lets clean test results
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@979 406a0c4d-033a-0410-8de8-e80135713968

---
+++ test/clean.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# must be run from tests dir
+if test ! -f ubertest.php; then exit 0; fi
+
+# clean subdirs
+for i in test_*
+do
+	if test ! -f "$i/test.xml"; then continue; fi
+	rm -f "$i/report.txt"
+	rm -fr "$i/Conf/"
+done
+
+# clean files
+rm -f data/*.sp*
+rm -f *.log

9e025f33f068965395f5f4ea17a9451a3db9e2e4
shodan <shodan@406a0c4d-033a-0410-8de8-e80135713968>
2007-12-19 12:24:59 +0000

    banners, keywords, no C comments
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@1005 406a0c4d-033a-0410-8de8-e80135713968

--- src/sphinxmetaphone.cpp
+++ src/sphinxmetaphone.cpp
@@ -1,3 +1,16 @@
+//
+// $Id$
+//
+
+//
+// Copyright (c) 2001-2007, Andrew Aksyonoff. All rights reserved.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License. You should have
+// received a copy of the GPL license along with this program; if you
+// did not, you can find it at http://www.gnu.org/
+//
+
 #include "sphinx.h"
 
 inline int sphUTF8Decode ( BYTE * & pBuf ); // forward ref
@@ -18,18 +31,19 @@ static bool IsVowel ( BYTE c )
 
 static bool SlavoGermanic ( BYTE * pString )
 {
+	// OPTIMIZE!
 	char * szWord = (char *) pString;
 
-    if ( strstr ( szWord, "W" ) )
+	if ( strstr ( szWord, "W" ) )
 		return true;
 
-    if ( strstr( szWord, "K" ) )
+	if ( strstr( szWord, "K" ) )
 		return true;
 
-    if ( strstr ( szWord, "CZ" ) )
+	if ( strstr ( szWord, "CZ" ) )
 		return true;
 
-    if ( strstr ( szWord, "WITZ" ) )
+	if ( strstr ( szWord, "WITZ" ) )
 		return true;
 
 	return false;
@@ -90,7 +104,7 @@ static bool StrAt ( const CurrentWord_t & Word, int iStart, int iLength, const c
 
 static void MetaphAdd ( BYTE * pPrimary, BYTE * pSecondary, char * szAddPrimary, char * szAddSecondary )
 {
-    strcat ( (char *)pPrimary,	 szAddPrimary );
+	strcat ( (char *)pPrimary,	 szAddPrimary );
 	strcat ( (char *)pSecondary, szAddSecondary );
 }
 
@@ -121,13 +135,13 @@ static int ProcessCode ( int iCode, int iCur, CurrentWord_t & Word, BYTE * sPrim
 	case 'O':
 	case 'U':
 	case 'Y':
-		/* all init vowels now map to 'A' */
+		// all init vowels now map to 'A'
 		if ( !iCur )
 			ADD ( "A", "A" );
 		break;
 
 	case 'B':
-		/* "-mb", e.g", "dumb", already skipped over... */
+		// "-mb", e.g", "dumb", already skipped over...
 		ADD_RET ( "P", "P", ( pWord [iCur + 1] == 'B' ) ? 2 : 1 )
 
 	case 0xC7:
@@ -135,37 +149,37 @@ static int ProcessCode ( int iCode, int iCur, CurrentWord_t & Word, BYTE * sPrim
 		ADD_RET ( "S", "S", 1 )
 
 	case 'C':
-		/* various germanic */
+		// various germanic
 		if ( iCur > 1 && !IsVowel ( pWord [iCur - 2] ) && StrAt ( Word, iCur - 1, 3, "ACH" )
 			&& ( pWord [iCur + 2] != 'I' && ( pWord [iCur + 2] != 'E' || StrAt ( Word, iCur - 2, 6, "BACHER", "MACHER" ) ) ) )
 			ADD_RET ( "K", "K", 2 )
 
-		/* special case 'caesar' */
+		// special case 'caesar'
 		if ( iCur == 0 && StrAt ( Word, 0, 6, "CAESAR" ) )
 			ADD_RET ( "S", "S", 2 )
 
-		/* italian 'chianti' */
+		// italian 'chianti'
 		if ( StrAt ( Word, iCur, 4, "CHIA" ) )
 			ADD_RET ( "K", "K", 2 )
 
 		if ( StrAt ( Word, iCur, 2, "CH" ) )
 		{
-			/* find 'michael' */
+			// find 'michael'
 			if ( iCur > 0 && StrAt ( Word, iCur, 4, "CHAE" ) )
 				ADD_RET ( "K", "X", 2 )
 
-			/* greek roots e.g. 'chemistry', 'chorus' */
+			// greek roots e.g. 'chemistry', 'chorus'
 			if ( iCur == 0 && ( StrAt ( Word, iCur + 1, 5, "HARAC", "HARIS" ) || StrAt ( Word, iCur + 1, 3, "HOR", "HYM", "HIA", "HEM" ) ) && !StrAt ( Word, 0, 5, "CHORE" ) )
 				ADD_RET ( "K", "K", 2 )
 
-			/* germanic, greek, or otherwise 'ch' for 'kh' sound */
+			// germanic, greek, or otherwise 'ch' for 'kh' sound
 			if ( ( StrAt ( Word, 0, 4, "VAN ", "VON " ) || StrAt ( Word, 0, 3, "SCH" ) )
-				/*  'architect but not 'arch', 'orchestra', 'orchid' */
+				// 'architect but not 'arch', 'orchestra', 'orchid'
 				|| StrAt ( Word, iCur - 2, 6, "ORCHES", "ARCHIT", "ORCHID" )
 				|| StrAt ( Word, iCur + 2, 1, "T", "S" )
-				|| ( ( StrAt ( Word, iCur - 1, 1, "A", "O", "U", "E" ) || iCur == 0 ) 	/* e.g., 'wachtler', 'wechsler', but not 'tichner' */
+				|| ( ( StrAt ( Word, iCur - 1, 1, "A", "O", "U", "E" ) || iCur == 0 ) 	// e.g., 'wachtler', 'wechsler', but not 'tichner'
 					&& ( StrAt ( Word, iCur + 2, 1, "L", "R", "N", "M" ) || StrAt ( Word, iCur + 2, 1, "B", "H", "F", "V" )
-					  || StrAt ( Word, iCur + 2, 1, "W", " " ) ) ) )
+						|| StrAt ( Word, iCur + 2, 1, "W", " " ) ) ) )
 			{
 				ADD ( "K", "K" );
 			}
@@ -173,7 +187,7 @@ static int ProcessCode ( int iCode, int iCur, CurrentWord_t & Word, BYTE * sPrim
 			{
 				if ( iCur > 0 )
 				{
-					if ( StrAt ( Word, 0, 2, "MC" ) ) /* e.g., "McHugh" */		
+					if ( StrAt ( Word, 0, 2, "MC" ) ) // e.g., "McHugh"
 						ADD ( "K", "K" );
 					else
 						ADD ( "X", "K" );
@@ -185,27 +199,27 @@ static int ProcessCode ( int iCode, int iCur, CurrentWord_t & Word, BYTE * sPrim
 			return 2;
 		}
 
-		/* e.g, 'czerny' */
+		// e.g, 'czerny'
 		if ( StrAt ( Word, iCur, 2, "CZ" ) && !StrAt ( Word, iCur - 2, 4, "WICZ" ) )
 			ADD_RET ( "S", "X", 2 )
 
-		/* e.g., 'focaccia' */
+		// e.g., 'focaccia'
 		if ( StrAt ( Word, iCur + 1, 3, "CIA" ) )
 			ADD_RET ( "X", "X", 3 )
 
-		/* double 'C', but not if e.g. 'McClellan' */
+		// double 'C', but not if e.g. 'McClellan'
 		if ( StrAt ( Word, iCur, 2, "CC" ) && !( iCur == 1 && pWord [0] == 'M' ) )
 		{
-			/* 'bellocchio' but not 'bacchus' */
+			// 'bellocchio' but not 'bacchus'
 			if ( StrAt ( Word, iCur + 2, 1, "I", "E", "H" ) && !StrAt ( Word, iCur + 2, 2, "HU" ) )
 			{
-				/* 'accident', 'accede' 'succeed' */
+				// 'accident', 'accede' 'succeed'
 				if ( ( iCur == 1 && pWord [iCur - 1] == 'A' ) || StrAt ( Word, iCur - 1, 5, "UCCEE", "UCCES" ) )
 					ADD_RET ( "KS", "KS", 2 )
-				else /* 'bacci', 'bertucci', other italian */
+				else // 'bacci', 'bertucci', other italian
 					ADD_RET ( "X", "X", 2 )
 			}
-			else /* Pierce's rule */
+			else // Pierce's rule
 				ADD_RET ( "K", "K", 2 )
 		}
 
@@ -214,17 +228,17 @@ static int ProcessCode ( int iCode, int iCur, CurrentWord_t & Word, BYTE * sPrim
 
 		if ( StrAt ( Word, iCur, 2, "CI", "CE", "CY" ) )
 		{
-			/* italian vs. english */
+			// italian vs. english
 			if ( StrAt ( Word, iCur, 3, "CIO", "CIE", "CIA" ) )
 				ADD_RET ( "S", "X", 2 )
 			else
 				ADD_RET ( "S", "S", 2 )
 		}
 
-		/* else */
+		// else
 		ADD ( "K", "K" );
 
-		/* name sent in 'mac caffrey', 'mac gregor */
+		// name sent in 'mac caffrey', 'mac gregor
 		if ( StrAt ( Word, iCur + 1, 2, " C", " Q", " G" ) )
 			return 3;
 		else
@@ -237,16 +251,16 @@ static int ProcessCode ( int iCode, int iCur, CurrentWord_t & Word, BYTE * sPrim
 	case 'D':
 		if ( StrAt ( Word, iCur, 2, "DG" ) )
 		{
-			if ( StrAt ( Word, iCur + 2, 1, "I", "E", "Y" ) ) /* e.g. 'edge' */
+			if ( StrAt ( Word, iCur + 2, 1, "I", "E", "Y" ) ) // e.g. 'edge'
 				ADD_RET	( "J", "J", 3 )
-			else /* e.g. 'edgar' */
+			else // e.g. 'edgar'
 				ADD_RET	( "TK", "TK", 2 )
 		}
 
 		if ( StrAt ( Word, iCur, 2, "DT", "DD" ) )
 			ADD_RET ( "T", "T", 2 )
 
-		/* else */
+		// else
 		ADD_RET ( "T", "T", 1 )
 
 	case 'F':
@@ -260,7 +274,7 @@ static int ProcessCode ( int iCode, int iCur, CurrentWord_t & Word, BYTE * sPrim
 
 			if ( iCur < 3 )
 			{
-				/* 'ghislane', ghiradelli */
+				// 'ghislane', ghiradelli
 				if ( iCur == 0 )
 				{
 					if ( pWord [iCur + 2] == 'I' )
@@ -270,14 +284,14 @@ static int ProcessCode ( int iCode, int iCur, CurrentWord_t & Word, BYTE * sPrim
 				}
 			}
 
-			/* Parker's rule (with some further refinements) - e.g., 'hugh' */
-			if (   ( iCur > 1 && StrAt ( Word, iCur - 2, 1, "B", "H", "D" ) )	
-				|| ( iCur > 2 && StrAt ( Word, iCur - 3, 1, "B", "H", "D" ) ) /* e.g., 'bough' */
-				|| ( iCur > 3 && StrAt ( Word, iCur - 4, 1, "B", "H" ) ) )  /* e.g., 'broughton' */
+			// Parker's rule (with some further refinements) - e.g., 'hugh'
+			if ( ( iCur > 1 && StrAt ( Word, iCur - 2, 1, "B", "H", "D" ) )	
+				|| ( iCur > 2 && StrAt ( Word, iCur - 3, 1, "B", "H", "D" ) ) // e.g., 'bough'
+				|| ( iCur > 3 && StrAt ( Word, iCur - 4, 1, "B", "H" ) ) ) // e.g., 'broughton'
 				return 2;
 			else
 			{
-				/* e.g., 'laugh', 'McLaughlin', 'cough', 'gough', 'rough', 'tough' */
+				// e.g., 'laugh', 'McLaughlin', 'cough', 'gough', 'rough', 'tough'
 				if ( iCur > 2 && pWord [iCur - 1] == 'U' && StrAt ( Word, iCur - 3, 1, "C", "G", "L", "R", "T" ) )
 					ADD ( "F", "F" );
 				else
@@ -292,7 +306,7 @@ static int ProcessCode ( int iCode, int iCur, CurrentWord_t & Word, BYTE * sPrim
 		{
 			if ( iCur == 1 && IsVowel ( pWord [0] ) && !SlavoGermanic ( pWord ) )
 				ADD ( "KN", "N" );
-			else	/* not e.g. 'cagney' */
+			else	// not e.g. 'cagney'
 				if ( !StrAt ( Word, iCur + 2, 2, "EY" ) && pWord [iCur + 1] != 'Y' && !SlavoGermanic ( pWord ) )
 					ADD ( "N", "KN");
 				else
@@ -301,29 +315,29 @@ static int ProcessCode ( int iCode, int iCur, CurrentWord_t & Word, BYTE * sPrim
 			return 2;
 		}
 
-		/* 'tagliaro' */
+		// 'tagliaro'
 		if ( StrAt ( Word, iCur + 1, 2, "LI" ) && !SlavoGermanic ( pWord ) )
 			ADD_RET ( "KL", "L", 2 )
 
-		/* -ges-,-gep-,-gel-, -gie- at beginning */
-		if ( iCur == 0  && ( pWord [iCur + 1] == 'Y' || StrAt ( Word, iCur + 1, 2, "ES", "EP", "EB", "EL" )
+		// -ges-,-gep-,-gel-, -gie- at beginning
+		if ( iCur == 0 && ( pWord [iCur + 1] == 'Y' || StrAt ( Word, iCur + 1, 2, "ES", "EP", "EB", "EL" )
 			|| StrAt ( Word, iCur + 1, 2, "EY", "IB", "IL", "IN" ) || StrAt ( Word, iCur + 1, 2, "IE", "EI", "ER" ) ) )
 			ADD_RET ( "K", "J", 2 )
 
-		/*  -ger-,  -gy- */
+		// -ger-, -gy-
 		if ( ( StrAt ( Word, iCur + 1, 2, "ER" ) || pWord [iCur + 1] == 'Y' ) && !StrAt ( Word, 0, 6, "DANGER", "RANGER", "MANGER" )
 			 && !StrAt ( Word, iCur - 1, 1, "E", "I" ) && !StrAt ( Word, iCur - 1, 3, "RGY", "OGY") )
 			ADD_RET ( "K", "J", 2 )
 
-		/*  italian e.g, 'biaggi' */
+		// italian e.g, 'biaggi'
 		if ( StrAt ( Word, iCur + 1, 1, "E", "I", "Y" ) || StrAt ( Word, iCur - 1, 4, "AGGI", "OGGI" ) )
 		{
-			/* obvious germanic */
+			// obvious germanic
 			if ( StrAt ( Word, 0, 4, "VAN ", "VON " ) || StrAt ( Word, 0, 3, "SCH" ) || StrAt ( Word, iCur + 1, 2, "ET" ) )
 				ADD ( "K", "K" );
 			else
 			{
-				/* always soft if french ending */
+				// always soft if french ending
 				if ( StrAt ( Word, iCur + 1, 4, "IER " ) )
 					ADD ( "J", "J" );
 				else
@@ -336,13 +350,13 @@ static int ProcessCode ( int iCode, int iCur, CurrentWord_t & Word, BYTE * sPrim
 		ADD_RET ( "K", "K", pWord [iCur + 1] == 'G' ? 2 : 1 )
 
 	case 'H':
-		/* only keep if first & before vowel or btw. 2 vowels */
+		// only keep if first & before vowel or btw. 2 vowels
 		if ( ( iCur == 0 || IsVowel ( pWord [iCur - 1] ) ) && IsVowel ( pWord [iCur + 1] ) )
 			ADD_RET ( "H", "H", 2 )
-		break; /* also takes care of 'HH' */
+		break; // also takes care of 'HH'
 
 	case 'J':
-		/* obvious spanish, 'jose', 'san jacinto' */
+		// obvious spanish, 'jose', 'san jacinto'
 		if ( StrAt ( Word, iCur, 4, "JOSE" ) || StrAt ( Word, 0, 4, "SAN " ) )
 		{	
 			if ( ( iCur == 0 && pWord [iCur + 4] == ' ' ) || StrAt ( Word, 0, 4, "SAN " ) )
@@ -352,10 +366,10 @@ static int ProcessCode ( int iCode, int iCur, CurrentWord_t & Word, BYTE * sPrim
 		}
 
 		if ( iCur == 0 && !StrAt ( Word, iCur, 4, "JOSE" ) ) 
-			ADD ( "J", "A" );	/* Yankelovich/Jankelowicz */
+			ADD ( "J", "A" );	// Yankelovich/Jankelowicz
 		else
 		{
-			/* spanish pron. of e.g. 'bajador' */
+			// spanish pron. of e.g. 'bajador'
 			if ( ( iCur > 0 && IsVowel ( pWord [iCur - 1] ) )&& !SlavoGermanic ( pWord ) && ( pWord [iCur + 1] == 'A' || pWord [iCur + 1] == 'O' ) )
 				ADD ( "J", "H" );
 			else
@@ -368,7 +382,7 @@ static int ProcessCode ( int iCode, int iCur, CurrentWord_t & Word, BYTE * sPrim
 			}
 		}
 
-		if ( pWord [iCur + 1] == 'J' )	/* it could happen! */
+		if ( pWord [iCur + 1] == 'J' )	// it could happen!
 			return 2;
 
 		break;
@@ -379,7 +393,7 @@ static int ProcessCode ( int iCode, int iCur, CurrentWord_t & Word, BYTE * sPrim
 	case 'L':
 		if ( pWord [iCur + 1] == 'L' )
 		{
-			/* spanish e.g. 'cabrillo', 'gallegos' */
+			// spanish e.g. 'cabrillo', 'gallegos'
 			if ( ( iCur == iLast - 2 && StrAt ( Word, iCur - 1, 4, "ILLO", "ILLA", "ALLE" ) )
 				|| ( ( StrAt ( Word, iLast - 1, 2, "AS", "OS" ) || StrAt ( Word, iLast, 1, "A", "O" ) ) && StrAt ( Word, iCur - 1, 4, "ALLE" ) ) )
 				ADD_RET ( "L", "", 2 )
@@ -393,7 +407,7 @@ static int ProcessCode ( int iCode, int iCur, CurrentWord_t & Word, BYTE * sPrim
 	case 'M':
 		ADD ( "M", "M" );
 
-		/* 'dumb','thumb' */
+		// 'dumb','thumb'
 		if ( ( StrAt ( Word, iCur - 1, 3, "UMB" ) && ( iCur + 1 == iLast || StrAt ( Word, iCur + 2, 2, "ER" ) ) ) || pWord [iCur + 1] == 'M' )
 			return 2;
 
@@ -410,14 +424,14 @@ static int ProcessCode ( int iCode, int iCur, CurrentWord_t & Word, BYTE * sPrim
 		if ( pWord [iCur + 1] == 'H')
 			ADD_RET ( "F", "F", 2 )
 
-		/* also account for "campbell", "raspberry" */
+		// also account for "campbell", "raspberry"
 		ADD_RET ( "P", "P", StrAt ( Word, iCur + 1, 1, "P", "B" ) ? 2 : 1 )
 
 	case 'Q':
 		ADD_RET ( "K", "K", pWord [iCur + 1] == 'Q' ? 2 : 1 )
 
 	case 'R':
-		/* french e.g. 'rogier', but exclude 'hochmeier' */
+		// french e.g. 'rogier', but exclude 'hochmeier'
 		if ( iCur == iLast && !SlavoGermanic ( pWord ) && StrAt ( Word, iCur - 2, 2, "IE" ) && !StrAt ( Word, iCur - 4, 2, "ME", "MA" ) )
 			ADD ( "", "R" );
 		else
@@ -426,24 +440,24 @@ static int ProcessCode ( int iCode, int iCur, CurrentWord_t & Word, BYTE * sPrim
 		return pWord [iCur + 1] == 'R' ? 2 : 1;
 
 	case 'S':
-		/* special cases 'island', 'isle', 'carlisle', 'carlysle' */
+		// special cases 'island', 'isle', 'carlisle', 'carlysle'
 		if ( StrAt ( Word, iCur - 1, 3, "ISL", "YSL" ) )
 			return 1;
 
-		/* special case 'sugar-' */
+		// special case 'sugar-'
 		if ( iCur == 0 && StrAt ( Word, iCur, 5, "SUGAR" ) )
 			ADD_RET ( "X", "S", 1 )
 
 		if ( StrAt ( Word, iCur, 2, "SH" ) )
 		{
-			/* germanic */
+			// germanic
 			if ( StrAt ( Word, iCur + 1, 4, "HEIM", "HOEK", "HOLM", "HOLZ" ) )
 				ADD_RET ( "S", "S", 2 )
 			else
 				ADD_RET ( "X", "X", 2 )
 		}
 
-		/* italian & armenian */
+		// italian & armenian
 		if ( StrAt ( Word, iCur, 3, "SIO", "SIA" ) || StrAt ( Word, iCur, 4, "SIAN" ) )
 		{
 			if ( !SlavoGermanic ( pWord ) )
@@ -452,19 +466,19 @@ static int ProcessCode ( int iCode, int iCur, CurrentWord_t & Word, BYTE * sPrim
 				ADD_RET ( "S", "S", 3 )
 		}
 
-		/* german & anglicisations, e.g. 'smith' match 'schmidt', 'snider' match 'schneider' 
-		also, -sz- in slavic language altho in hungarian it is pronounced 's' */
+		// german & anglicisations, e.g. 'smith' match 'schmidt', 'snider' match 'schneider' 
+		// also, -sz- in slavic language altho in hungarian it is pronounced 's'
 		if ( ( iCur == 0 && StrAt ( Word, iCur + 1, 1, "M", "N", "L", "W" ) ) || StrAt ( Word, iCur + 1, 1, "Z" ) )
 			ADD_RET ( "S", "X", StrAt ( Word, iCur + 1, 1, "Z" ) ? 2 : 1 )
 
 		if ( StrAt ( Word, iCur, 2, "SC" ) )
 		{
-			/* Schlesinger's rule */
+			// Schlesinger's rule
 			if ( pWord [iCur + 2] == 'H' )
 				if ( StrAt ( Word, iCur + 3, 2, "OO", "ER", "EN", "UY" )
-					|| StrAt ( Word, iCur + 3, 2, "ED", "EM" ) ) /* dutch origin, e.g. 'school', 'schooner' */
+					|| StrAt ( Word, iCur + 3, 2, "ED", "EM" ) ) // dutch origin, e.g. 'school', 'schooner'
 				{
-					/* 'schermerhorn', 'schenker' */
+					// 'schermerhorn', 'schenker'
 					if ( StrAt ( Word, iCur + 3, 2, "ER", "EN" ) )
 						ADD_RET ( "X", "SK", 3 )
 					else
@@ -481,11 +495,11 @@ static int ProcessCode ( int iCode, int iCur, CurrentWord_t & Word, BYTE * sPrim
 				if ( StrAt ( Word, iCur + 2, 1, "I", "E", "Y" ) )
 					ADD_RET ( "S", "S", 3 )
 
-				/* else */
+				// else
 				ADD_RET ( "SK", "SK", 3 )
 		}
 
-		/* french e.g. 'resnais', 'artois' */
+		// french e.g. 'resnais', 'artois'
 		if ( iCur == iLast && StrAt ( Word, iCur - 2, 2, "AI", "OI" ) )
 			ADD ( "", "S" );
 		else
@@ -502,11 +516,11 @@ static int ProcessCode ( int iCode, int iCur, CurrentWord_t & Word, BYTE * sPrim
 
 		if ( StrAt ( Word, iCur, 2, "TH" ) || StrAt ( Word, iCur, 3, "TTH" ) )
 		{
-			/* special case 'thomas', 'thames' or germanic */
+			// special case 'thomas', 'thames' or germanic
 			if ( StrAt ( Word, iCur + 2, 2, "OM", "AM" ) || StrAt ( Word, 0, 4, "VAN ", "VON " ) || StrAt ( Word, 0, 3, "SCH" ) )
 				ADD_RET ( "T", "T", 2 )
 			else
-				ADD_RET ( "0", "T", 2 ) /* yes, zero */
+				ADD_RET ( "0", "T", 2 ) // yes, zero
 		}
 
 		ADD_RET ( "T", "T", StrAt ( Word, iCur + 1, 1, "T", "D" ) ? 2 : 1 )
@@ -515,38 +529,38 @@ static int ProcessCode ( int iCode, int iCur, CurrentWord_t & Word, BYTE * sPrim
 		ADD_RET ( "F", "F", pWord [iCur + 1] == 'V' ? 2 : 1 )
 
 	case 'W':
-		/* can also be in middle of word */
+		// can also be in middle of word
 		if ( StrAt ( Word, iCur, 2, "WR" ) )
 			ADD_RET ( "R", "R", 2 )
 
 		if ( iCur == 0 && ( IsVowel ( pWord [iCur + 1] ) || StrAt ( Word, iCur, 2, "WH" ) ) )
 		{
-			/* Wasserman should match Vasserman */
+			// Wasserman should match Vasserman
 			if ( IsVowel ( pWord [iCur + 1] ) )
 				ADD ( "A", "F" );
-			else	/* need Uomo to match Womo */
+			else	// need Uomo to match Womo
 				ADD ( "A", "A" );
 		}
 
-		/* Arnow should match Arnoff */
+		// Arnow should match Arnoff
 		if ( ( iCur == iLast && iCur > 0 && IsVowel ( pWord [iCur - 1] ) ) || StrAt ( Word, iCur - 1, 5, "EWSKI", "EWSKY", "OWSKI", "OWSKY" )
 			|| StrAt ( Word, 0, 3, "SCH" ) )
 			ADD_RET ( "", "F", 1 )
 
-		/* polish e.g. 'filipowicz' */
+		// polish e.g. 'filipowicz'
 		if ( StrAt ( Word, iCur, 4, "WICZ", "WITZ" ) )
 			ADD_RET ( "TS", "FX", 4 )
 		break;
 
 	case 'X':
-		/* french e.g. breaux */
+		// french e.g. breaux
 		if ( ! ( iCur == iLast && ( StrAt ( Word, iCur - 3, 3, "IAU", "EAU" ) || StrAt ( Word, iCur - 2, 2, "AU", "OU" ) ) ) )
-			ADD ( "KS", "KS" );          
+			ADD ( "KS", "KS" );
 
 		return ( pWord [iCur + 1] == 'C' || pWord [iCur + 1] == 'X' ) ? 2 : 1;
 
 	case 'Z':
-		/* chinese pinyin e.g. 'zhao' */
+		// chinese pinyin e.g. 'zhao'
 		if ( pWord [iCur + 1] == 'H')
 			ADD_RET ( "J", "J", 2 )
 		else
@@ -590,14 +604,14 @@ void stem_dmetaphone ( BYTE * pWord, bool bUTF8 )
 	Word.iLength		= iLength;
 	Word.iLengthPadded	= strlen ( (const char *)sOriginal );
 
-    /* skip these when at start of word */
-    if ( StrAt ( Word, 0, 2, "GN", "KN", "PN", "WR", "PS" ) )
+	// skip these when at start of word
+	if ( StrAt ( Word, 0, 2, "GN", "KN", "PN", "WR", "PS" ) )
 		iAdvance = 1;
 
-	/* Initial 'X' is pronounced 'Z' e.g. 'Xavier' */
+	// Initial 'X' is pronounced 'Z' e.g. 'Xavier'
 	if ( sOriginal [0] == 'X' )
 	{
-		ADD ( "S", "S" );	/* 'Z' maps to 'S' */
+		ADD ( "S", "S" );	// 'Z' maps to 'S'
 		iAdvance = 1;
 	}
 
@@ -605,7 +619,7 @@ void stem_dmetaphone ( BYTE * pWord, bool bUTF8 )
 	BYTE * pLastPtr = sOriginal;
 	int iCode = -1;
 
-	if ( bUTF8  )
+	if ( bUTF8 )
 		iCode = sphUTF8Decode ( pPtr );
 
 	while ( iCode != 0 )
@@ -637,3 +651,7 @@ void stem_dmetaphone ( BYTE * pWord, bool bUTF8 )
 	strcpy ( (char*)pWord, (char*)sPrimary );
 	// TODO: handle secondary too
 }
+
+//
+// $Id$
+//

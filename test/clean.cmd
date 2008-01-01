7c4447ab9dedc4bc9f6f4d24f2be5d69aa15ac5e
shodan <shodan@406a0c4d-033a-0410-8de8-e80135713968>
2008-01-01 23:33:01 +0000

    cleanup script for Windows
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@1043 406a0c4d-033a-0410-8de8-e80135713968

---
+++ test/clean.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+if exist "ubertest.php" (
+	for /D %%i in (test_*) do (
+		if exist "%%i\test.xml" (
+			del /q "%%i\report.txt" 2>nul
+			rmdir /s /q "%%i\Conf" 2>nul
+		)
+	)
+
+	del /q "data\*.sp*" 2>nul
+	del /q "*.log" 2>nul
+)

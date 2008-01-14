02311b580e8812bd33467ff735a67b40a8f1db31
shodan <shodan@406a0c4d-033a-0410-8de8-e80135713968>
2008-01-14 01:10:06 +0000

    added backslash escaping for number sign (finally!!!)
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@1062 406a0c4d-033a-0410-8de8-e80135713968

--- src/sphinxutils.cpp
+++ src/sphinxutils.cpp
@@ -424,7 +424,16 @@ bool CSphConfigParser::Parse ( const char * sFileName )
 		{
 			if ( *p=='\n' )					{ AddKey ( sToken, sValue ); iValue = 0; LOC_POP (); continue; }
 			if ( *p=='#' )					{ AddKey ( sToken, sValue ); iValue = 0; LOC_POP (); LOC_PUSH ( S_SKIP2NL ); continue; }
-			if ( *p=='\\' )					if ( p[1]=='\r' || p[1]=='\n' ) { LOC_PUSH ( S_SKIP2NL ); continue; }
+			if ( *p=='\\' )					
+			{
+				// backslash at the line end: continuation operator; let the newline be unhanlded
+				if ( p[1]=='\r' || p[1]=='\n' ) { LOC_PUSH ( S_SKIP2NL ); continue; }
+
+				// backslash before number sign: comment start char escaping; advance and pass it
+				if ( p[1]=='#' ) { p++; }
+
+				// otherwise: just a char, pass it
+			}
 			if ( iValue<iValueMax )			{ sValue[iValue++] = *p; sValue[iValue] = '\0'; }
 											continue;
 		}

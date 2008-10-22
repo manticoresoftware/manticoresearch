e9b525350910588a6a2684ce129d7f9026c69ad2
shodan <shodan@406a0c4d-033a-0410-8de8-e80135713968>
2008-10-22 01:35:14 +0000

    no more unix tools (except perl) and special chars in doc rebuild script
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@1512 406a0c4d-033a-0410-8de8-e80135713968

--- doc/mk.cmd
+++ doc/mk.cmd
@@ -1,10 +1,10 @@
 @echo off
 
-pwd | sed -e s/\\/\//g > 1.tmp
+cd | perl -pe s/\\/\//g > 1.tmp
 for /F "usebackq" %%i in (1.tmp) do set PWD=%%i
-cat sphinx.xml ^
-	| sed -e "s/<b>/<emphasis role=\"bold\">/g" ^
-	| sed -e "s/<\/b>/<\/emphasis>/g" ^
+type sphinx.xml ^
+	| perl -pe "s/<b>/<emphasis role=\"bold\">/g" ^
+	| perl -pe "s/<\/b>/<\/emphasis>/g" ^
 	> 1.tmp
 xsltproc ^
 	--stringparam section.autolabel 1 ^
@@ -12,10 +12,9 @@ xsltproc ^
 	--stringparam html.stylesheet %PWD%/sphinx.css ^
 	--stringparam toc.section.depth 4 ^
 	%DOCBOOKXSL%/html/docbook.xsl 1.tmp ^
-	| fromdos | towin ^
-	| sed -e "s/ /\&nbsp;/g" ^
-	| sed -e "s/©/\&copy;/g" ^
+	| perl -pe "s/\xA0/\&nbsp;/g" ^
+	| perl -pe "s/\xA9/\&copy;/g" ^
 	> sphinx.html
-rm 1.tmp
+del 1.tmp
 
 perl html2txt.pl < sphinx.html > sphinx.txt

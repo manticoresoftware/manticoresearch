e6764798e35ff7c135475a7b6ae4e13528e321be
shodan <shodan@406a0c4d-033a-0410-8de8-e80135713968>
2008-11-26 13:48:34 +0000

    automatic fixups for yacc-generated source
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@1580 406a0c4d-033a-0410-8de8-e80135713968

--- src/yy.cmd
+++ src/yy.cmd
@@ -2,3 +2,10 @@
 bison -l -d -o yysphinxexpr.c sphinxexpr.y
 bison -l -d -o yysphinxselect.c sphinxselect.y
 bison -l -d -o yysphinxquery.c sphinxquery.y
+
+type yysphinxexpr.c | perl -npe "s/  __attr/\/\/  __attr/" > yysphinxexpr.new
+type yysphinxselect.c | perl -npe "s/^yyerrlab1:/\/\/yyerrlab1:/m" | perl -npe "s/  __attr/\/\/  __attr/" > yysphinxselect.new
+type yysphinxquery.c | perl -npe "s/^yyerrlab1:/\/\/yyerrlab1:/m" | perl -npe "s/  __attr/\/\/  __attr/" > yysphinxquery.new
+
+del /q yy*.c
+rename yy*.new yy*.c

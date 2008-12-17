1f722a50729630d389c1037bc869660058c195c7
shodan <shodan@406a0c4d-033a-0410-8de8-e80135713968>
2008-12-17 20:46:36 +0000

    added --sql switch to test.php
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@1617 406a0c4d-033a-0410-8de8-e80135713968

--- api/test.php
+++ api/test.php
@@ -37,6 +37,7 @@ if ( !is_array($_SERVER["argv"]) || empty($_SERVER["argv"]) )
 	print ( "-d, --distinct <ATTR>\tcount distinct values of 'ATTR''\n" );
 	print ( "-l, --limit <COUNT>\tretrieve COUNT matches (default: 20)\n" );
 	print ( "--select <EXPRLIST>\tuse 'EXPRLIST' as select-list (default: *)\n" );
+	print ( "--sql <QUERY>\t\trun 'QUERY' as SphinxQL (ignores other options)\n" );
 	exit;
 }
 
@@ -47,6 +48,7 @@ foreach ( $_SERVER["argv"] as $arg )
 $cl = new SphinxClient ();
 
 $q = "";
+$sql = "";
 $mode = SPH_MATCH_ALL;
 $host = "localhost";
 $port = 3312;
@@ -81,6 +83,7 @@ for ( $i=0; $i<count($args); $i++ )
 	else if ( $arg=="-d" || $arg=="--distinct" )	$distinct = $args[++$i];
 	else if ( $arg=="-l" || $arg=="--limit" )		$limit = (int)$args[++$i];
 	else if ( $arg=="--select" )					$select = $args[++$i];
+	else if ( $arg=="--sql" )						$sql = $args[++$i];
 	else if ( $arg=="-fr"|| $arg=="--filterrange" )	$cl->SetFilterRange ( $args[++$i], $args[++$i], $args[++$i] );
 	else if ( $arg=="-r" )
 	{
@@ -99,18 +102,25 @@ for ( $i=0; $i<count($args); $i++ )
 
 $cl->SetServer ( $host, $port );
 $cl->SetConnectTimeout ( 1 );
-$cl->SetWeights ( array ( 100, 1 ) );
-$cl->SetMatchMode ( $mode );
-if ( count($filtervals) )	$cl->SetFilter ( $filter, $filtervals );
-if ( $groupby )				$cl->SetGroupBy ( $groupby, SPH_GROUPBY_ATTR, $groupsort );
-if ( $sortby )				$cl->SetSortMode ( SPH_SORT_EXTENDED, $sortby );
-if ( $sortexpr )			$cl->SetSortMode ( SPH_SORT_EXPR, $sortexpr );
-if ( $distinct )			$cl->SetGroupDistinct ( $distinct );
-if ( $select )				$cl->SetSelect ( $select );
-if ( $limit )				$cl->SetLimits ( 0, $limit, ( $limit>1000 ) ? $limit : 1000 );
-$cl->SetRankingMode ( $ranker );
 $cl->SetArrayResult ( true );
-$res = $cl->Query ( $q, $index );
+
+if ( $sql )
+{
+	$res = $cl->SqlQuery ( $sql );
+
+} else
+{	$cl->SetWeights ( array ( 100, 1 ) );
+	$cl->SetMatchMode ( $mode );
+	if ( count($filtervals) )	$cl->SetFilter ( $filter, $filtervals );
+	if ( $groupby )				$cl->SetGroupBy ( $groupby, SPH_GROUPBY_ATTR, $groupsort );
+	if ( $sortby )				$cl->SetSortMode ( SPH_SORT_EXTENDED, $sortby );
+	if ( $sortexpr )			$cl->SetSortMode ( SPH_SORT_EXPR, $sortexpr );
+	if ( $distinct )			$cl->SetGroupDistinct ( $distinct );
+	if ( $select )				$cl->SetSelect ( $select );
+	if ( $limit )				$cl->SetLimits ( 0, $limit, ( $limit>1000 ) ? $limit : 1000 );
+	$cl->SetRankingMode ( $ranker );
+	$res = $cl->Query ( $q, $index );
+}
 
 ////////////////
 // print me out

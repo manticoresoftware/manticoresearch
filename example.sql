1e7fd438463da15cd963fd1495b327da92ddb248
shodan <shodan@406a0c4d-033a-0410-8de8-e80135713968>
2008-12-25 15:32:28 +0000

    sample tags
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@1625 406a0c4d-033a-0410-8de8-e80135713968

--- example.sql
+++ example.sql
@@ -10,9 +10,23 @@ CREATE TABLE test.documents
 	content		TEXT NOT NULL
 );
 
-
 REPLACE INTO test.documents ( id, group_id, group_id2, date_added, title, content ) VALUES
 	( 1, 1, 5, NOW(), 'test one', 'this is my test document number one. also checking search within phrases.' ),
 	( 2, 1, 6, NOW(), 'test two', 'this is my test document number two' ),
 	( 3, 2, 7, NOW(), 'another doc', 'this is another group' ),
 	( 4, 2, 8, NOW(), 'doc number four', 'this is to test groups' );
+
+DROP TABLE IF EXISTS test.tags;
+
+CREATE TABLE test.tags
+(
+	docid INTEGER NOT NULL,
+	tagid INTEGER NOT NULL,
+	UNIQUE(docid,tagid)
+);
+
+INSERT INTO test.tags VALUES
+	(1,1), (1,3), (1,5), (1,7),
+	(2,6), (2,4), (2,2),
+	(3,15),
+	(4,7), (4,40);

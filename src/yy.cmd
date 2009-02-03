543e89145dfd66f8424ce9a32178690c3cd283f0
shodan <shodan@406a0c4d-033a-0410-8de8-e80135713968>
2009-02-03 23:34:44 +0000

    resurrected a fix for gcc vs __attribute__ braindamage
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@1680 406a0c4d-033a-0410-8de8-e80135713968

--- src/yy.cmd
+++ src/yy.cmd
@@ -8,8 +8,9 @@ flex -i -ollsphinxql.c sphinxql.l
 type yysphinxexpr.c | perl -npe "s/  __attr/\/\/  __attr/" > yysphinxexpr.new
 type yysphinxselect.c | perl -npe "s/^yyerrlab1:/\/\/yyerrlab1:/m" | perl -npe "s/  __attr/\/\/  __attr/" > yysphinxselect.new
 type yysphinxquery.c | perl -npe "s/^yyerrlab1:/\/\/yyerrlab1:/m" | perl -npe "s/  __attr/\/\/  __attr/" > yysphinxquery.new
+type yysphinxql.c | perl -npe "s/  __attr/\/\/  __attr/" > yysphinxql.new
 type llsphinxql.c | perl -npe "s/(#include <unistd.h>)/#if !USE_WINDOWS\n\1\n#endif/" > llsphinxql.new
 
-del /q yysphinxexpr.c yysphinxselect.c yysphinxquery.c llsphinxql.c
+del /q yy*.c ll*.c
 rename yy*.new yy*.c
 rename ll*.new ll*.c

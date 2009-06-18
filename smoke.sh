27229ee067d9d292d16aca147216d9f3ffe0e3f2
klirichek <klirichek@406a0c4d-033a-0410-8de8-e80135713968>
2009-06-18 15:40:59 +0000

    Minor changes: * added lines to .hgignore * modified smoke.sh to run make faster on multikernel systems
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@1837 406a0c4d-033a-0410-8de8-e80135713968

--- smoke.sh
+++ smoke.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
+jobs=$(grep -c processor /proc/cpuinfo)
+
+if [ q"1" = q"$jobs" ] ; then
+    jobs=""
+else
+    jobs="-j$jobs"
+fi
+
 for CONFARGS in "--with-debug" "--with-debug --enable-id64";
 do
 	BANNER="testing $CONFARGS build"
 	./configure $CONFARGS 1>/dev/null 2>&1 || { echo "$BANNER: configure failed"; exit 1; }
 	make clean 1>/dev/null 2>&1 || { echo "$BANNER: make clean failed"; exit 1; }
-	make 1>/dev/null 2>&1 || { echo "$BANNER: make failed"; exit 1; }
+	make $jobs 1>/dev/null 2>&1 || { echo "$BANNER: make failed"; exit 1; }
 
 	cd ./test
 	php ubertest.php t -u test --strict 1>/dev/null 2>&1 || { echo "$BANNER: regression suite failed"; exit 1; }

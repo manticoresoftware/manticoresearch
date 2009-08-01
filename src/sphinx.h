b24f15149ffc5ecee896a2b91a48abb0180b3e99
shodan <shodan@406a0c4d-033a-0410-8de8-e80135713968>
2009-08-01 23:43:07 +0000

    lets globally ignore C4201 (nameless struct/union)
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@1960 406a0c4d-033a-0410-8de8-e80135713968

--- src/sphinx.h
+++ src/sphinx.h
@@ -2324,10 +2324,15 @@ void				sphSetInternalErrorCallback ( SphErrorCallback_fn fnCallback );
 
 /////////////////////////////////////////////////////////////////////////////
 
-/// workaround to suppress C4511/C4512 warnings (copy ctor and assignment operator) in VS 2003
+// workaround to suppress C4511/C4512 warnings (copy ctor and assignment operator) in VS 2003
 #if _MSC_VER>=1300 && _MSC_VER<1400
 #pragma warning(disable:4511)
 #pragma warning(disable:4512)
+#endif
+
+// suppress C4201 (nameless struct/union is a nonstandard extension) because even min-spec gcc 3.4.6 works ok
+#if defined(_MSC_VER)
+#pragma warning(disable:4201)
 #endif
 
 //

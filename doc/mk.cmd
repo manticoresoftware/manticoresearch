15718fbdb3c8516f65495a0ec333868c3a9f6116
shodan <shodan@406a0c4d-033a-0410-8de8-e80135713968>
2010-01-24 02:59:15 +0000

    documented xmlpipe_field_xxx etc directives updated changelog, about, credits, features doc sections; and wrapped overlong lines everywhere updated builder script (wraps overlong list); and rebuilt txt/html
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@2219 406a0c4d-033a-0410-8de8-e80135713968

--- doc/mk.cmd
+++ doc/mk.cmd
@@ -1,21 +1,19 @@
 @echo off
 
-cd | perl -pe s/\\/\//g > 1.tmp
-for /F "usebackq" %%i in (1.tmp) do set PWD=%%i
 type sphinx.xml ^
 	| perl -pe "s/<b>/<emphasis role=\"bold\">/g" ^
 	| perl -pe "s/<\/b>/<\/emphasis>/g" ^
 	| perl -pe "s/bug #(\d+)/<ulink url=\"http:\/\/sphinxsearch.com\/bugs\/view.php\?id=\1\">bug #\1<\/ulink>/" ^
-	> 1.tmp
-xsltproc ^
-	--stringparam section.autolabel 1 ^
-	--stringparam output.html.stylesheets 1 ^
-	--stringparam html.stylesheet %PWD%/sphinx.css ^
-	--stringparam toc.section.depth 4 ^
-	%DOCBOOKXSL%/html/docbook.xsl 1.tmp ^
+	| xsltproc ^
+		--stringparam section.autolabel 1 ^
+		--stringparam output.html.stylesheets 1 ^
+		--stringparam html.stylesheet %CD%/sphinx.css ^
+		--stringparam toc.section.depth 4 ^
+		%DOCBOOKXSL%/html/docbook.xsl ^
+		- ^
 	| perl -pe "s/\xA0/\&nbsp;/g" ^
 	| perl -pe "s/\xA9/\&copy;/g" ^
+	| perl -pe "s/((<\/(li|dt|dt|head|div)>)+)/\1\n/g" ^
 	> sphinx.html
-del 1.tmp
 
 perl html2txt.pl < sphinx.html > sphinx.txt

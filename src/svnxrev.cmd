a5753cbf24d5413bae89e84d97b828a196cf0cca
shodan <shodan@406a0c4d-033a-0410-8de8-e80135713968>
2010-04-05 13:36:34 +0000

    windows batch magic (lets make hg checkouts build)
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@2266 406a0c4d-033a-0410-8de8-e80135713968

--- src/svnxrev.cmd
+++ src/svnxrev.cmd
@@ -1,4 +1,11 @@
 @echo off
+setlocal enabledelayedexpansion
+
+set REVFILE=%1\src\sphinxversion.h
 if exist %1\.svn (
-	svn info --xml %1 | perl %1\src\svnxrev.pl %1\src\sphinxversion.h
+	svn info --xml %1 | perl %1\src\svnxrev.pl %REVFILE%
+) else if exist %1\.hg (
+	if exist %REVFILE% for /f "delims=" %%a in (%REVFILE%) do set oldfile=%%a
+	for /f %%i in ('hg id') do set newfile=#define SPH_SVN_TAGREV "%%i"
+	if "!newfile!" neq "!oldfile!" echo !newfile!> %REVFILE%
 )

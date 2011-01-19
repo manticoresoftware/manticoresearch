73ae4bed4b1b1ec7bcfdf69d2d9334f6441a6635
tomat <tomat@406a0c4d-033a-0410-8de8-e80135713968>
2011-01-19 20:25:31 +0000

    fixed exceptions vs star search ie ( word* ) ( bug #702 ) added regression to test 165
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@2642 406a0c4d-033a-0410-8de8-e80135713968

--- src/sphinx.cpp
+++ src/sphinx.cpp
@@ -2277,6 +2277,10 @@ void CSphLowercaser::AddRemaps ( const CSphVector<CSphRemapRange> & dRemaps, DWO
 			bool bWordPart = ( iCodepoint & MASK_CODEPOINT ) && !( iCodepoint & FLAG_CODEPOINT_SYNONYM );
 			int iNew = iRemapped | uFlags | ( iCodepoint & MASK_FLAGS );
 			iCodepoint = bWordPart ? ( iNew | FLAG_CODEPOINT_DUAL ) : iNew;
+
+			// new code-point flag removes SYNONYM
+			if ( ( iCodepoint & FLAG_CODEPOINT_SYNONYM ) && uFlags==0 && iRemapped!=0 )
+				iCodepoint &= ~FLAG_CODEPOINT_SYNONYM;
 		}
 	}
 }

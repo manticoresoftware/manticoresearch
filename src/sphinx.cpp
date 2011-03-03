12c23188107d641d899b592462736a4cc2bfe635
shodan <shodan@406a0c4d-033a-0410-8de8-e80135713968>
2011-03-03 22:11:40 +0000

    fixed stemming vs exactdict vs internal keywords (stemmed zone names when searching)
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@2697 406a0c4d-033a-0410-8de8-e80135713968

--- src/sphinx.cpp
+++ src/sphinx.cpp
@@ -11665,10 +11665,10 @@ SphWordID_t CSphDictExact::GetWordID ( BYTE * pWord )
 		return 0;
 
 	if ( pWord[0]=='=' )
-	{
 		pWord[0] = MAGIC_WORD_HEAD_NONSTEMMED;
+
+	if ( pWord[0]<' ' )
 		return m_pDict->GetWordIDNonStemmed ( pWord );
-	}
 
 	return m_pDict->GetWordID ( pWord );
 }

77e35297b04ac9a4f083dddd6518be8ed498118e
shodan <shodan@406a0c4d-033a-0410-8de8-e80135713968>
2011-04-22 02:40:59 +0000

    suddenly, version 2.0.1
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@2791 406a0c4d-033a-0410-8de8-e80135713968

--- src/sphinx.h
+++ src/sphinx.h
@@ -189,7 +189,7 @@ inline const	DWORD *	DOCINFO2ATTRS ( const DWORD * pDocinfo )	{ return DOCINFO2A
 #define SPHINX_TAG "-dev"
 #endif
 
-#define SPHINX_VERSION			"1.11" SPHINX_BITS_TAG SPHINX_TAG " (" SPH_SVN_TAGREV ")"
+#define SPHINX_VERSION			"2.0.1" SPHINX_BITS_TAG SPHINX_TAG " (" SPH_SVN_TAGREV ")"
 #define SPHINX_BANNER			"Sphinx " SPHINX_VERSION "\nCopyright (c) 2001-2011, Andrew Aksyonoff\nCopyright (c) 2008-2011, Sphinx Technologies Inc (http://sphinxsearch.com)\n\n"
 #define SPHINX_SEARCHD_PROTO	1
 

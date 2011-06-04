manticoresoftware/manticoresearch

commit 3c600c17a62cdf72cfecb7871f909f2ad91b9bbc

Added syslog logging
git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@2833 406a0c4d-033a-0410-8de8-e80135713968

src/searchd.cpp
@@ -79,6 +79,10 @@
 
 #endif
 
+#if USE_SYSLOG
+	#include <syslog.h>
+#endif
+
 /////////////////////////////////////////////////////////////////////////////
 // MISC GLOBALS
 /////////////////////////////////////////////////////////////////////////////
@@ -183,8 +187,10 @@ enum LogFormat_e
 	LOG_FORMAT_SPHINXQL
 };
 
-static ESphLogLevel		g_eLogLevel		= LOG_INFO;
+static ESphLogLevel		g_eLogLevel		= SPH_LOG_INFO;
 static int				g_iLogFile		= STDOUT_FILENO;	// log file descriptor
+static bool				g_bLogSyslog	= false;
+static bool				g_bQuerySyslog	= false;
 static CSphString		g_sLogFile;							// log file name
 static bool				g_bLogTty		= false;			// cached isatty(g_iLogFile)
 static LogFormat_e		g_eLogFormat	= LOG_FORMAT_PLAIN;
@@ -891,9 +897,9 @@ void sphLogEntry ( ESphLogLevel eLevel, char * sBuf, char * sTtyBuf )
 			WORD eType = EVENTLOG_INFORMATION_TYPE;
 			switch ( eLevel )
 			{
-				case LOG_FATAL:		eType = EVENTLOG_ERROR_TYPE; break;
-				case LOG_WARNING:	eType = EVENTLOG_WARNING_TYPE; break;
-				case LOG_INFO:		eType = EVENTLOG_INFORMATION_TYPE; break;
+				case SPH_LOG_FATAL:		eType = EVENTLOG_ERROR_TYPE; break;
+				case SPH_LOG_WARNING:	eType = EVENTLOG_WARNING_TYPE; break;
+				case SPH_LOG_INFO:		eType = EVENTLOG_INFORMATION_TYPE; break;
 			}
 
 			ReportEvent ( hEventSource,	// event log handle
@@ -934,13 +940,24 @@ void sphLog ( ESphLogLevel eLevel, const char * sFmt, va_list ap )
 	static const int	FLUSH_THRESH_TIME	= 1000000; // in microseconds
 	static const int	FLUSH_THRESH_COUNT	= 100;
 
-	static ESphLogLevel eLastLevel = LOG_INFO;
+	static ESphLogLevel eLastLevel = SPH_LOG_INFO;
 	static DWORD uLastEntry = 0;
 	static int64_t tmLastStamp = -1000000-FLUSH_THRESH_TIME;
 	static int iLastRepeats = 0;
 
 	// only if we can
-	if ( ( sFmt && eLevel>g_eLogLevel ) || ( g_iLogFile<0 && !g_bService ) )
+	if ( sFmt && eLevel>g_eLogLevel )
+		return;
+
+#if USE_SYSLOG
+	if ( g_bLogSyslog && sFmt )
+	{
+		const int levels[] = { LOG_EMERG, LOG_WARNING, LOG_INFO, LOG_DEBUG, LOG_DEBUG, LOG_DEBUG };
+		vsyslog ( levels[eLevel], sFmt, ap );
+	}
+#endif
+
+	if ( g_iLogFile<0 && !g_bService )
 		return;
 
 	// format the banner
@@ -949,9 +966,9 @@ void sphLog ( ESphLogLevel eLevel, const char * sFmt, va_list ap )
 
 	const char * sBanner = "";
 	if ( sFmt==NULL ) eLevel = eLastLevel;
-	if ( eLevel==LOG_FATAL ) sBanner = "FATAL: ";
-	if ( eLevel==LOG_WARNING ) sBanner = "WARNING: ";
-	if ( eLevel>=LOG_DEBUG ) sBanner = "DEBUG: ";
+	if ( eLevel==SPH_LOG_FATAL ) sBanner = "FATAL: ";
+	if ( eLevel==SPH_LOG_WARNING ) sBanner = "WARNING: ";
+	if ( eLevel>=SPH_LOG_DEBUG ) sBanner = "DEBUG: ";
 
 	char sBuf [ 1024 ];
 	snprintf ( sBuf, sizeof(sBuf)-1, "[%s] [%5d] ", sTimeBuf, (int)getpid() );
@@ -990,7 +1007,7 @@ void sphLog ( ESphLogLevel eLevel, const char * sFmt, va_list ap )
 
 		tmLastStamp = tmNow;
 		iLastRepeats = 0;
-		eLastLevel = LOG_INFO;
+		eLastLevel = SPH_LOG_INFO;
 		uLastEntry = 0;
 	}
 
@@ -1012,7 +1029,7 @@ void sphFatal ( const char * sFmt, ... )
 {
 	va_list ap;
 	va_start ( ap, sFmt );
-	sphLog ( LOG_FATAL, sFmt, ap );
+	sphLog ( SPH_LOG_FATAL, sFmt, ap );
 	va_end ( ap );
 	Shutdown ();
 	exit ( 1 );
@@ -4332,7 +4349,7 @@ bool ParseSearchQuery ( InputBuffer_c & tReq, CSphQuery & tQuery, int iVer, int 
 void LogQueryPlain ( const CSphQuery & tQuery, const CSphQueryResult & tRes )
 {
 	assert ( g_eLogFormat==LOG_FORMAT_PLAIN );
-	if ( g_iQueryLogFile<0 || !tRes.m_sError.IsEmpty() )
+	if ( ( !g_bQuerySyslog && g_iQueryLogFile<0 ) || !tRes.m_sError.IsEmpty() )
 		return;
 
 	char sBuf[2048];
@@ -4340,9 +4357,19 @@ void LogQueryPlain ( const CSphQuery & tQuery, const CSphQueryResult & tRes )
 	char * pMax = sBuf+sizeof(sBuf)-4;
 
 	// [time]
-	*p++ = '[';
-	p += sphFormatCurrentTime ( p, pMax-p );
-	*p++ = ']';
+#if USE_SYSLOG
+	if ( !g_bQuerySyslog )
+	{
+#endif
+
+		*p++ = '[';
+		p += sphFormatCurrentTime ( p, pMax-p );
+		*p++ = ']';
+
+#if USE_SYSLOG
+	} else
+		p += snprintf ( p, pMax-p, "[query]" );
+#endif
 
 	// querytime sec
 	int iQueryTime = Max ( tRes.m_iQueryTime, 0 );
@@ -4406,6 +4433,11 @@ void LogQueryPlain ( const CSphQuery & tQuery, const CSphQueryResult & tRes )
 			*p = ( *q=='\n' ) ? ' ' : *q;
 	}
 
+#if USE_SYSLOG
+	if ( !g_bQuerySyslog )
+	{
+#endif
+
 	// line feed
 	if ( p<pMax-1 )
 	{
@@ -4417,6 +4449,16 @@ void LogQueryPlain ( const CSphQuery & tQuery, const CSphQueryResult & tRes )
 
 	lseek ( g_iQueryLogFile, 0, SEEK_END );
 	sphWrite ( g_iQueryLogFile, sBuf, strlen(sBuf) );
+
+#if USE_SYSLOG
+	} else
+	{
+		if ( p<pMax )
+			*p++ = '\0';
+		sBuf[sizeof(sBuf)-1] = '\0';
+		syslog ( LOG_INFO, "%s", sBuf );
+	}
+#endif
 }
 
 
@@ -10677,13 +10719,13 @@ void HandleMysqlSet ( NetOutputBuffer_c & tOut, BYTE & uPacketID, SqlStmt_t & tS
 		} else if ( tStmt.m_sSetName=="log_level" )
 		{
 			if ( tStmt.m_sSetValue=="info" )
-				g_eLogLevel = LOG_INFO;
+				g_eLogLevel = SPH_LOG_INFO;
 			else if ( tStmt.m_sSetValue=="debug" )
-				g_eLogLevel = LOG_DEBUG;
+				g_eLogLevel = SPH_LOG_DEBUG;
 			else if ( tStmt.m_sSetValue=="debugv" )
-				g_eLogLevel = LOG_VERBOSE_DEBUG;
+				g_eLogLevel = SPH_LOG_VERBOSE_DEBUG;
 			else if ( tStmt.m_sSetValue=="debugvv" )
-				g_eLogLevel = LOG_VERY_VERBOSE_DEBUG;
+				g_eLogLevel = SPH_LOG_VERY_VERBOSE_DEBUG;
 			else
 			{
 				SendMysqlErrorPacket ( tOut, uPacketID, "Unknown log_level value (must be one of info, debug, debugv, debugvv)" );
@@ -12735,7 +12777,7 @@ void CheckReopen ()
 	}
 
 	// reopen query log
-	if ( g_iQueryLogFile!=g_iLogFile && g_iQueryLogFile>=0 && !isatty ( g_iQueryLogFile ) )
+	if ( !g_bQuerySyslog && g_iQueryLogFile!=g_iLogFile && g_iQueryLogFile>=0 && !isatty ( g_iQueryLogFile ) )
 	{
 		int iFD = ::open ( g_sQueryLogFile.cstr(), O_CREAT | O_RDWR | O_APPEND, S_IREAD | S_IWRITE );
 		if ( iFD<0 )
@@ -13983,14 +14025,31 @@ void OpenDaemonLog ( const CSphConfigSection & hSearchd )
 	// create log
 		const char * sLog = "searchd.log";
 		if ( hSearchd.Exists ( "log" ) )
-			sLog = hSearchd["log"].cstr();
+		{
+			if ( hSearchd["log"]=="syslog" )
+#if !USE_SYSLOG
+				if ( g_iLogFile<0 )
+				{
+					g_iLogFile = STDOUT_FILENO;
+					sphWarning ( "failed to use syslog for logging. You have to reconfigure --with-syslog and rebuild the daemon!" );
+					sphInfo ( "will use default file 'searchd.log' for logging." );
+				}
+#else
+				g_bLogSyslog = true;
+#endif
+			else
+				sLog = hSearchd["log"].cstr();
+		}
 
 		umask ( 066 );
-		g_iLogFile = open ( sLog, O_CREAT | O_RDWR | O_APPEND, S_IREAD | S_IWRITE );
-		if ( g_iLogFile<0 )
+		if ( !g_bLogSyslog )
 		{
-			g_iLogFile = STDOUT_FILENO;
-			sphFatal ( "failed to open log file '%s': %s", sLog, strerror(errno) );
+			g_iLogFile = open ( sLog, O_CREAT | O_RDWR | O_APPEND, S_IREAD | S_IWRITE );
+			if ( g_iLogFile<0 )
+			{
+				g_iLogFile = STDOUT_FILENO;
+				sphFatal ( "failed to open log file '%s': %s", sLog, strerror(errno) );
+			}
 		}
 
 		g_sLogFile = sLog;
@@ -14078,9 +14137,9 @@ int WINAPI ServiceMain ( int argc, char **argv )
 #else
 		OPT1 ( "--nodetach" )		g_bOptNoDetach = true;
 #endif
-		OPT1 ( "--logdebug" )		g_eLogLevel = LOG_DEBUG;
-		OPT1 ( "--logdebugv" )		g_eLogLevel = LOG_VERBOSE_DEBUG;
-		OPT1 ( "--logdebugvv" )		g_eLogLevel = LOG_VERY_VERBOSE_DEBUG;
+		OPT1 ( "--logdebug" )		g_eLogLevel = SPH_LOG_DEBUG;
+		OPT1 ( "--logdebugv" )		g_eLogLevel = SPH_LOG_VERBOSE_DEBUG;
+		OPT1 ( "--logdebugvv" )		g_eLogLevel = SPH_LOG_VERY_VERBOSE_DEBUG;
 		OPT1 ( "--safetrace" )		g_bSafeTrace = true;
 		OPT1 ( "--test" )			{ g_bWatchdog = false; bTestMode = true; }
 		OPT1 ( "--strip-path" )		g_bStripPath = true;
@@ -14482,7 +14541,21 @@ int WINAPI ServiceMain ( int argc, char **argv )
 		// create log
 		const char * sLog = "searchd.log";
 		if ( hSearchd.Exists ( "log" ) )
-			sLog = hSearchd["log"].cstr();
+		{
+			if ( hSearchd["log"]=="syslog" )
+#if !USE_SYSLOG
+				if ( g_iLogFile<0 )
+				{
+					g_iLogFile = STDOUT_FILENO;
+					sphWarning ( "failed to use syslog for logging. You have to reconfigure --with-syslog and rebuild the daemon!" );
+					sphInfo ( "will use default file 'searchd.log' for logging." );
+				}
+#else
+				g_bLogSyslog = true;
+#endif
+			else
+				sLog = hSearchd["log"].cstr();
+		}
 
 		umask ( 066 );
 		if ( g_iLogFile!=STDOUT_FILENO )
@@ -14490,11 +14563,14 @@ int WINAPI ServiceMain ( int argc, char **argv )
 			close ( g_iLogFile );
 			g_iLogFile = STDOUT_FILENO;
 		}
-		g_iLogFile = open ( sLog, O_CREAT | O_RDWR | O_APPEND, S_IREAD | S_IWRITE );
-		if ( g_iLogFile<0 )
+		if ( !g_bLogSyslog )
 		{
-			g_iLogFile = STDOUT_FILENO;
-			sphFatal ( "failed to open log file '%s': %s", sLog, strerror(errno) );
+			g_iLogFile = open ( sLog, O_CREAT | O_RDWR | O_APPEND, S_IREAD | S_IWRITE );
+			if ( g_iLogFile<0 )
+			{
+				g_iLogFile = STDOUT_FILENO;
+				sphFatal ( "failed to open log file '%s': %s", sLog, strerror(errno) );
+			}
 		}
 
 		g_sLogFile = sLog;
@@ -14503,9 +14579,14 @@ int WINAPI ServiceMain ( int argc, char **argv )
 		// create query log if required
 		if ( hSearchd.Exists ( "query_log" ) )
 		{
-			g_iQueryLogFile = open ( hSearchd["query_log"].cstr(), O_CREAT | O_RDWR | O_APPEND, S_IREAD | S_IWRITE );
-			if ( g_iQueryLogFile<0 )
-				sphFatal ( "failed to open query log file '%s': %s", hSearchd["query_log"].cstr(), strerror(errno) );
+			if ( hSearchd["query_log"]=="syslog" )
+				g_bQuerySyslog = true;
+			else
+			{
+				g_iQueryLogFile = open ( hSearchd["query_log"].cstr(), O_CREAT | O_RDWR | O_APPEND, S_IREAD | S_IWRITE );
+				if ( g_iQueryLogFile<0 )
+					sphFatal ( "failed to open query log file '%s': %s", hSearchd["query_log"].cstr(), strerror(errno) );
+			}
 			g_sQueryLogFile = hSearchd["query_log"].cstr();
 		}
 	}
@@ -14635,8 +14716,21 @@ int WINAPI ServiceMain ( int argc, char **argv )
 
 	// if we're running in console mode, dump queries to tty as well
 	if ( g_bOptNoLock && hSearchd ( "query_log" ) )
+	{
+		g_bQuerySyslog = false;
+		g_bLogSyslog = false;
 		g_iQueryLogFile = g_iLogFile;
+	}
 
+#if USE_SYSLOG
+	if ( g_bLogSyslog || g_bQuerySyslog )
+	{
+		openlog ( "searchd", LOG_PID, LOG_DAEMON );
+	}
+#else
+	if ( g_bQuerySyslog )
+		sphFatal ( "Wrong query_log file! You have to reconfigure --with-syslog and rebuild daemon if you want to use syslog there." );
+#endif
 	/////////////////
 	// serve clients
 	/////////////////

config/config.h.in
@@ -250,6 +250,9 @@
 /* Define to 1 if you want to compile with PostgreSQL support */
 #undef USE_PGSQL
 
+/* define to use POSIX Syslog for logging */
+#undef USE_SYSLOG
+
 /* define to use Zlib */
 #undef USE_ZLIB
 

configure
@@ -718,6 +718,7 @@ enable_id64
 with_libstemmer
 with_iconv
 with_unixodbc
+with_syslog
 '
       ac_precious_vars='build_alias
 host_alias
@@ -1369,6 +1370,8 @@ Optional Packages:
   --with-iconv            compile with iconv support (default is autodetect)
   --with-unixodbc         compile with UnixODBC support (default is
                           autodetect)
+  --with-syslog           compile with possibility to use syslog for logging
+                          (default is no)
 
 Some influential environment variables:
   CC          C compiler command
@@ -4863,6 +4866,14 @@ else
 fi
 
 
+ac_fn_c_check_header_mongrel "$LINENO" "syslog.h" "ac_cv_header_syslog_h" "$ac_includes_default"
+if test "x$ac_cv_header_syslog_h" = x""yes; then :
+  have_syslog_h=yes
+else
+  have_syslog_h=no
+fi
+
+
 
 
 
@@ -7236,6 +7247,7 @@ fi
 
 
 
+
 # Check whether --with-libstemmer was given.
 if test "${with_libstemmer+set}" = set; then :
   withval=$with_libstemmer; ac_cv_use_libstemmer=$withval
@@ -7477,6 +7489,35 @@ $as_echo "not found" >&6; }
 			{ $as_echo "$as_me:${as_lineno-$LINENO}: WARNING: ODBC source support will NOT be available" >&5
 $as_echo "$as_me: WARNING: ODBC source support will NOT be available" >&2;}
 		fi
+	fi
+else
+	{ $as_echo "$as_me:${as_lineno-$LINENO}: result: disabled" >&5
+$as_echo "disabled" >&6; }
+fi
+
+
+
+
+# Check whether --with-syslog was given.
+if test "${with_syslog+set}" = set; then :
+  withval=$with_syslog; ac_cv_use_syslog=$withval
+else
+  ac_cv_use_syslog=no
+
+fi
+
+{ $as_echo "$as_me:${as_lineno-$LINENO}: checking for Syslog" >&5
+$as_echo_n "checking for Syslog... " >&6; }
+if test  $ac_cv_use_syslog != no ; then
+	if test  $have_syslog_h = yes ; then
+
+$as_echo "#define USE_SYSLOG 1" >>confdefs.h
+
+		{ $as_echo "$as_me:${as_lineno-$LINENO}: result: yes" >&5
+$as_echo "yes" >&6; }
+	else
+		{ $as_echo "$as_me:${as_lineno-$LINENO}: result: no" >&5
+$as_echo "no" >&6; }
 	fi
 else
 	{ $as_echo "$as_me:${as_lineno-$LINENO}: result: disabled" >&5

configure.ac
@@ -72,6 +72,7 @@ AC_CHECK_HEADER(expat.h,[have_expat_h=yes],[have_expat_h=no])
 AC_CHECK_HEADER(iconv.h,[have_iconv_h=yes],[have_iconv_h=no])
 AC_CHECK_HEADER(zlib.h,[have_zlib_h=yes],[have_zlib_h=no])
 AC_CHECK_HEADER(sql.h,[have_sql_h=yes],[have_sql_h=no])
+AC_CHECK_HEADER(syslog.h,[have_syslog_h=yes],[have_syslog_h=no])
 
 dnl --------------------------------------------------------------------------
 
@@ -398,6 +399,25 @@ if test [ $ac_cv_use_unixodbc != no ]; then
 			AC_MSG_RESULT([not found])
 			AC_MSG_WARN([ODBC source support will NOT be available])
 		fi
+	fi
+else
+	AC_MSG_RESULT([disabled])
+fi
+
+dnl ---
+
+
+AC_ARG_WITH([syslog],
+	AC_HELP_STRING([--with-syslog], [compile with possibility to use syslog for logging (default is no)]),
+	[ac_cv_use_syslog=$withval], [ac_cv_use_syslog=no]
+)
+AC_MSG_CHECKING([for Syslog])
+if test [ $ac_cv_use_syslog != no ]; then
+	if test [ $have_syslog_h = yes ]; then
+		AC_DEFINE([USE_SYSLOG],1,[define to use POSIX Syslog for logging])
+		AC_MSG_RESULT([yes])
+	else
+		AC_MSG_RESULT([no])
 	fi
 else
 	AC_MSG_RESULT([disabled])

doc/sphinx.xml
@@ -2463,7 +2463,8 @@ OPTION ranker=proximity;
 </programlisting>
 Note that <b>all</b> requests would be logged in this format,
 including those sent via SphinxAPI and SphinxSE, not just those
-sent via SphinxQL.
+sent via SphinxQL. Also note, that this kind of logging works only with plain log
+files and will not work if you use 'syslog' for logging.
 </para>
 <para>
 The features of SphinxQL log format compared to the default text
@@ -9603,6 +9604,10 @@ Log file name.
 Optional, default is 'searchd.log'.
 All <filename>searchd</filename> run time events will be logged in this file.
 </para>
+<para>
+Also you can use the 'syslog' as the file name. In this case the events will be sent to syslog daemon.
+To use the syslog option the sphinx must be configured '--with-syslog' on building.
+</para>
 <bridgehead>Example:</bridgehead>
 <programlisting>
 log = /var/log/searchd.log
@@ -9615,6 +9620,12 @@ log = /var/log/searchd.log
 Query log file name.
 Optional, default is empty (do not log queries).
 All search queries will be logged in this file. The format is described in <xref linkend="query-log-format"/>.
+</para>
+<para>
+In case of 'plain' format, you can use the 'syslog' as the path to the log file.
+In this case all search queries will be sent to syslog daemon with LOG_INFO priority,
+prefixed with '[query]' instead of timestamp.
+To use the syslog option the sphinx must be configured '--with-syslog' on building.
 </para>
 <bridgehead>Example:</bridgehead>
 <programlisting>

src/indexer.cpp
@@ -285,17 +285,17 @@ void ShowProgress ( const CSphIndexProgress * pProgress, bool bPhaseEnd )
 
 static void Logger ( ESphLogLevel eLevel, const char * sFmt, va_list ap )
 {
-	if ( eLevel>=LOG_DEBUG )
+	if ( eLevel>=SPH_LOG_DEBUG )
 		return;
 
 	switch ( eLevel )
 	{
-		case LOG_FATAL: fprintf ( stdout, "FATAL: " ); break;
-		case LOG_WARNING: fprintf ( stdout, "WARNING: " ); break;
-		case LOG_INFO: fprintf ( stdout, "WARNING: " ); break;
-		case LOG_DEBUG: // yes, I know that this branch will never execute because of the condition above.
-		case LOG_VERBOSE_DEBUG:
-		case LOG_VERY_VERBOSE_DEBUG: fprintf ( stdout, "DEBUG: " ); break;
+		case SPH_LOG_FATAL: fprintf ( stdout, "FATAL: " ); break;
+		case SPH_LOG_WARNING: fprintf ( stdout, "WARNING: " ); break;
+		case SPH_LOG_INFO: fprintf ( stdout, "WARNING: " ); break;
+		case SPH_LOG_DEBUG: // yes, I know that this branch will never execute because of the condition above.
+		case SPH_LOG_VERBOSE_DEBUG:
+		case SPH_LOG_VERY_VERBOSE_DEBUG: fprintf ( stdout, "DEBUG: " ); break;
 	}
 
 	vfprintf ( stdout, sFmt, ap );

src/sphinx.h
@@ -27,6 +27,7 @@
 	#define USE_LIBXML		0	/// whether to compile libxml support
 	#define	USE_LIBSTEMMER	0	/// whether to compile libstemmber support
 	#define USE_WINDOWS		1	/// whether to compile for Windows
+	#define USE_SYSLOG		0	/// whether to use syslog for logging
 
 	#define UNALIGNED_RAM_ACCESS	1
 	#define USE_LITTLE_ENDIAN		1

src/sphinxutils.cpp
@@ -1105,7 +1105,7 @@ void sphWarning ( const char * sFmt, ... )
 {
 	va_list ap;
 	va_start ( ap, sFmt );
-	Log ( LOG_WARNING, sFmt, ap );
+	Log ( SPH_LOG_WARNING, sFmt, ap );
 	va_end ( ap );
 }
 
@@ -1114,7 +1114,7 @@ void sphInfo ( const char * sFmt, ... )
 {
 	va_list ap;
 	va_start ( ap, sFmt );
-	Log ( LOG_INFO, sFmt, ap );
+	Log ( SPH_LOG_INFO, sFmt, ap );
 	va_end ( ap );
 }
 
@@ -1122,7 +1122,7 @@ void sphLogFatal ( const char * sFmt, ... )
 {
 	va_list ap;
 	va_start ( ap, sFmt );
-	Log ( LOG_FATAL, sFmt, ap );
+	Log ( SPH_LOG_FATAL, sFmt, ap );
 	va_end ( ap );
 }
 
@@ -1130,7 +1130,7 @@ void sphLogDebug ( const char * sFmt, ... )
 {
 	va_list ap;
 	va_start ( ap, sFmt );
-	Log ( LOG_DEBUG, sFmt, ap );
+	Log ( SPH_LOG_DEBUG, sFmt, ap );
 	va_end ( ap );
 }
 
@@ -1138,7 +1138,7 @@ void sphLogDebugv ( const char * sFmt, ... )
 {
 	va_list ap;
 	va_start ( ap, sFmt );
-	Log ( LOG_VERBOSE_DEBUG, sFmt, ap );
+	Log ( SPH_LOG_VERBOSE_DEBUG, sFmt, ap );
 	va_end ( ap );
 }
 
@@ -1146,7 +1146,7 @@ void sphLogDebugvv ( const char * sFmt, ... )
 {
 	va_list ap;
 	va_start ( ap, sFmt );
-	Log ( LOG_VERY_VERBOSE_DEBUG, sFmt, ap );
+	Log ( SPH_LOG_VERY_VERBOSE_DEBUG, sFmt, ap );
 	va_end ( ap );
 }
 

commit 2a2cb8e7e8549031a2d9ae4aeeee79a806a1acd2

fixed *logs path ( broken by r2833 )
git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@2842 406a0c4d-033a-0410-8de8-e80135713968

src/searchd.cpp
@@ -14090,6 +14090,7 @@ void OpenDaemonLog ( const CSphConfigSection & hSearchd )
 		if ( hSearchd.Exists ( "log" ) )
 		{
 			if ( hSearchd["log"]=="syslog" )
+			{
 #if !USE_SYSLOG
 				if ( g_iLogFile<0 )
 				{
@@ -14100,8 +14101,10 @@ void OpenDaemonLog ( const CSphConfigSection & hSearchd )
 #else
 				g_bLogSyslog = true;
 #endif
-			else
+			} else
+			{
 				sLog = hSearchd["log"].cstr();
+			}
 		}
 
 		umask ( 066 );
@@ -14606,6 +14609,7 @@ int WINAPI ServiceMain ( int argc, char **argv )
 		if ( hSearchd.Exists ( "log" ) )
 		{
 			if ( hSearchd["log"]=="syslog" )
+			{
 #if !USE_SYSLOG
 				if ( g_iLogFile<0 )
 				{
@@ -14616,8 +14620,10 @@ int WINAPI ServiceMain ( int argc, char **argv )
 #else
 				g_bLogSyslog = true;
 #endif
-			else
+			} else
+			{
 				sLog = hSearchd["log"].cstr();
+			}
 		}
 
 		umask ( 066 );
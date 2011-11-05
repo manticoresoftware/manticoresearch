2448aa90bcee3ab2d62aea8e5f1cb616aeb5ca95
shodan <shodan@406a0c4d-033a-0410-8de8-e80135713968>
2011-11-05 14:57:52 +0000

    minor documentation updates, rebuilt html/txt versions
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@2999 406a0c4d-033a-0410-8de8-e80135713968

--- doc/mk.cmd
+++ doc/mk.cmd
@@ -30,6 +30,7 @@ type sphinx.xml ^
 	| perl -pe "s/((<\/(li|dt|dt|head|div)>)+)/\1\n/g" ^
 	| perl -pe "s/<a name=\"id\d+\"><\/a>//g" ^
 	| perl -pe "s/<\/head>/\n<style type=\"text\/css\">pre.programlisting { background-color: #f0f0f0; padding: 0.5em; margin-left: 2em; margin-right: 2em; }<\/style>\n<\/head>/" ^
+	| perl -e "undef $/; $_ = <>; s/<li><p>(([^<]|(<\/?(a|code|span|strong|b|i|pre)\b[^>]*?>))+)<\/p><\/li>/<li>\1<\/li>/gms; print;" ^
 	> sphinx.html
 
 perl html2txt.pl < sphinx.html > sphinx.txt

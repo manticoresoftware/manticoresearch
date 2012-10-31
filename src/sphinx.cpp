95a227ee9e39f2f10df75fe3f3f0077b4f33e63e
shodan <shodan@406a0c4d-033a-0410-8de8-e80135713968>
2012-10-31 15:29:47 +0000

    fixed index_field_lengths, added a test, likely regressed #1310 again
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@3496 406a0c4d-033a-0410-8de8-e80135713968

--- src/sphinx.cpp
+++ src/sphinx.cpp
@@ -9636,7 +9636,7 @@ int CSphHitBuilder::cidxWriteRawVLB ( int fd, CSphWordHit * pHit, int iHits, DWO
 			}
 
 			if ( !pAttrs )
-				sphDie ( "INTERNAL ERROR: pDocinfo wasn't filled?" );
+				sphDie ( "INTERNAL ERROR: failed to lookup attributes while saving collected hits" );
 			assert ( DOCINFO2ID ( pAttrs - DOCINFO_IDSIZE )==pHit->m_iDocID );
 			iAttrID = pHit->m_iDocID;
 		}
@@ -11338,17 +11338,6 @@ int CSphIndex_VLN::Build ( const CSphVector<CSphSource*> & dSources, int iMemory
 					pSource->m_tDocInfo.SetAttr ( m_tSchema.GetAttr(iAttr).m_tLocator, iNumWords );
 				}
 
-			if ( m_tSettings.m_eDocinfo!=SPH_DOCINFO_NONE )
-			{
-				// store next entry
-				DOCINFOSETID ( pDocinfo, pSource->m_tDocInfo.m_iDocID );
-
-				// old docinfo found, use it instead of the new one
-				const DWORD * pSrc = pPrevDocinfo ? DOCINFO2ATTRS ( pPrevDocinfo ) : pSource->m_tDocInfo.m_pDynamic;
-				memcpy ( DOCINFO2ATTRS ( pDocinfo ), pSrc, sizeof(CSphRowitem)*m_tSchema.GetRowSize() );
-				pDocinfo += iDocinfoStride;
-			}
-
 			// store hits
 			while ( const ISphHits * pDocHits = pSource->IterateHits ( m_sLastWarning ) )
 			{
@@ -11444,6 +11433,14 @@ int CSphIndex_VLN::Build ( const CSphVector<CSphSource*> & dSources, int iMemory
 			// because field lengths are computed during that iterating
 			if ( m_tSettings.m_eDocinfo!=SPH_DOCINFO_NONE )
 			{
+				// store next entry
+				DOCINFOSETID ( pDocinfo, pSource->m_tDocInfo.m_iDocID );
+
+				// old docinfo found, use it instead of the new one
+				const DWORD * pSrc = pPrevDocinfo ? DOCINFO2ATTRS ( pPrevDocinfo ) : pSource->m_tDocInfo.m_pDynamic;
+				memcpy ( DOCINFO2ATTRS ( pDocinfo ), pSrc, sizeof(CSphRowitem)*m_tSchema.GetRowSize() );
+				pDocinfo += iDocinfoStride;
+
 				// if not inlining, flush buffer if it's full
 				// (if inlining, it will flushed later, along with the hits)
 				if ( m_tSettings.m_eDocinfo==SPH_DOCINFO_EXTERN && pDocinfo>=pDocinfoMax )

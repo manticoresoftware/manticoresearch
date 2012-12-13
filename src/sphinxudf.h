3fdd2460b12b66b7f000d4785ffc6411027641b6
shodan <shodan@406a0c4d-033a-0410-8de8-e80135713968>
2012-12-13 14:51:20 +0000

    added sphinx_unpack_factors() helper to UDF interface
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@3569 406a0c4d-033a-0410-8de8-e80135713968

--- src/sphinxudf.h
+++ src/sphinxudf.h
@@ -69,6 +69,116 @@ typedef long long				sphinx_int64_t;
 typedef unsigned long long		sphinx_uint64_t;
 #endif
 
+//////////////////////////////////////////////////////////////////////////
+
+/// unpacked representation of all the field-level ranking factors
+typedef struct st_sphinx_field_factors
+{
+	unsigned int	hit_count;
+	unsigned int	id;
+	unsigned int	lcs;
+	unsigned int	word_count;
+	float			tf_idf;
+	float			min_idf;
+	float			max_idf;
+	float			sum_idf;
+	int				min_hit_pos;
+	int				min_best_span_pos;
+	unsigned int	exact_hit;
+	int				max_window_hits;
+} SPH_UDF_FIELD_FACTORS;
+
+/// unpacked representation of all the term-level ranking factors
+typedef struct st_sphinx_term_factors
+{
+	unsigned int	keyword_mask;
+	unsigned int	id;
+	int				tf;
+	float			idf;
+} SPH_UDF_TERM_FACTORS;
+
+#define SPH_UDF_MAX_FIELD_FACTORS	256
+#define SPH_UDF_MAX_TERM_FACTORS	2048
+
+/// unpacked representation of all the ranking factors (document, field, and term-level)
+typedef struct st_sphinx_factors
+{
+	int					doc_bm25;
+	float				doc_bm25a;
+	unsigned int		matched_fields;
+	int					doc_word_count;
+	int					num_fields;
+	int					max_uniq_qpos;
+
+	SPH_UDF_FIELD_FACTORS	field [ SPH_UDF_MAX_FIELD_FACTORS ];
+	SPH_UDF_TERM_FACTORS	term [ SPH_UDF_MAX_TERM_FACTORS ];
+} SPH_UDF_FACTORS;
+
+/// helper function that unpacks PACKEDFACTORS() blob into SPH_UDF_FACTORS structure
+/// MUST be in sync with PackFactors() method in sphinxsearch.cpp
+/// returns 0 on success
+/// returns an error code on error
+int sphinx_unpack_factors ( const unsigned int * in, SPH_UDF_FACTORS * out )
+{
+	const unsigned int * pack = in;
+	SPH_UDF_FIELD_FACTORS * f;
+	SPH_UDF_TERM_FACTORS * t;
+	int i, size;
+
+	if ( !in )
+		return 1;
+
+	// extract size, extract document-level factors
+	size = *in++;
+	out->doc_bm25 = *in++;
+	out->doc_bm25a = (float)*in++;
+	out->matched_fields = *in++;
+	out->doc_word_count = *in++;
+	out->num_fields = *in++;
+
+	// extract field-level factors
+	if ( out->num_fields > SPH_UDF_MAX_FIELD_FACTORS )
+		return 1;
+	for ( i=0; i<out->num_fields; i++ )
+	{
+		out->field[i].hit_count = *in++;
+		if ( !out->field[i].hit_count )
+			continue;
+
+		f = &(out->field[i]);
+		f->id = *in++;
+		f->lcs = *in++;
+		f->word_count = *in++;
+		f->tf_idf = (float)*in++;
+		f->min_idf = (float)*in++;
+		f->max_idf = (float)*in++;
+		f->sum_idf = (float)*in++;
+		f->min_hit_pos = (int)*in++;
+		f->min_best_span_pos = (int)*in++;
+		f->exact_hit = *in++;
+		f->max_window_hits = (int)*in++;
+	}
+
+	// extract term-level factors
+	out->max_uniq_qpos = *in++;
+	if ( out->max_uniq_qpos > SPH_UDF_MAX_TERM_FACTORS )
+		return 1;
+	for ( i=0; i<out->max_uniq_qpos; i++ )
+	{
+		t = &(out->term[i]);
+		t->keyword_mask = *in++;
+		if ( t->keyword_mask )
+		{
+			t->id = *in++;
+			t->tf = (int)*in++;
+			t->idf = (float)*in++;
+		}
+	}
+
+	// do a safety check, and return
+	return ( size!=(in-pack)*sizeof(unsigned int) ) ? 1 : 0;
+}
+
 #endif // _sphinxudf_
 
 //

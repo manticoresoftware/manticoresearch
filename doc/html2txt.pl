3a37a7f1c4375aaffd32edfcd243e91837794946
shodan <shodan@406a0c4d-033a-0410-8de8-e80135713968>
2013-08-25 11:40:40 +0000

    documented new ranking factors, IDF flags, added some tables support to html2txt
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@4112 406a0c4d-033a-0410-8de8-e80135713968

--- doc/html2txt.pl
+++ doc/html2txt.pl
@@ -65,16 +65,111 @@ $red = "";
 $li = 0;
 $pre = 0;
 
+sub max ($$) { $_[$_[0] < $_[1]] }
+
+sub pad
+{
+	my ($s, $l) = @_;
+	$s .= " " x ( $l - length($s) );
+	return $s;
+}
+
 while ( $html =~ s/^(.*?)<([\/]*\w+)(\s+.*?)*>//ms )
 {
 	$text = $1;
 	$tag = lc $2;
 	$attrs = lc $3;
 
+	if ( $tag eq "table" )
+	{
+		@table = ();
+		@row = ();
+		next;
+	}
+	if ( $tag eq "/table" )
+	{
+		# compute column lengths
+		@lens = ();
+		@wlens = ();
+		foreach $row ( @table )
+		{
+			$n = 0;
+			foreach $col ( @{$row} ) { $lens[$n++] = max($lens[$n], length($col)); }
+		}
+		$len = 0;
+		for $l ( @lens ) { $len += $l; }
+
+		$k = ( $Text::Wrap::columns - 2 - 2*length(@lens) ) / $len;
+		$save = $Text::Wrap::columns;
+
+		for ( $n=0; $n<=$#lens; $n++ )
+		{
+			$clen = $lens[$n]; # target column length
+			$clen = int(1+$k*$clen) if ( $clen>10 ); # only wrap columns under 10 chars
+			$lens[$n] = $clen;
+		}
+
+		# wrap and print
+		$sep = "+";
+		for ( $j=0; $j<$n; $j++ )
+		{
+			$sep .= "-" x $lens[$j] . "--+";
+		}
+		$sep .= "\n";
+
+		$res .= $sep;
+		$r = 0;
+		foreach $row ( @table )
+		{
+			$n = 0;
+			@rowtext = (); # 1st index text lines, 2nd index columns
+			$maxline = 0;
+			foreach $col ( @{$row} )
+			{
+				$Text::Wrap::columns = $lens[$n] + 1;
+				@clines = split /\n/, wrap ( "", "", $col );
+				$rline = 0;
+				for $cline ( @clines )
+				{
+					$rowtext[$rline][$n] = $cline;
+					$rline++;
+					$maxline = max($maxline, $rline);
+				}
+				$n++;
+			}
+			for ( $i=0; $i<$maxline; $i++ )
+			{
+				$res .= "|";
+				for ( $j=0; $j<$n; $j++ )
+				{
+					$res .= " " . pad ( $rowtext[$i][$j], $lens[$j] ) . " |";
+				}
+				$res .= "\n";
+			}
+			$res .= $sep if ( ++$r==1 );
+		}
+		$res .= $sep;
+
+		$Text::Wrap::columns = $save;
+		next;
+	}
+	if ( in ( [ "/th", "/td" ], $tag ) )
+	{
+		push ( @row, $text );
+		next;
+	}
+	if ( $tag eq "/tr" )
+	{
+		push @table, [@row];
+		@row = ();
+		next;
+	}
+
 	$acc .= $text;
 
 	next if ( in ( [ "a","/a","i","/i","p","code","/code","b","/b","span","/span",
-		"strong","/strong","br","em","/em" ], $tag ) );
+		"strong","/strong","br","em","/em", "colgroup", "/colgroup", "col", "/col",
+		"thead", "/thead", "tr", "th", "tbody", "/tbody", "td" ], $tag ) );
 
 	if ( in ( [ "div","/div","/h1","/h2","/h3","/h4","/h5","/p","hr","h5",
 		"dl","ol","ul","dt","dd","/dl","/ol","/ul","/dt","/dd",
@@ -170,7 +265,7 @@ while ( $html =~ s/^(.*?)<([\/]*\w+)(\s+.*?)*>//ms )
 	if ( $tag eq "h3" ) { $hdr = "-"; next; }
 	if ( $tag eq "h4" ) { $hdr = ""; next; }
 
-	die ( "unknown tag='$tag' attrs='$attrs'\n" );
+	die ( "unknown tag=[$tag] attrs=[$attrs] text=[$text]\n" );
 }
 
 $res .= "\n\n--eof--\n";

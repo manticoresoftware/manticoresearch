8fd14e72b671d45220a2f99ee7580d1cd8041be0
klirichek <klirichek@406a0c4d-033a-0410-8de8-e80135713968>
2014-02-06 08:07:12 +0000

    fixed cmake config for win
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@4544 406a0c4d-033a-0410-8de8-e80135713968

--- cmake/c_flag_overrides.cmake
+++ cmake/c_flag_overrides.cmake
@@ -4,4 +4,5 @@
         set(CMAKE_C_FLAGS_MINSIZEREL_INIT     "/MT /O1 /Ob1 /D NDEBUG")
         set(CMAKE_C_FLAGS_RELEASE_INIT        "/MT /O2 /Ob2 /D NDEBUG")
         set(CMAKE_C_FLAGS_RELWITHDEBINFO_INIT "/MT /Zi /O2 /Ob1 /D NDEBUG")
+	set(CMAKE_C_STANDARD_LIBRARIES_INIT "%(AdditionalDependencies)")
     endif(MSVC)
--- cmake/cxx_flag_overrides.cmake
+++ cmake/cxx_flag_overrides.cmake
@@ -4,5 +4,5 @@
         set(CMAKE_CXX_FLAGS_MINSIZEREL_INIT     "/MT /O1 /Ob1 /D NDEBUG")
         set(CMAKE_CXX_FLAGS_RELEASE_INIT        "/MT /O2 /Ob2 /D NDEBUG")
         set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "/MT /Zi /O2 /Ob1 /D NDEBUG")
-	set(CMAKE_CXX_STANDARD_LIBRARIES "%(AdditionalDependencies)")
+	set(CMAKE_CXX_STANDARD_LIBRARIES_INIT "%(AdditionalDependencies)")
     endif(MSVC)

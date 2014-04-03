65e163b96f63fa0d6f522dc0646861aa4783e931
tomat <tomat@406a0c4d-033a-0410-8de8-e80135713968>
2014-04-03 13:05:41 +0000

    fixed #1919 indexing tsv source with bitcount attributes added regression to test 68
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@4647 406a0c4d-033a-0410-8de8-e80135713968

--- src/sphinx.cpp
+++ src/sphinx.cpp
@@ -30308,7 +30308,9 @@ bool CSphSource_BaseSV::Setup ( const CSphConfigSection & hSource, FILE * pPipe,
 			if ( pColon )
 			{
 				int iColon = pColon-sColumn.cstr();
-				sColumn.SetBinary ( sColumn.cstr(), iColon );
+				CSphString sTmp;
+				sTmp.SetBinary ( sColumn.cstr(), iColon );
+				sColumn.Swap ( sTmp );
 			}
 
 			// let's handle different char cases

11e8c3fca752e13bb85054d5d735db2a06236fc3
shodan <shodan@406a0c4d-033a-0410-8de8-e80135713968>
2015-02-06 11:33:37 +0000

    and i dub thee 2.3.1-dev
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/trunk@4896 406a0c4d-033a-0410-8de8-e80135713968

--- src/sphinx.h
+++ src/sphinx.h
@@ -199,7 +199,7 @@ inline const	DWORD *	STATIC2DOCINFO ( const DWORD * pAttrs )	{ return STATIC2DOC
 #define SPHINX_TAG "-dev"
 #endif
 
-#define SPHINX_VERSION			"2.2.8" SPHINX_BITS_TAG SPHINX_TAG " (" SPH_SVN_TAGREV ")"
+#define SPHINX_VERSION			"2.3.1" SPHINX_BITS_TAG SPHINX_TAG " (" SPH_SVN_TAGREV ")"
 #define SPHINX_BANNER			"Sphinx " SPHINX_VERSION "\nCopyright (c) 2001-2015, Andrew Aksyonoff\nCopyright (c) 2008-2015, Sphinx Technologies Inc (http://sphinxsearch.com)\n\n"
 #define SPHINX_SEARCHD_PROTO	1
 #define SPHINX_CLIENT_VERSION	1

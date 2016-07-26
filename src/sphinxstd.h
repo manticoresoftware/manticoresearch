0fb3d80ef4b3c4d2022f224783f47ab8ad68fcd4
klirichek <klirichek@sphinxsearch.com>
2016-07-26 19:25:04 +0700

    Thread-safety checks added (backported)
    Thread-safety check macros backported from sphinx3
    (and also applied to some simple cases)
    
    Thread-safety check warnings switched on for clang DEBUG builds (works
    immediately on CLion project on Mac; on Linux google for 'using clang
    on clion')

--- src/sphinxstd.h
+++ src/sphinxstd.h
@@ -2780,15 +2780,78 @@ bool sphIsLtLib();
 
 //////////////////////////////////////////////////////////////////////////
 
+#if defined(__clang__)
+#define THREAD_ANNOTATION_ATTRIBUTE__(x) __attribute__((x))
+#else
+#define THREAD_ANNOTATION_ATTRIBUTE__( x ) // no-op
+#endif
+
+#define CAPABILITY( x ) \
+    THREAD_ANNOTATION_ATTRIBUTE__(capability(x))
+
+#define SCOPED_CAPABILITY \
+    THREAD_ANNOTATION_ATTRIBUTE__(scoped_lockable)
+
+#define GUARDED_BY( x ) \
+    THREAD_ANNOTATION_ATTRIBUTE__(guarded_by(x))
+
+#define PT_GUARDED_BY( x ) \
+    THREAD_ANNOTATION_ATTRIBUTE__(pt_guarded_by(x))
+
+#define ACQUIRED_BEFORE( ... ) \
+    THREAD_ANNOTATION_ATTRIBUTE__(acquired_before(__VA_ARGS__))
+
+#define ACQUIRED_AFTER( ... ) \
+    THREAD_ANNOTATION_ATTRIBUTE__(acquired_after(__VA_ARGS__))
+
+#define REQUIRES( ... ) \
+    THREAD_ANNOTATION_ATTRIBUTE__(requires_capability(__VA_ARGS__))
+
+#define REQUIRES_SHARED( ... ) \
+    THREAD_ANNOTATION_ATTRIBUTE__(requires_shared_capability(__VA_ARGS__))
+
+#define ACQUIRE( ... ) \
+    THREAD_ANNOTATION_ATTRIBUTE__(acquire_capability(__VA_ARGS__))
+
+#define ACQUIRE_SHARED( ... ) \
+    THREAD_ANNOTATION_ATTRIBUTE__(acquire_shared_capability(__VA_ARGS__))
+
+#define RELEASE( ... ) \
+    THREAD_ANNOTATION_ATTRIBUTE__(release_capability(__VA_ARGS__))
+
+#define RELEASE_SHARED( ... ) \
+    THREAD_ANNOTATION_ATTRIBUTE__(release_shared_capability(__VA_ARGS__))
+
+#define TRY_ACQUIRE( ... ) \
+    THREAD_ANNOTATION_ATTRIBUTE__(try_acquire_capability(__VA_ARGS__))
+
+#define TRY_ACQUIRE_SHARED( ... ) \
+    THREAD_ANNOTATION_ATTRIBUTE__(try_acquire_shared_capability(__VA_ARGS__))
+
+#define EXCLUDES( ... ) \
+    THREAD_ANNOTATION_ATTRIBUTE__(locks_excluded(__VA_ARGS__))
+
+#define ASSERT_CAPABILITY( x ) \
+    THREAD_ANNOTATION_ATTRIBUTE__(assert_capability(x))
+
+#define ASSERT_SHARED_CAPABILITY( x ) \
+    THREAD_ANNOTATION_ATTRIBUTE__(assert_shared_capability(x))
+
+#define RETURN_CAPABILITY( x ) \
+    THREAD_ANNOTATION_ATTRIBUTE__(lock_returned(x))
+
+#define NO_THREAD_SAFETY_ANALYSIS \
+    THREAD_ANNOTATION_ATTRIBUTE__(no_thread_safety_analysis)
+
 /// mutex implementation
-class CSphMutex
+class CAPABILITY ( "mutex" ) CSphMutex
 {
 public:
 	CSphMutex ();
 	~CSphMutex ();
 
-	bool Lock ();
-	bool Unlock ();
+	bool Lock () ACQUIRE();
+	bool Unlock () RELEASE();
 	bool TimedLock ( int iMsec );
 
 protected:
@@ -2852,18 +2915,18 @@ protected:
 
 /// scoped mutex lock
 template < typename T >
-class CSphScopedLock : ISphNoncopyable
+class SCOPED_CAPABILITY CSphScopedLock : ISphNoncopyable
 {
 public:
 	/// lock on creation
-	explicit CSphScopedLock ( T & tMutex )
+	explicit CSphScopedLock ( T & tMutex ) ACQUIRE( tMutex )
 		: m_tMutexRef ( tMutex )
 	{
 		m_tMutexRef.Lock();
 	}
 
 	/// unlock on going out of scope
-	~CSphScopedLock ()
+	~CSphScopedLock () RELEASE()
 	{
 		m_tMutexRef.Unlock ();
 	}

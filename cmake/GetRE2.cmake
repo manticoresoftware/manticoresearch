9c9165cbe105ca2ad0cf9a29caadf7e2605bf1fd
klirichek <klirichek@sphinxsearch.com>
2017-06-21 12:36:03 +0700

    CMAKE: improved RE2 and Stemmer searching on build
    Main CMakeLits was splitted, two scripts for searchd RE2 and Stemmer
    are extracted into separate files.
    
    LIBS_BUNDLE definition (used mainly in windows) may be used as place
    for RE2 (file master.zip) and stemmer (libstermmer_c.tgz) source
    archives. In this case build process will not try to reach internet
    and get files from there.
    
    In both cases if no file found, it will be taken - re2 from github
    (using master.zip download instead of git cloning, which is
    unnecessary in the case), and stemmer from snowball.tartarus.org

---
+++ cmake/GetRE2.cmake
@@ -0,0 +1,105 @@
+#=============================================================================
+# Copyright 2017 Manticore Software Ltd.
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# This file need to get RE2 library sources
+# First it try 'traditional' way - find RE2 lib.
+# Then (if it is not found) it try to look into ${LIBS_BUNDLE} for file named 'master.zip'
+# It is supposed, that file (if any) contains github's master archive of RE2 sources.
+# If no file found, it will  try to fetch it from github, address
+# https://github.com/manticoresoftware/re2/archive/master.zip
+
+set ( RE2_URL "https://github.com/manticoresoftware/re2/archive/master.zip" )
+mark_as_advanced ( RE2_URL )
+
+set ( WITH_RE2_INCLUDES "" CACHE PATH "path to re2 header files" )
+set ( WITH_RE2_LIBS "" CACHE PATH "path to re2 libraries" )
+set ( WITH_RE2_ROOT "" CACHE PATH "path to the libre2 bundle (where both header and library lives)" )
+mark_as_advanced ( WITH_RE2_INCLUDES )
+mark_as_advanced ( WITH_RE2_LIBS )
+mark_as_advanced ( WITH_RE2_ROOT )
+
+find_package ( RE2 )
+if ( RE2_FOUND )
+	set ( USE_RE2 1 )
+	include_directories ( ${RE2_INCLUDE_DIRS} )
+	# check whether we will build RE2 from sources ourselves
+	if ( RE2_PATH )
+		add_subdirectory ( ${RE2_PATH} )
+		list ( APPEND EXTRA_LIBRARIES RE2 )
+	else ( RE2_PATH )
+		list ( APPEND EXTRA_LIBRARIES ${RE2_LIBRARIES} )
+	endif ( RE2_PATH )
+else ( RE2_FOUND )
+	set ( RE2_BASEDIR "${SPHINXSEARCH_BINARY_DIR}/libre2" )
+	mark_as_advanced ( RE2_BASEDIR )
+	if ( NOT EXISTS "${RE2_BASEDIR}/Makefile" )
+		# check whether we have local copy (to not disturb network)
+		if ( EXISTS "${LIBS_BUNDLE}/master.zip" )
+			message ( STATUS "Unpack RE2 from ${LIBS_BUNDLE}/master.zip" )
+			execute_process (
+					COMMAND "${CMAKE_COMMAND}" -E tar xfz "${LIBS_BUNDLE}/master.zip"
+					WORKING_DIRECTORY "${SPHINXSEARCH_BINARY_DIR}" )
+			# download from github as zip archive
+		else ( EXISTS "${LIBS_BUNDLE}/master.zip" )
+			if ( NOT EXISTS "${SPHINXSEARCH_BINARY_DIR}/master.zip" )
+				message ( STATUS "Downloading RE2" )
+				file ( DOWNLOAD ${RE2_URL} ${SPHINXSEARCH_BINARY_DIR}/master.zip SHOW_PROGRESS )
+			endif ()
+			message ( STATUS "Unpack RE2 from ${SPHINXSEARCH_BINARY_DIR}/master.zip" )
+			execute_process (
+					COMMAND "${CMAKE_COMMAND}" -E tar xfz "${SPHINXSEARCH_BINARY_DIR}/master.zip"
+					WORKING_DIRECTORY "${SPHINXSEARCH_BINARY_DIR}" )
+		endif ( EXISTS "${LIBS_BUNDLE}/master.zip" )
+		file ( RENAME "${SPHINXSEARCH_BINARY_DIR}/re2-master" "${RE2_BASEDIR}" )
+
+		# also backup original Makefile; it is important step!
+		configure_file ( "${RE2_BASEDIR}/Makefile" "${RE2_BASEDIR}/MakefileOrig" @ONLY )
+	endif ( NOT EXISTS "${RE2_BASEDIR}/Makefile" )
+
+	# RE2 sources found. Now patch them, if necessary
+	if ( NOT EXISTS "${RE2_BASEDIR}/is_patched.txt" )
+		set ( PATCH_FILE "${CMAKE_SOURCE_DIR}/libre2/libre2.patch" )
+
+		mark_as_advanced ( PATCH_FILE )
+		message ( STATUS "Patching RE2" )
+		find_package ( Git QUIET )
+		if ( GIT_EXECUTABLE )
+			execute_process ( COMMAND "${GIT_EXECUTABLE}" apply ${PATCH_FILE}
+					WORKING_DIRECTORY "${RE2_BASEDIR}"
+					)
+			file ( WRITE "${RE2_BASEDIR}/is_patched.txt" "ok" )
+			# no git
+		else ()
+			find_program ( PATCH_PROG patch )
+			if ( PATCH_PROG )
+				mark_as_advanced ( PATCH_PROG )
+				execute_process (
+						COMMAND "${PATCH_PROG}" -p1 --binary -i "${PATCH_FILE}"
+						WORKING_DIRECTORY "${RE2_BASEDIR}"
+				)
+				file ( WRITE "${RE2_BASEDIR}/is_patched.txt" "ok" )
+			endif ()
+		endif ()
+	endif ()
+	if ( NOT EXISTS "${RE2_BASEDIR}/is_patched.txt" )
+		message ( ERROR "Couldn't patch RE2 distro. No Git or Patch found" )
+	endif ()
+
+	# copy our CMakeLists there
+	if ( NOT EXISTS "${RE2_BASEDIR}/CMakeLists.txt" )
+		message ( STATUS "${CMAKE_SOURCE_DIR} - source dir" )
+		configure_file ( "${CMAKE_SOURCE_DIR}/libre2/CMakeLists.txt" "${RE2_BASEDIR}/CMakeLists.txt" @ONLY )
+	endif ()
+	set ( USE_RE2 1 )
+	set ( RE2_INCLUDE_DIRS "${RE2_BASEDIR}" )
+	include_directories ( ${RE2_BASEDIR} )
+	add_subdirectory ( ${RE2_BASEDIR} ${RE2_BASEDIR} EXCLUDE_FROM_ALL )
+	list ( APPEND EXTRA_LIBRARIES RE2 )
+endif ( RE2_FOUND )
---
+++ cmake/GetStemmer.cmake
@@ -0,0 +1,68 @@
+#=============================================================================
+# Copyright 2017 Manticore Software Ltd.
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# This file need to get libstemmer sources
+# First it try 'traditional' way - find stemmer package.
+# Then (if it is not found) it try to look into ${LIBS_BUNDLE} for file named 'libstemmer_c.tgz'
+# It is supposed, that file (if any) contains archive from snowball with stemmer's sources.
+# If no file found, it will try to fetch it from
+# http://snowball.tartarus.org/dist/libstemmer_c.tgz
+
+set ( STEMMER_URL "http://snowball.tartarus.org/dist/libstemmer_c.tgz" )
+mark_as_advanced( STEMMER_URL)
+
+find_package ( stemmer )
+if ( STEMMER_FOUND )
+	SET ( USE_LIBSTEMMER 1 )
+	include_directories ( ${STEMMER_INCLUDE_DIR} )
+	if ( STEMMER_INTERNAL )
+		add_subdirectory ( ${CMAKE_SOURCE_DIR}/libstemmer_c )
+		list ( APPEND EXTRA_LIBRARIES stemmer )
+	else ()
+		list ( APPEND EXTRA_LIBRARIES ${STEMMER_LIBRARY} )
+	endif ( STEMMER_INTERNAL )
+else ( STEMMER_FOUND )
+	if ( EXISTS "${LIBS_BUNDLE}/libstemmer_c.tgz" )
+		message ( STATUS "Unpack Stemmer from ${LIBS_BUNDLE}/libstemmer_c.tgz" )
+		execute_process (
+				COMMAND "${CMAKE_COMMAND}" -E tar xfz "${LIBS_BUNDLE}/libstemmer_c.tgz"
+				WORKING_DIRECTORY "${SPHINXSEARCH_BINARY_DIR}" )
+		# download from github as zip archive
+	else ( EXISTS "${LIBS_BUNDLE}/libstemmer_c.tgz" )
+		set ( STEMMER_URL "http://snowball.tartarus.org/dist/libstemmer_c.tgz" )
+		if ( NOT EXISTS "${SPHINXSEARCH_BINARY_DIR}/libstemmer_c.tgz" )
+			message ( STATUS "Downloading Stemmer" )
+			file ( DOWNLOAD ${STEMMER_URL} ${SPHINXSEARCH_BINARY_DIR}/libstemmer_c.tgz SHOW_PROGRESS )
+		endif ()
+		message ( STATUS "Unpack Stemmer from ${SPHINXSEARCH_BINARY_DIR}/libstemmer_c.tgz" )
+		execute_process (
+				COMMAND "${CMAKE_COMMAND}" -E tar xfz "${SPHINXSEARCH_BINARY_DIR}/libstemmer_c.tgz"
+				WORKING_DIRECTORY "${SPHINXSEARCH_BINARY_DIR}" )
+	endif ( EXISTS "${LIBS_BUNDLE}/libstemmer_c.tgz" )
+	set ( STEMMER_BASEDIR "${SPHINXSEARCH_BINARY_DIR}/libstemmer_c" )
+	# copy our CMakeLists there
+	if ( NOT EXISTS "${STEMMER_BASEDIR}/CMakeLists.txt" )
+		configure_file ( "${CMAKE_SOURCE_DIR}/libstemmer_c/CMakeLists.txt" "${STEMMER_BASEDIR}/CMakeLists.txt" @ONLY )
+	endif ()
+	set ( USE_LIBSTEMMER 1 )
+	set ( STEMMER_INCLUDE_DIR "${STEMMER_BASEDIR}/include" )
+	include_directories ( ${STEMMER_BASEDIR}/include )
+	add_subdirectory ( ${STEMMER_BASEDIR} ${STEMMER_BASEDIR} EXCLUDE_FROM_ALL )
+	list ( APPEND EXTRA_LIBRARIES stemmer )
+	if ( NOT EXISTS "${STEMMER_BASEDIR}/CMakeLists.txt" )
+		message ( SEND_ERROR "missing libstemmer sources from libstemmer_c.
+Please download the C version of libstemmer library from
+http://snowball.tartarus.org/ and extract its sources over libstemmer_c/
+subdirectory in order to build Sphinx with libstemmer support. Or
+install the package named like 'libstemmer-dev' using your favorite
+package manager." )
+		unset ( WITH_STEMMER CACHE )
+	endif ()
+ENDIF ( STEMMER_FOUND )

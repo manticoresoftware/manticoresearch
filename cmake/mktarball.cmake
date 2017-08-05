4b914367ba6082ccff2aec971e41150cc36cef16
klirichek <klirichek@sphinxsearch.com>
2017-08-05 17:30:31 +0700

    Use flex/bison generated files in tarball target
    tarball target (invoked by 'make tarball' call) is used to create
    unlinked-from-git sources (in order to distribute to another machihes
    and build it there). (By default we also use flex/bison to immediately generate
    syntax/grammar parsers in build time).

--- cmake/mktarball.cmake
+++ cmake/mktarball.cmake
@@ -6,10 +6,35 @@ set ( PACKDIR "${BINARY_DIR}/sources/${FNAME}" )
 file (COPY ${SOURCE_DIR}/ DESTINATION ${PACKDIR}
 		PATTERN "cmake-*" EXCLUDE
 		PATTERN "build-*" EXCLUDE
+		PATTERN "build" EXCLUDE
 		PATTERN ".*" EXCLUDE
 		)
 configure_file ("${BINARY_DIR}/config/gen_sphinxversion.h"
 		"${PACKDIR}/src/sphinxversion.h" COPYONLY )
+
+macro (packbis src)
+	if ( USE_BISON )
+		configure_file ( "${BINARY_DIR}/config/bis${src}.c"
+				"${PACKDIR}/src/yy${src}.c" COPYONLY )
+		configure_file ( "${BINARY_DIR}/config/bis${src}.h"
+				"${PACKDIR}/src/yy${src}.h" COPYONLY )
+	endif()
+endmacro()
+
+macro( packflex src )
+	if ( USE_FLEX )
+		configure_file ( "${BINARY_DIR}/config/flex${src}.c"
+				"${PACKDIR}/src/ll${src}.c" COPYONLY )
+	endif ()
+endmacro()
+
+packbis ( sphinxexpr )
+packbis ( sphinxjson )
+packflex ( sphinxjson )
+packbis ( sphinxql )
+packflex ( sphinxql )
+packbis ( sphinxquery )
+packbis ( sphinxselect )
 
 execute_process (
 		COMMAND "${CMAKE_COMMAND}" -E tar cfz "${BINARY_DIR}/${FNAME}.tar.gz" "${FNAME}"

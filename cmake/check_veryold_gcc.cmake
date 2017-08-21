e2a7806d7584d51e615e0badf383d2ca776d2f89
klirichek <klirichek@sphinxsearch.com>
2017-08-21 19:55:50 +0700

    gcc minspec bumped to 4.7.2
    That is minimal gcc which available on debian wheezy and can build with
    -std=c++11

--- cmake/check_veryold_gcc.cmake
+++ cmake/check_veryold_gcc.cmake
@@ -2,7 +2,10 @@
 if ( ${CMAKE_COMPILER_IS_GNUCXX} )
 	set ( _CHECK_OLDCOMPILER_SOURCE_CODE "
 	#ifdef __GNUC__
-	#if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ > 3)
+	#define GCC_VERSION (__GNUC__ * 10000 \
+                     + __GNUC_MINOR__ * 100 \
+                     + __GNUC_PATCHLEVEL__)
+	#if GCC_VERSION > 40701
 	int main() {}
 	#else
 	syntax error
@@ -13,6 +16,6 @@ if ( ${CMAKE_COMPILER_IS_GNUCXX} )
 	include ( CheckCXXSourceCompiles )
 	CHECK_CXX_SOURCE_COMPILES ( "${_CHECK_OLDCOMPILER_SOURCE_CODE}" _RESULT )
 	if ( NOT _RESULT )
-		message ( FATAL_ERROR "Gcc version error. Minspec is 3.4" )
+		message ( FATAL_ERROR "Gcc version error. Minspec is 4.7.2" )
 	endif ( NOT _RESULT )
 endif ()

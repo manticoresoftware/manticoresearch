96011d983d7e9147f850fb1870df4918def92b11
eugene <eugene@manticoresearch.com>
2019-02-22 23:34:47 +1000

    #642: fixed

--- src/gtests_text.cpp
+++ src/gtests_text.cpp
@@ -409,6 +409,53 @@ TEST ( Text, expression_parser )
 	SafeDeleteArray ( pRow );
 }
 
+TEST ( Text, string_expressions_parser )
+{
+	CSphSchema tSchema;
+
+	CSphColumnInfo tCol;
+	tCol.m_eAttrType = SPH_ATTR_STRING;
+	tCol.m_sName = "str";
+	tSchema.AddAttr ( tCol, false );
+
+	auto* pRow = new CSphRowitem[tSchema.GetRowSize ()];
+	pRow[0] = 1; // offset to stringpool
+
+	// first byte is skipped zero (we look at offset=1)
+	// then string 8 symbols len, '11111 22', NOT null-terminated
+	const char* sStringPool = "\0\01011 11 2222222";
+
+	CSphMatch tMatch;
+	tMatch.m_uDocID = 123;
+	tMatch.m_iWeight = 456;
+	tMatch.m_pStatic = ( CSphRowitem* )pRow;
+
+	const char * sExpr = "substring_index(str,' ',-1)";
+
+	CSphString sError;
+	CSphScopedPtr<ISphExpr> pExpr ( sphExprParse ( sExpr, tSchema, NULL, NULL, sError, NULL ));
+
+	ASSERT_TRUE ( pExpr.Ptr ()) << sError.cstr ();
+	pExpr->Command ( SPH_EXPR_SET_STRING_POOL, ( void* ) sStringPool );
+
+	auto fRes = pExpr->Eval ( tMatch );
+	auto iRes = pExpr->IntEval ( tMatch );
+	auto iRes64 = pExpr->Int64Eval ( tMatch );
+	const BYTE * sRes = nullptr;
+	auto iFoo = pExpr->StringEval ( tMatch, &sRes );
+	CSphString strRes;
+	strRes.SetBinary ( (const char*) sRes, iFoo );
+	ASSERT_EQ ( fRes, 22.0);
+	ASSERT_EQ ( iRes, 22 );
+	ASSERT_EQ ( iRes64, 22 );
+	ASSERT_STREQ ( strRes.cstr(), "22");
+
+	if ( pExpr->IsDataPtrAttr () )
+		SafeDeleteArray ( sRes );
+
+	SafeDeleteArray ( pRow );
+}
+
 //////////////////////////////////////////////////////////////////////////
 
 TEST ( Text, ArabicStemmer )

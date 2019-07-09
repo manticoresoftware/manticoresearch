9bb630578150fdc4352e71847371356873c163fc
klirichek <alexey@manticoresearch.com>
2019-07-09 23:05:17 +0700

    Fix test of sphinxclient API
    Removed stateful dependency from pre-existed mysql db with some kind
    of initial data

--- api/libsphinxclient/smoke.cmake
+++ api/libsphinxclient/smoke.cmake
@@ -2,16 +2,15 @@
 cmake_minimum_required ( VERSION 3.13 )
 
 set (searchd "${BINDIR}/src/searchd")
-set (test "${CURBINDIR}/testcli")
-set (cmpfile "${CURBINDIR}/smoke_ref.txt")
+set (cmpfile "${SRCDIR}/smoke_ref.txt")
 
 execute_process ( COMMAND ${searchd} -c smoke_test.conf --test OUTPUT_QUIET)
 execute_process ( COMMAND ${CMAKE_COMMAND} -E sleep 1 )
-execute_process ( COMMAND ${test} --smoke --port 10312 OUTPUT_FILE ${cmpfile})
-execute_process ( COMMAND  ${searchd} -c smoke_test.conf --stop OUTPUT_QUIET)
+execute_process ( COMMAND ./testcli --smoke --port 10312 OUTPUT_FILE smoke_ref.txt)
+execute_process ( COMMAND ${searchd} -c smoke_test.conf --stop OUTPUT_QUIET)
 execute_process ( COMMAND ${CMAKE_COMMAND} -E compare_files ${cmpfile} smoke_ref.txt RESULT_VARIABLE res_is_different_from_model )
 
 if ( res_is_different_from_model )
-	execute_process ( COMMAND diff --unified=3 smoke_ref.txt ${cmpfile} RESULT_VARIABLE difres )
+	execute_process ( COMMAND diff --unified=3 ${cmpfile} smoke_ref.txt RESULT_VARIABLE difres )
 	message ( SEND_ERROR "${cmpfile} does not match smoke_ref.txt!" )
 endif ()

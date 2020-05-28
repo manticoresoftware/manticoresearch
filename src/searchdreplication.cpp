319883e1722041f28ff81590f7d2ae8cce2018ac
Alexey N. Vinogradov <alexey@manticoresearch.com>
2020-05-28 17:58:30 +0700

    Revert "fixed #1342 crash of daemon on use replication statements without replication library"
    This reverts commit c0027fed

--- src/searchdreplication.cpp
+++ src/searchdreplication.cpp
@@ -460,23 +460,6 @@ static void Logger_fn ( wsrep_log_level_t eLevel, const char * sMsg )
 	LoggerWrapper ( eLevel, sMsg );
 }
 
-static void WarnReplicationDisabled ( CSphString * pWarning )
-{
-#if !USE_GALERA
-	const char * sWarningReplicationMissed = "binary compiled without replication library support, replication is disabled";
-	if ( pWarning )
-		*pWarning = sWarningReplicationMissed;
-	else
-		sphWarning ( "%s", sWarningReplicationMissed );
-#else
-	const char * sWarningReplicationDisabled = "data_dir option is missing in config or no replication listener is set, replication is disabled";
-	if ( pWarning )
-		*pWarning = sWarningReplicationDisabled;
-	else
-		sphWarning ( "%s", sWarningReplicationDisabled );
-#endif
-}
-
 // commands version (commands these got replicated via Galera)
 // ver 0x104 added docstore from RT index
 static const WORD g_iReplicateCommandVer = 0x104;
@@ -2198,7 +2181,7 @@ static bool ClusterCheckPath ( const CSphString & sPath, const CSphString & sNam
 {
 	if ( !g_bReplicationEnabled )
 	{
-		WarnReplicationDisabled ( &sError );
+		sError.SetSprintf ( "data_dir option is missing in config or no replication listener is set, replication is disabled" );
 		return false;
 	}
 
@@ -2305,10 +2288,6 @@ static void NewClusterClean ( const CSphString & sPath )
 // setup IP, ports and node incoming address
 static void SetListener ( const CSphVector<ListenerDesc_t> & dListeners )
 {
-#if !USE_GALERA
-	return;
-#endif
-
 	bool bGotReplicationPorts = false;
 	ARRAY_FOREACH ( i, dListeners )
 	{
@@ -2365,7 +2344,7 @@ void ReplicationStart ( const CSphConfigSection & hSearchd, const CSphVector<Lis
 	if ( !g_bReplicationEnabled )
 	{
 		if ( GetClustersInt().GetLength() )
-			WarnReplicationDisabled ( nullptr );
+			sphWarning ( "data_dir option is missing in config or no replication listener is set, replication is disabled" );
 		return;
 	}
 
@@ -2493,7 +2472,7 @@ static bool CheckClusterStatement ( const CSphString & sCluster, bool bCheckClus
 {
 	if ( !g_bReplicationEnabled )
 	{
-		WarnReplicationDisabled ( &sError );
+		sError = "data_dir option is missing or no replication listeners configured";
 		return false;
 	}
 

manticoresoftware/manticoresearch

commit 4893f1d4a731829fe948b6b6da121e806d06e211

fixed #1310 missed crash info on searching RT index disk chunk on daemon crash

src/searchd.cpp
@@ -943,7 +943,7 @@ static int		g_iCrashInfoLen = 0;
 static char		g_sMinidump[SPH_TIME_PID_MAX_SIZE] = "";
 #endif
 
-thread_local CrashQuery_t* SphCrashLogger_c::m_pTlsCrashQuery;    // pointer to on-stack instance of this class
+thread_local CrashQuery_t* SphCrashLogger_c::m_pTlsCrashQuery = nullptr;    // pointer to on-stack instance of this class
 
 static CrashQuery_t g_tUnhandled;
 

src/sphinxrt.cpp
@@ -6167,7 +6167,8 @@ void QueryDiskChunks ( const CSphQuery * pQuery,
 				if ( bInterrupt ) // some earlier job met error; abort.
 					return;
 
-				GuardedCrashQuery_t tCrashQueryClean ( CrashQueryGet() ); // clean up TLS for thread in the pool
+				CrashQuery_t tCleanQuery;
+				GuardedCrashQuery_t tCrashQueryClean ( tCleanQuery ); // clean up TLS for thread in the pool
 				CrashQuerySetTop ( &tCrashQuery ); // set crash info container
 
 				auto iThdID = dTlsData.PrepareLocalTlsContext ( iChunk );

commit 1c3e84a36b6f9b9d50f538373e0cea4bb731dfe4

Fix invalid write (mem corruption)
Corruption was introduced by commit 4893f1d4

That fixes #1426

src/searchd.cpp
@@ -943,8 +943,6 @@ static int		g_iCrashInfoLen = 0;
 static char		g_sMinidump[SPH_TIME_PID_MAX_SIZE] = "";
 #endif
 
-thread_local CrashQuery_t* SphCrashLogger_c::m_pTlsCrashQuery = nullptr;    // pointer to on-stack instance of this class
-
 static CrashQuery_t g_tUnhandled;
 
 // lets invalidate pointer when this instance goes out of scope to get immediate crash
@@ -1161,11 +1159,6 @@ void SphCrashLogger_c::SetupTimePID ()
 
 	g_iCrashInfoLen = snprintf ( g_sCrashInfo, SPH_TIME_PID_MAX_SIZE-1,
 		"------- FATAL: CRASH DUMP -------\n[%s] [%5d]\n", sTimeBuf, (int)getpid() );
-}
-
-void SphCrashLogger_c::SetTopQueryTLS ( CrashQuery_t * pQuery )
-{
-	m_pTlsCrashQuery = pQuery;
 }
 
 CrashQuery_t SphCrashLogger_c::GetQuery()

src/sphinxrt.cpp
@@ -6212,7 +6212,6 @@ void QueryDiskChunks ( const CSphQuery * pQuery,
 
 	// store and manage tls stuff
 	Tls_context_c dTlsData ( dSorters, pResult );
-	CrashQuery_t tCrashQuery = CrashQueryGet();
 
 	bool bMtEnabled = dTlsData.IsEnabled ();
 
@@ -6237,14 +6236,13 @@ void QueryDiskChunks ( const CSphQuery * pQuery,
 			// capture iChunk and dWaiter by value
 			// iChunk will provide independent chunk id on each iteration (capturing by ref will mess everything)
 			// dWaiter as smart-pointer will ensure that mutex is not released until all chunks processed.
-			auto fnCalc = [&, iChunk, dWaiter, tCrashQuery] () mutable
+			auto fnCalc = [&, iChunk, dWaiter] () mutable
 			{
 				if ( bInterrupt ) // some earlier job met error; abort.
 					return;
 
 				CrashQuery_t tCleanQuery;
 				GuardedCrashQuery_t tCrashQueryClean ( tCleanQuery ); // clean up TLS for thread in the pool
-				CrashQuerySetTop ( &tCrashQuery ); // set crash info container
 
 				auto iThdID = dTlsData.PrepareLocalTlsContext ( iChunk );
 				auto & dLocalSorters = dTlsData.GetTlsSorters(iThdID);

src/threadutils.cpp
@@ -270,6 +270,8 @@ SphThread_t makeTinyThread ( Handler tHandler, int iNum=0, const char * sName="m
 
 		static void Do ( void * pTr )
 		{
+			CrashQuery_t tQueryTLS;
+			SphCrashLogger_c::SetTopQueryTLS ( &tQueryTLS );
 			auto * pThis = (TinyThread_t *) pTr;
 			szName = pThis->m_szName;
 			iNumber = pThis->m_iNumber;
@@ -901,3 +903,5 @@ void GlobalSchedule ( Threads::Handler dHandler )
 		InitGlobalThreadPool ();
 	dPool->Schedule ( std::move ( dHandler ));
 }
+
+thread_local CrashQuery_t * SphCrashLogger_c::m_pTlsCrashQuery = nullptr;    // pointer to on-stack instance of this class

src/threadutils.h
@@ -201,7 +201,10 @@ public:
 	static void SetLastQuery( const CrashQuery_t& tQuery );
 	static void SetupTimePID();
 	static CrashQuery_t GetQuery();
-	static void SetTopQueryTLS( CrashQuery_t* pQuery );
+	inline static void SetTopQueryTLS( CrashQuery_t* pQuery )
+	{
+		m_pTlsCrashQuery = pQuery;
+	}
 
 	// create thread with crash logging
 	static bool ThreadCreate( SphThread_t* pThread, void ( * pCall )( void* ), void* pArg, bool bDetached = false,
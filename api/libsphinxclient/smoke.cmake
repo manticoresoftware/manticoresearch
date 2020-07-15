edfa6567403ab11039407d70d1460a7e2e29e8af
Alexey N. Vinogradov <alexey@manticoresearch.com>
2020-07-15 21:21:30 +0700

    fix api smoke test

--- api/libsphinxclient/smoke.cmake
+++ api/libsphinxclient/smoke.cmake
@@ -1,12 +1,13 @@
 # this is cross-platform smoke script
 cmake_minimum_required ( VERSION 3.13 )
 
-set (searchd "${BINDIR}/src/searchd")
+set (searchd "${SEARCHD}")
 set (cmpfile "${SRCDIR}/smoke_ref.txt")
+set (testcli "${CLI}")
 
 execute_process ( COMMAND ${searchd} -c smoke_test.conf --test OUTPUT_QUIET)
 execute_process ( COMMAND ${CMAKE_COMMAND} -E sleep 1 )
-execute_process ( COMMAND ./testcli --smoke --port 10312 OUTPUT_FILE smoke_ref.txt)
+execute_process ( COMMAND ${testcli} --smoke --port 10312 OUTPUT_FILE smoke_ref.txt)
 execute_process ( COMMAND ${searchd} -c smoke_test.conf --stop OUTPUT_QUIET)
 execute_process ( COMMAND ${CMAKE_COMMAND} -E compare_files ${cmpfile} smoke_ref.txt RESULT_VARIABLE res_is_different_from_model )
 

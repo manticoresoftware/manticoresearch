eebf6a56c6985c16f60cab9b2d113f1b8a3b7bf1
Alexey N. Vinogradov <alexey@manticoresearch.com>
2020-07-17 13:28:42 +0700

    Add git attributes parsing
    Now build from raw tarball possible with version info.
    Tarball has to be made with 'git archive', or get from
    github/gitlab as 'download sources'.

--- sphinxrev.cmake
+++ sphinxrev.cmake
@@ -1,65 +1,132 @@
 # this cmake file fully replaces all old things like svnxrev, hgxrev, etc scripts.
 cmake_minimum_required ( VERSION 2.8 )
 
-# first try to use binary git
-if ( EXISTS "${SOURCE_DIR}/.git" )
-	find_package ( Git QUIET )
-	if ( GIT_FOUND )
-		execute_process ( COMMAND "${GIT_EXECUTABLE}" log -1 --format=%h
-				WORKING_DIRECTORY "${SOURCE_DIR}"
-				RESULT_VARIABLE res
-				OUTPUT_VARIABLE SPH_GIT_COMMIT_ID
-				ERROR_QUIET
-				OUTPUT_STRIP_TRAILING_WHITESPACE )
-			# it would be --date=format:%y%m%d, but old git on centos doesn't understand it
-		execute_process ( COMMAND "${GIT_EXECUTABLE}" log -1 --date=short --format=%ad
-				WORKING_DIRECTORY "${SOURCE_DIR}"
-				RESULT_VARIABLE res
-				OUTPUT_VARIABLE GIT_TIMESTAMP_ID
-				ERROR_QUIET
-				OUTPUT_STRIP_TRAILING_WHITESPACE )
-		execute_process ( COMMAND "${GIT_EXECUTABLE}" status -s -b
-				WORKING_DIRECTORY "${SOURCE_DIR}"
-				RESULT_VARIABLE res
-				OUTPUT_VARIABLE GIT_BRANCH_ID
-				ERROR_QUIET
-				OUTPUT_STRIP_TRAILING_WHITESPACE )
-		string ( REGEX REPLACE "\n.*$" "" GIT_BRANCH_ID "${GIT_BRANCH_ID}" )
-		string ( REPLACE "## " "" GIT_BRANCH_ID "${GIT_BRANCH_ID}" )
-		set ( GIT_BRANCH_ID "git branch ${GIT_BRANCH_ID}" )
-		string ( REPLACE "-" "" GIT_TIMESTAMP_ID "${GIT_TIMESTAMP_ID}" )
-		string ( SUBSTRING "${GIT_TIMESTAMP_ID}" 2 -1 GIT_TIMESTAMP_ID )
-	endif ( GIT_FOUND )
-endif ()
+# guess version strings from current git repo
+function(guess_from_git)
 
-# Notning found. Try back task - suppose se have pre-generated sphinxversion. Parse it.
-if ( NOT SPH_GIT_COMMIT_ID )
-	if ( EXISTS "${SOURCE_DIR}/src/sphinxversion.h" )
-		FILE ( READ "${SOURCE_DIR}/src/sphinxversion.h" _CONTENT )
+	if (NOT EXISTS "${SOURCE_DIR}/.git")
+		return()
+	endif ()
+
+	find_package(Git QUIET)
+	if (NOT GIT_FOUND)
+		return()
+	endif()
+
+	# extract short has as SPH_GIT_COMMIT_ID
+	execute_process(COMMAND "${GIT_EXECUTABLE}" log -1 --format=%h
+			WORKING_DIRECTORY "${SOURCE_DIR}"
+			RESULT_VARIABLE res
+			OUTPUT_VARIABLE SPH_GIT_COMMIT_ID
+			ERROR_QUIET
+			OUTPUT_STRIP_TRAILING_WHITESPACE)
+	set(SPH_GIT_COMMIT_ID "${SPH_GIT_COMMIT_ID}" PARENT_SCOPE)
+
+	# extract timestamp and make number YYMMDD from it
+	# it would be --date=format:%y%m%d, but old git on centos doesn't understand it
+	execute_process(COMMAND "${GIT_EXECUTABLE}" log -1 --date=short --format=%ad
+			WORKING_DIRECTORY "${SOURCE_DIR}"
+			RESULT_VARIABLE res
+			OUTPUT_VARIABLE GIT_TIMESTAMP_ID
+			ERROR_QUIET
+			OUTPUT_STRIP_TRAILING_WHITESPACE)
+	string(REPLACE "-" "" GIT_TIMESTAMP_ID "${GIT_TIMESTAMP_ID}")
+	string(SUBSTRING "${GIT_TIMESTAMP_ID}" 2 -1 GIT_TIMESTAMP_ID)
+	set(GIT_TIMESTAMP_ID "${GIT_TIMESTAMP_ID}" PARENT_SCOPE)
+
+	# extract branch name (top of 'git status -s -b'), throw out leading '## '
+	execute_process(COMMAND "${GIT_EXECUTABLE}" status -s -b
+			WORKING_DIRECTORY "${SOURCE_DIR}"
+			RESULT_VARIABLE res
+			OUTPUT_VARIABLE GIT_BRANCH_ID
+			ERROR_QUIET
+			OUTPUT_STRIP_TRAILING_WHITESPACE)
+	string(REGEX REPLACE "\n.*$" "" GIT_BRANCH_ID "${GIT_BRANCH_ID}")
+	string(REPLACE "## " "" GIT_BRANCH_ID "${GIT_BRANCH_ID}")
+	set(GIT_BRANCH_ID "git branch ${GIT_BRANCH_ID}" PARENT_SCOPE)
+endfunction()
+
+# guess version strings from template header file (git archive mark it there)
+function(extract_from_git_slug HEADER)
+	if (EXISTS "${HEADER}")
+		FILE(READ "${HEADER}" _CONTENT)
 		# replace lf into ';' (it makes list from the line)
-		STRING ( REGEX REPLACE "\n" ";" _CONTENT "${_CONTENT}" )
-		foreach ( LINE ${_CONTENT} )
-			# match definitions like - #define NAME "VALUE"
-			IF ( "${LINE}" MATCHES "^#define[ \t]+(.*)[ \t]+\"(.*)\"" )
-				set ( ${CMAKE_MATCH_1} "${CMAKE_MATCH_2}" )
+		STRING(REGEX REPLACE "\n" ";" _CONTENT "${_CONTENT}")
+		foreach (LINE ${_CONTENT})
+			# match definitions like - // GIT_*_ID VALUE
+			if ("${LINE}" MATCHES "^//[ \t]+(GIT_.*_ID)[ \t]\"(.*)\"")
+				set(${CMAKE_MATCH_1} "${CMAKE_MATCH_2}")
 			endif ()
 		endforeach ()
-		set (GIT_BRANCH_ID "from tarball")
-	else ()
-		# final flow: check source dir name for matching tarball
-		get_filename_component ( DIR ${SOURCE_DIR} NAME )
-		if ( NOT "${DIR}" MATCHES "^manticore-([0-9]+\\.[0-9]+\\.[0-9]+)-([0-9][0-9][0-9][0-9][0-9][0-9])-([0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]+)-?(.*)?$" )
-			message ( FATAL_ERROR "Git not found, or the sources are not git clone, or not located in the folder originally unpacked from tarball, or not contain pre-created sphinxversion.h header. Please, put this file to your src/ folder manually." )
+		if (GIT_COMMIT_ID STREQUAL "$Format:%h$")
+			return() # no slug
 		endif()
-		set ( VERNUMBERS "${CMAKE_MATCH_1}" )
-		set ( GIT_TIMESTAMP_ID "${CMAKE_MATCH_2}" )
-		set ( SPH_GIT_COMMIT_ID "${CMAKE_MATCH_3}")
-		set ( SPHINX_TAG "${CMAKE_MATCH_4}" )
-		set ( GIT_BRANCH_ID "from the folder '${DIR}'" )
+		# commit id
+		set(SPH_GIT_COMMIT_ID "${GIT_COMMIT_ID}" PARENT_SCOPE)
+		# timestamp
+		string(REPLACE "-" "" GIT_TIMESTAMP_ID "${GIT_TIMESTAMP_ID}")
+		string(SUBSTRING "${GIT_TIMESTAMP_ID}" 2 6 GIT_TIMESTAMP_ID)
+		set(GIT_TIMESTAMP_ID "${GIT_TIMESTAMP_ID}" PARENT_SCOPE)
+		# branch id
+		set(GIT_BRANCH_ID "from tarball" PARENT_SCOPE)
 	endif ()
+endfunction()
+
+# guess version strings from header file (may be pre-generated)
+function (extract_from_header HEADER )
+	if (EXISTS "${HEADER}")
+		FILE(READ "${HEADER}" _CONTENT)
+		# replace lf into ';' (it makes list from the line)
+		STRING(REGEX REPLACE "\n" ";" _CONTENT "${_CONTENT}")
+		foreach (LINE ${_CONTENT})
+			# match definitions like - #define NAME "VALUE"
+			IF ("${LINE}" MATCHES "^#define[ \t]+(.*)[ \t]+\"(.*)\"")
+				set(${CMAKE_MATCH_1} "${CMAKE_MATCH_2}" PARENT_SCOPE)
+			endif ()
+		endforeach ()
+		set(GIT_BRANCH_ID "from tarball" PARENT_SCOPE)
+	endif()
+endfunction()
+
+# guess version strings from the name of the folder (if unpacked from tarball)
+function(guess_from_dir_name SOURCE_DIR)
+	get_filename_component(DIR ${SOURCE_DIR} NAME)
+	if (NOT "${DIR}" MATCHES "^manticore-([0-9]+\\.[0-9]+\\.[0-9]+)-([0-9][0-9][0-9][0-9][0-9][0-9])-([0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]+)-?(.*)?$")
+		return()
+	endif ()
+	set(VERNUMBERS "${CMAKE_MATCH_1}" PARENT_SCOPE)
+	set(GIT_TIMESTAMP_ID "${CMAKE_MATCH_2}" PARENT_SCOPE)
+	set(SPH_GIT_COMMIT_ID "${CMAKE_MATCH_3}" PARENT_SCOPE)
+	set(SPHINX_TAG "${CMAKE_MATCH_4}" PARENT_SCOPE)
+	set(GIT_BRANCH_ID "from the folder '${DIR}'" PARENT_SCOPE)
+endfunction()
+
+# function definitions finished, execution starts from here
+##################################
+
+# first try to use binary git
+guess_from_git()
+
+# 2-nd try - if we build from git archive. Correct hash and date provided then, but no branch
+if (NOT SPH_GIT_COMMIT_ID)
+	extract_from_git_slug("${SOURCE_DIR}/src/sphinxversion.h.in")
 endif ()
 
-# extract version number string from sphinx.h
+# following are secondary legacy guesses
+if ( NOT SPH_GIT_COMMIT_ID )
+	extract_from_header("${SOURCE_DIR}/src/sphinxversion.h")
+endif()
+
+if ( NOT SPH_GIT_COMMIT_ID )
+	guess_from_dir_name ("${SOURCE_DIR}")
+endif ()
+
+# nothing found, bail with error
+if ( NOT SPH_GIT_COMMIT_ID )
+	message(FATAL_ERROR "Git not found, or the sources are not git clone, or not located in the folder originally unpacked from tarball, or not contain pre-created sphinxversion.h header. Please, put this file to your src/ folder manually.")
+endif ()
+
+# extract version number string from sphinxversion.h.in
 if ( NOT VERNUMBERS )
 	FILE ( STRINGS "${SOURCE_DIR}/src/sphinxversion.h.in" _STRINGS LIMIT_COUNT 500
 			REGEX "^#define[ \t]+VERNUMBERS.*" )
@@ -86,10 +153,9 @@ if ( NEED_NEWFILE )
 	message ( STATUS "Version ${VERNUMBERS} ${SPH_GIT_COMMIT_ID}@${GIT_TIMESTAMP_ID}, ${GIT_BRANCH_ID}" )
 	configure_file ( "${VERFILE}1" "${VERFILE}" COPYONLY )
 	file ( REMOVE "${VERFILE}1" )
+	configure_file("${SOURCE_DIR}/dist/CPackOptions.cmake.in" "${BINARY_DIR}/config/CPackOptions.cmake" @ONLY)
 else()
 	message ( STATUS "Version not changed: ${VERNUMBERS} ${SPH_GIT_COMMIT_ID}@${GIT_TIMESTAMP_ID}, ${GIT_BRANCH_ID}" )
 endif()
-
-configure_file ( "${SOURCE_DIR}/dist/CPackOptions.cmake.in" "${BINARY_DIR}/config/CPackOptions.cmake" @ONLY )
 
 

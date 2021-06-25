4189ab50f366374b041c150012176e43bdb36784
Alexey N. Vinogradov <alexey@manticoresearch.com>
2021-06-25 18:55:51 +0700

    Remove confusing code of columnar retrieving
    That is never used, however it looks like we may build manticore with
    columnar. That is false. (we may build columnar with manticore instead -
     and that is matter of columnar project)

--- cmake/GetColumnar.cmake
+++ cmake/GetColumnar.cmake
@@ -3,13 +3,14 @@ include(update_bundle)
 set (COLUMNAR_ABI 8)
 set (COLUMNAR_REQUIRED_VER 1.${COLUMNAR_ABI} )
 
-# Path COLUMNAR_SRC has to be provided from outside. If it is defined, columnar sources will be added via 'add_subdirectory',
-# as submodule from that folder. Note that path to columnar in that case you have to provide in runtime via env.
+# Note: we don't build, neither link with columnar. Only thing we expect to get is a few interface headers, aka 'columnar_api'.
+# Actual usage of columnar is solely defined by availability of the module named below. That module is build (or not built)
+# separately outside the manticore.
 
-# Variable SKIP_COLUMNAR is set to install only columnar API and build manticore; NOT build columnar itself.
-
-# Otherwise we first try perform find_package(), and if not success - download columnar sources, build them and install
-# into temporary folder which then will be used in find_package() call to locate columnar_api
+# In order to debug columnar, you should, namely, debug columnar. Open columnar's source, provide MANTICORE_LOCATOR there and configure.
+# It will be 'inverted' project with columnar as main code and manticore as helper aside.
+# If you provide locator as 'SOURCE_DIR /path/to/manticore/sources', they will be used inplace, without any copying, and will be available
+# to edit in IDE.
 
 if (WIN32)
 	set (LIB_MANTICORE_COLUMNAR "lib_manticore_columnar.dll")
@@ -17,57 +18,34 @@ else ()
 	set (LIB_MANTICORE_COLUMNAR "lib_manticore_columnar.so")
 endif ()
 
-macro (return_if_columnar_found)
-	if (TARGET columnar::columnar)
-		get_target_property (COLUMNARLIB columnar::columnar LOCATION)
-		get_filename_component (LIB_MANTICORE_COLUMNAR ${COLUMNARLIB} NAME)
-		message (STATUS "Name is ${LIB_MANTICORE_COLUMNAR}")
-		message (STATUS "Location is ${COLUMNARLIB}")
-		trace (columnar::columnar)
-	else()
-		message (STATUS "Location of columnar is not known; you need to provide it manually in env if you want in-place testing possible")
-	endif ()
-	include (FeatureSummary)
-	set_package_properties (columnar PROPERTIES TYPE RUNTIME
+macro (return_if_columnar_api_found)
+	if (TARGET columnar::columnar_api)
+		include (FeatureSummary)
+		set_package_properties (columnar PROPERTIES TYPE RUNTIME
 			DESCRIPTION "column-oriented storage library, aiming to provide decent performance with low memory footprint at big data volume"
 			URL "https://github.com/manticoresoftware/columnar/"
 			)
-	if (TARGET columnar::columnar_api)
 		trace (columnar::columnar_api)
 		return ()
 	endif ()
 endmacro ()
 
-# special CI path. Columnar set this with path to it's sources. We don't need to build it, and also target API assumed available
+# Columnar might be already provided by inverted inclusion - i.e. when sources of manticore included as testing tool into columnar's sources
 if (TARGET columnar::columnar_api)
 	message (STATUS "Columnar is already defined, skip.")
 	return ()
 endif ()
-
-if (COLUMNAR_SRC)
-	set (SKIP_COLUMNAR FALSE)
-	add_subdirectory ("${COLUMNAR_SRC}" "${MANTICORE_BINARY_DIR}/columnar-build")
-	message (STATUS "Name is ${LIB_MANTICORE_COLUMNAR}, build as sub-project.")
-	message (STATUS "Location is unknown, somewhere in subdir of columnar-build")
-	trace_internal (columnar::columnar)
-	include (FeatureSummary)
-	set_package_properties (columnar PROPERTIES TYPE RUNTIME
-			DESCRIPTION "column-oriented storage library, aiming to provide decent performance with low memory footprint at big data volume"
-			URL "https://github.com/manticoresoftware/columnar/"
-			)
-	return()
-endif()
 
 # set current path to modules in local usr
 set (COLUMNAR_BUILD "${MANTICORE_BINARY_DIR}/usr")
 list (APPEND CMAKE_PREFIX_PATH "${COLUMNAR_BUILD}")
 message (DEBUG "CMAKE_PREFIX_PATH became ${CMAKE_PREFIX_PATH}")
 
-find_package (columnar ${COLUMNAR_REQUIRED_VER} CONFIG)
-return_if_columnar_found ()
+find_package (columnar ${COLUMNAR_REQUIRED_VER} COMPONENTS columnar_api CONFIG)
+return_if_columnar_api_found ()
 
-# Not found. get columnar src, build columnar_api.
-set (SKIP_COLUMNAR TRUE) # that will cause columnar NOT to build, only build columnar_api
+# Not found. get columnar src, extract columnar_api.
+set (SKIP_COLUMNAR TRUE) # that will cause columnar NOT to build, only columnar_api
 
 if (DEFINED ENV{COLUMNAR_LOCATOR})
 	set (COLUMNAR_LOCATOR $ENV{COLUMNAR_LOCATOR})
@@ -83,5 +61,5 @@ configure_file (${MANTICORE_SOURCE_DIR}/cmake/columnar-imported.cmake.in columna
 execute_process (COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" . WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/columnar-build)
 execute_process (COMMAND ${CMAKE_COMMAND} --build . WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/columnar-build)
 
-find_package (columnar ${COLUMNAR_REQUIRED_VER} REQUIRED CONFIG)
-return_if_columnar_found ()
+find_package (columnar ${COLUMNAR_REQUIRED_VER} REQUIRED COMPONENTS columnar_api CONFIG)
+return_if_columnar_api_found ()

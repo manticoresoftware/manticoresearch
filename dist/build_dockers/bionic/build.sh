6ee65cf931d78f7042ae0d64b46eeb20833bc880
Sergey Nikolaev <prostuda@academ.org>
2022-04-21 11:55:40 +0200

    migration from clang 12 to clang 13 for Focal, Bullseye, Bionic, Buster and Stretch

--- dist/build_dockers/bionic/build.sh
+++ dist/build_dockers/bionic/build.sh
@@ -7,7 +7,7 @@
 . build_clang.sh
 docker tag ${distr}_cmake:320 registry.gitlab.com/manticoresearch/dev/${distr}_cmake:320
 docker tag ${distr}_cmake:320 registry.gitlab.com/manticoresearch/dev/${distr}_cmake:latest
-docker tag ${distr}_clang:12 registry.gitlab.com/manticoresearch/dev/${distr}_clang:12
+docker tag ${distr}_clang:13 registry.gitlab.com/manticoresearch/dev/${distr}_clang:13
 echo "docker push registry.gitlab.com/manticoresearch/dev/${distr}_cmake:320"
 echo "docker push registry.gitlab.com/manticoresearch/dev/${distr}_cmake:latest"
-echo "docker push registry.gitlab.com/manticoresearch/dev/${distr}_clang:12"
+echo "docker push registry.gitlab.com/manticoresearch/dev/${distr}_clang:13"
--- dist/build_dockers/buster/build_clang.sh
+++ dist/build_dockers/buster/build_clang.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 . distr.txt
-docker build -t ${distr}_clang:12 clang_12
+docker build -t ${distr}_clang:13 clang_13

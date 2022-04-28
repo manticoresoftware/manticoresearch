manticoresoftware/manticoresearch

commit 7667fe0ffce4280aab281a7820262050ee642327

zero-copy http
When parsing http requests we make copy of incoming data, because
parsing implies intrusion (that is - we place \0-terminator in some
places in order to make json parser happy).

However actually we don't need that arrived data untouched, since it's
only purpose is to be parsed and processed. So, let's use directly
incoming buffer and avoid copying it.

Some corner cases should be traced:

1. URL-decoding. We sometimes need both url-decoded and raw line.
However these chunks are tiny, and ok to copy.
2. Text of data to be exposed for 'show threads'. Here is also, that is
no reason to keep huge amount of data; but some arbitrary chunk, say,
100 chars, should be enough. That is ok to copy such tiny chunk.

What is done here:

Perform parsing of incoming data directly in network buffer instead of
redundand copying it.

Note: for bulk we acquire up to first 100 bytes into task info (that is
line exposed in the 'show threads').

That closes #2677

src/searchd.cpp
@@ -1459,6 +1459,11 @@ void ISphOutputBuffer::SendBytes ( const CSphString& sStr )
 	SendBytes ( sStr.cstr(), sStr.Length() );
 }
 
+void ISphOutputBuffer::SendBytes ( const Str_t& sStr )
+{
+	m_dBuf.Append ( sStr );
+}
+
 void ISphOutputBuffer::SendBytes ( const VecTraits_T<BYTE> & dBuf )
 {
 	m_dBuf.Append ( dBuf );
@@ -9763,7 +9768,7 @@ static void SendMysqlPercolateReply ( RowBuffer_i & tOut, const CPqResult & tRes
 				}
 			}
 
-			tOut.PutString ( sDocs.cstr () );
+			tOut.PutString ( sDocs );
 			sDocs.Clear ();
 		}
 		if ( bQuery )
@@ -11593,7 +11598,7 @@ void HandleMysqlCallSuggest ( RowBuffer_i & tOut, SqlStmt_t & tStmt, bool bQuery
 			for ( auto &dMatched : tRes.m_dMatched )
 				sBuf.Appendf ( "%d", dMatched.m_iDocs );
 			tOut.PutString ( "docs" );
-			tOut.PutString ( sBuf.cstr () );
+			tOut.PutString ( sBuf );
 			tOut.Commit ();
 		}
 	} else
@@ -12193,7 +12198,7 @@ void HandleMysqlShowThreads ( RowBuffer_i & tOut, const SqlStmt_t * pStmt )
 			continue;
 		tOut.PutNumAsString ( dThd.m_iThreadID );
 		tOut.PutString ( dThd.m_sThreadName );
-		tOut.PutString ( dThd.m_sProto.cstr() );
+		tOut.PutString ( dThd.m_sProto );
 		tOut.PutString ( TaskStateName ( dThd.m_eTaskState ) );
 		tOut.PutString ( dThd.m_sClientName ); // Host
 		tOut.PutNumAsString ( dThd.m_iConnID ); // ConnID
@@ -12218,7 +12223,7 @@ void HandleMysqlShowThreads ( RowBuffer_i & tOut, const SqlStmt_t * pStmt )
 		}
 
 		if ( bAll )
-			tOut.PutString ( dThd.m_sChain.cstr () ); // Chain
+			tOut.PutString ( dThd.m_sChain ); // Chain
 		auto tInfo = FormatInfo ( dThd, eFmt, tBuf );
 		tOut.PutString ( tInfo.first, Min ( tInfo.second, iCols ) ); // Info m_pTaskInfo
 		if ( !tOut.Commit () )
@@ -14105,7 +14110,7 @@ void HandleSelectFiles ( RowBuffer_i & tOut, const SqlStmt_t * pStmt )
 	if ( sFormat!="external" )
 		ARRAY_CONSTFOREACH( i, dFiles )
 		{
-			tOut.PutString ( dFiles[i].cstr () );
+			tOut.PutString ( dFiles[i] );
 			tOut.PutNumAsString ( sphGetFileSize ( dFiles[i], nullptr ) );
 			if ( !tOut.Commit () )
 				return;
@@ -14116,7 +14121,7 @@ void HandleSelectFiles ( RowBuffer_i & tOut, const SqlStmt_t * pStmt )
 		dExt.Uniq ();
 		ARRAY_CONSTFOREACH( i, dExt )
 		{
-			tOut.PutString ( dExt[i].cstr () );
+			tOut.PutString ( dExt[i] );
 			tOut.PutNumAsString ( sphGetFileSize ( dExt[i], nullptr ) );
 			if ( !tOut.Commit () )
 				return;
@@ -15634,7 +15639,7 @@ static void HandleMysqlShowPlan ( RowBuffer_i & tOut, const QueryProfile_c & p, 
 	tOut.PutString ( "transformed_tree" );
 	StringBuilder_c sPlan;
 	sph::RenderBsonPlan ( sPlan, bson::MakeHandle ( p.m_dPlan ), bDot );
-	tOut.PutString ( sPlan.cstr (), sPlan.GetLength() );
+	tOut.PutString ( sPlan );
 	tOut.Commit();
 
 	tOut.Eof ( bMoreResultsFollow );
@@ -15766,7 +15771,7 @@ void HandleMysqlExplain ( RowBuffer_i & tOut, const SqlStmt_t & tStmt, bool bDot
 	tOut.HeadEnd ();
 
 	tOut.PutString ( "transformed_tree" );
-	tOut.PutString ( sRes.cstr(), sRes.GetLength() );
+	tOut.PutString ( sRes );
 	tOut.Commit();
 	tOut.Eof ();
 }
@@ -16415,11 +16420,8 @@ void HandleCommandJson ( ISphOutputBuffer & tOut, WORD uVer, InputBuffer_c & tRe
 	CSphString sEndpoint = tReq.GetString ();
 	CSphString sCommand = tReq.GetString ();
 	
-	ESphHttpEndpoint eEndpoint = sphStrToHttpEndpoint ( sEndpoint );
-
 	CSphVector<BYTE> dResult;
-	SmallStringHash_T<CSphString> tOptions;
-	sphProcessHttpQueryNoResponce ( eEndpoint, sCommand.cstr(), tOptions, dResult );
+	sphProcessHttpQueryNoResponce ( sEndpoint, sCommand, dResult );
 
 	auto tReply = APIAnswer ( tOut, VER_COMMAND_JSON );
 	tOut.SendString ( sEndpoint.cstr() );

src/dynamic_idx.cpp
@@ -265,17 +265,13 @@ public:
 	void PutArray ( const void * pBlob, int iLen, bool bSendEmpty ) override {}
 
 	// pack zero-terminated string (or "" if it is zero itself)
-	void PutString ( const char * sMsg, int iMaxLen ) override
+	void PutString ( const char * sMsg, int iLen ) override
 	{
 		if ( !m_pMatch )
 			return;
-		int iLen = ( sMsg && *sMsg ) ? (int) strlen ( sMsg ) : 0;
 
 		if ( !sMsg )
 			sMsg = "";
-
-		if ( iMaxLen>=0 )
-			iLen = Min ( iLen, iMaxLen );
 
 		auto & tCol = GetNextCol ();
 		auto & tMatch = *m_pMatch;
@@ -473,7 +469,7 @@ public:
 	void PutNumAsString ( int iVal ) override {}
 	void PutNumAsString ( DWORD uVal ) override {}
 	void PutArray ( const void * pBlob, int iLen, bool bSendEmpty ) override {}
-	void PutString ( const char * sMsg, int iMaxLen ) override {}
+	void PutString ( const char * sMsg, int iLen ) override {}
 	void PutMicrosec ( int64_t iUsec ) override {}
 	void PutNULL () override {}
 	bool Commit() override { return false;}

src/netreceive_ql.cpp
@@ -446,15 +446,10 @@ public:
 	}
 
 	// pack zero-terminated string (or "" if it is zero itself)
-	void PutString ( const char * sMsg, int iMaxLen=-1 ) override
+	void PutString ( const char * sMsg, int iLen ) override
 	{
-		int iLen = ( sMsg && *sMsg ) ? ( int ) strlen ( sMsg ) : 0;
-
 		if (!sMsg)
 			sMsg = "";
-
-		if ( iMaxLen>=0 )
-			iLen = Min ( iLen, iMaxLen );
 
 		PutArray ( sMsg, iLen, false );
 	}

src/searchdaemon.h
@@ -358,6 +358,7 @@ public:
 	void		SendBytes ( const void * pBuf, int iLen );	///< (was) protected to avoid network-vs-host order bugs
 	void		SendBytes ( const char * pBuf );    // used strlen() to get length
 	void		SendBytes ( const CSphString& sStr );    // used strlen() to get length
+	void		SendBytes ( const Str_t& sStr );
 	void		SendBytes ( const VecTraits_T<BYTE>& dBuf );
 	void		SendBytes ( const StringBuilder_c& dBuf );
 	void		SendBytes ( ByteBlob_t dData );
@@ -1347,24 +1348,6 @@ enum ESphHttpStatus
 	SPH_HTTP_STATUS_TOTAL
 };
 
-enum ESphHttpEndpoint
-{
-	SPH_HTTP_ENDPOINT_INDEX,
-	SPH_HTTP_ENDPOINT_SQL,
-	SPH_HTTP_ENDPOINT_JSON_SEARCH,
-	SPH_HTTP_ENDPOINT_JSON_INDEX,
-	SPH_HTTP_ENDPOINT_JSON_CREATE,
-	SPH_HTTP_ENDPOINT_JSON_INSERT,
-	SPH_HTTP_ENDPOINT_JSON_REPLACE,
-	SPH_HTTP_ENDPOINT_JSON_UPDATE,
-	SPH_HTTP_ENDPOINT_JSON_DELETE,
-	SPH_HTTP_ENDPOINT_JSON_BULK,
-	SPH_HTTP_ENDPOINT_PQ,
-	SPH_HTTP_ENDPOINT_CLI,
-
-	SPH_HTTP_ENDPOINT_TOTAL
-};
-
 bool CheckCommandVersion ( WORD uVer, WORD uDaemonVersion, ISphOutputBuffer & tOut );
 bool IsMaxedOut ();
 bool IsReadOnly ();
@@ -1378,10 +1361,8 @@ bool sphCheckWeCanModify ();
 bool sphCheckWeCanModify ( StmtErrorReporter_i & tOut );
 bool sphCheckWeCanModify ( const char* szStmt, RowBuffer_i& tOut );
 
-bool				sphProcessHttpQueryNoResponce ( ESphHttpEndpoint eEndpoint, const char * sQuery, const SmallStringHash_T<CSphString> & tOptions, CSphVector<BYTE> & dResult );
+bool				sphProcessHttpQueryNoResponce ( const CSphString& sEndpoint, const CSphString& sQuery, CSphVector<BYTE> & dResult );
 void				sphHttpErrorReply ( CSphVector<BYTE> & dData, ESphHttpStatus eCode, const char * szError );
-ESphHttpEndpoint	sphStrToHttpEndpoint ( const CSphString & sEndpoint );
-CSphString			sphHttpEndpointToStr ( ESphHttpEndpoint eEndpoint );
 
 void ExecuteApiCommand ( SearchdCommand_e eCommand, WORD uCommandVer, int iLength, InputBuffer_c & tBuf, ISphOutputBuffer & tOut );
 void HandleCommandPing ( ISphOutputBuffer & tOut, WORD uVer, InputBuffer_c & tReq );
@@ -1506,7 +1487,7 @@ public:
 			sTime.Sprintf ( "%0.2F%%", iVal*10000/iBase );
 		else
 			sTime << "100%";
-		PutString ( sTime.cstr () );
+		PutString ( sTime );
 	}
 
 	virtual void PutNumAsString ( int64_t iVal ) = 0;
@@ -1518,7 +1499,7 @@ public:
 	virtual void PutArray ( const void * pBlob, int iLen, bool bSendEmpty = false ) = 0;
 
 	// pack zero-terminated string (or "" if it is zero itself)
-	virtual void PutString ( const char * sMsg, int iMaxLen=-1 ) = 0;
+	virtual void PutString ( const char * sMsg, int iLen ) = 0;
 
 	virtual void PutMicrosec ( int64_t iUsec ) = 0;
 
@@ -1561,6 +1542,11 @@ public:
 		PutArray ( ( const char * )dData.begin(), dData.GetLength(), bSendEmpty );
 	}
 
+	void PutString ( const char* szMsg )
+	{
+		PutString ( szMsg, szMsg ? (int)strlen ( szMsg ) : 0 );
+	}
+
 	void PutString ( const CSphString & sMsg )
 	{
 		PutString ( sMsg.cstr() );
@@ -1568,7 +1554,7 @@ public:
 
 	void PutString ( const StringBuilder_c & sMsg )
 	{
-		PutString ( sMsg.cstr (), sMsg.GetLength() );
+		PutString ( sMsg.cstr(), sMsg.GetLength() );
 	}
 
 	void PutTimeAsString ( int64_t tmVal )
@@ -1580,14 +1566,14 @@ public:
 		}
 		StringBuilder_c sTime;
 		sTime.Sprintf ("%t", tmVal);
-		PutString ( sTime.cstr() );
+		PutString ( sTime );
 	}
 
 	void PutTimestampAsString ( int64_t tmTimestamp )
 	{
 		StringBuilder_c sTime;
 		sTime.Sprintf ( "%T", tmTimestamp );
-		PutString ( sTime.cstr ());
+		PutString ( sTime );
 	}
 
 	void ErrorEx ( const char * sStmt, const char * sTemplate, ... )
@@ -1638,7 +1624,7 @@ public:
 		va_start ( ap, sFmt );
 		sRight.vSprintf ( sFmt, ap );
 		va_end ( ap );
-		PutString ( sRight.cstr() );
+		PutString ( sRight );
 		return Commit();
 	}
 

src/searchdhttp.h
@@ -32,8 +32,7 @@ public:
 	bool KeepAlive() const;
 
 private:
-	bool ParseList ( Str_t sData );
-	bool IsNdjsonBody() const;
+	void ParseList ( Str_t sData );
 
 	// callbacks
 	int ParserUrl ( Str_t sData );
@@ -58,12 +57,9 @@ private:
 	static int cbMessageStatus ( http_parser* pParser, const char* sAt, size_t iLen );
 
 private:
-	bool m_bKeepAlive { false };
-	const char* m_szError { nullptr };
-	ESphHttpEndpoint m_eEndpoint { SPH_HTTP_ENDPOINT_TOTAL };
+	bool m_bKeepAlive = false;
+	const char* m_szError = nullptr;
 	CSphString m_sEndpoint;
-	CSphString m_sInvalidEndpoint;
-	Str_t m_sRawUrlQuery { dEmptyStr };
 	StringBuilder_c m_sUrl;
 	StringBuilder_c m_sCurField;
 	StringBuilder_c m_sCurValue;

src/sphinxjson.cpp
@@ -1489,6 +1489,11 @@ JsonObj_c::JsonObj_c ( const char * szJson )
 	m_pRoot = cJSON_Parse ( szJson );
 }
 
+JsonObj_c::JsonObj_c ( Str_t sJson )
+{
+	m_pRoot = cJSON_Parse ( sJson.first );
+}
+
 
 JsonObj_c::JsonObj_c ( JsonObj_c && rhs ) noexcept
 	: m_pRoot ( nullptr )

src/sphinxjson.h
@@ -367,6 +367,7 @@ public:
 	explicit		JsonObj_c ( bool bArray = false );
 	explicit		JsonObj_c ( cJSON * pRoot, bool bOwner = true );
 	explicit		JsonObj_c ( const char * szJson );
+	explicit		JsonObj_c ( Str_t sJson );
 					JsonObj_c ( JsonObj_c && rhs ) noexcept;
 					~JsonObj_c();
 

commit ddb6724b6079a36f77727310d31338a24248e5db

fix broken show threads
was broken in 7667fe0f (zero-copy http)

src/searchd.cpp
@@ -12227,7 +12227,9 @@ void HandleMysqlShowThreads ( RowBuffer_i & tOut, const SqlStmt_t * pStmt )
 		if ( bAll )
 			tOut.PutString ( dThd.m_sChain ); // Chain
 		auto tInfo = FormatInfo ( dThd, eFmt, tBuf );
-		tOut.PutString ( tInfo.first, Min ( tInfo.second, iCols ) ); // Info m_pTaskInfo
+		if ( iCols >= 0 && iCols < tInfo.second )
+			tInfo.second = iCols;
+		tOut.PutString ( tInfo.first, tInfo.second ); // Info m_pTaskInfo
 		if ( !tOut.Commit () )
 			break;
 	}
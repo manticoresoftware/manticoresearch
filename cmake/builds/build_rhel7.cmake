2d55807bc7da625dff8a47e9e73b996bdfa7dd39
alexey <klirichek@manticoresearch.com>
2022-05-13 23:19:23 +0700

    cross-friendly rpmbuild

--- cmake/builds/build_rhel7.cmake
+++ cmake/builds/build_rhel7.cmake
@@ -5,4 +5,5 @@
 message ( STATUS "Will create RPM for RedHat/Centos 7" )
 
 set (SYSTEMD_KILLMODE "none")
+set ( RELEASE_DIST ".el7.centos" )
 include ( builds/CommonRpm )
--- cmake/builds/build_rhel8.cmake
+++ cmake/builds/build_rhel8.cmake
@@ -5,4 +5,5 @@
 message ( STATUS "Will create RPM for RedHat/Centos 8" )
 
 set (SYSTEMD_KILLMODE "process")
+set ( RELEASE_DIST ".el8" )
 include ( builds/CommonRpm )

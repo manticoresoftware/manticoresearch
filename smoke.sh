f9ef80d438ce9776d5d825a282938c190dd09689
alexey <klirichek@manticoresearch.com>
2022-10-22 13:04:16 +0700

    refactor valgrind helper
    unify stackmock calculation
    add gcc 9.4 and clang 15.0.2
    modify default flags

--- smoke.sh
+++ smoke.sh
@@ -6,8 +6,10 @@ cd build
 
 export CTEST_CONFIGURATION_TYPE=RelWithDebInfo
 export CTEST_CMAKE_GENERATOR=Ninja
-export XTRA_FLAGS="CMAKE_C_COMPILER=/usr/bin/clang-12;CMAKE_CXX_COMPILER=/usr/bin/clang++-12"
+export NO_STACK_CALCULATION=1
+export MANTICORE_THREADS_EX='2/1+2/1'
+export XTRA_FLAGS="CMAKE_C_COMPILER=/usr/bin/clang-13;CMAKE_CXX_COMPILER=/usr/bin/clang++-13"
 #export MEMORYCHECK_COMMAND_OPTIONS='--tool=memcheck --leak-check=full --track-origins=yes --time-stamp=yes'
-ctest -V -S ../misc/ctest/memcheck.cmake
+time ctest -V -S ../misc/ctest/memcheck.cmake
 #export CTEST_CONFIGURATION_TYPE=Debug
 #ctest -V -S ../misc/ctest/gltest.cmake

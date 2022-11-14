3f021ac6cd557c27f9d7be50205376937bfaff43
alexey <klirichek@manticoresearch.com>
2022-11-14 09:53:23 +0700

    switch back to clang-13
    clang-15 produces new DWARF with unknown DW_FORM_0x25, which breaks
    debugedit tool.

--- dist/build_dockers/cross/linux.cmake
+++ dist/build_dockers/cross/linux.cmake
@@ -22,6 +22,9 @@ set ( CMAKE_CXX_COMPILER ${LLVM}/bin/clang++ )
 
 set ( CMAKE_C_COMPILER_TARGET ${OS_TRIPLE} )
 set ( CMAKE_CXX_COMPILER_TARGET ${OS_TRIPLE} )
+# debug in DWARF-4 is set for fresh clang (>=15), as debugedit in rpm can't understand DWARF-5 flavour
+# build-id set to sha1, as it is mandatory for debuginfo.
+set ( CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -gdwarf-4 -DNDEBUG" CACHE STRING "" )
 set ( CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld -Xlinker --build-id=sha1" )
 set ( CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=lld -Xlinker --build-id=sha1" )
 set ( CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld -Xlinker --build-id=sha1" )

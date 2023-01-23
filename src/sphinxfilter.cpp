eb61315c3fa3a49609cc1d15106dbcddd0a5144b
Ilya Kuznetsov <ilya@manticoresearch.com>
2023-01-23 12:02:11 +0100

    added null check to TryToCreateExpressionFilter

--- src/sphinxfilter.cpp
+++ src/sphinxfilter.cpp
@@ -1262,7 +1262,7 @@ static std::unique_ptr<ISphFilter> CreateFilterExpr ( ISphExpr * _pExpr, const C
 static std::unique_ptr<ISphFilter> TryToCreateExpressionFilter ( CSphRefcountedPtr<ISphExpr> & pExpr, const CSphString & sAttrName, const ISphSchema & tSchema, const CSphFilterSettings & tSettings, const CommonFilterSettings_t & tFixedSettings, ExprParseArgs_t & tExprArgs, CSphString & sError )
 {
 	pExpr = sphExprParse ( sAttrName.cstr(), tSchema, sError, tExprArgs );
-	if ( pExpr->UsesDocstore() )
+	if ( pExpr && pExpr->UsesDocstore() )
 	{
 		sError.SetSprintf ( "unsupported filter on field '%s' (filters are supported only on attributes, not stored fields)", sAttrName.cstr() );
 		pExpr = nullptr;

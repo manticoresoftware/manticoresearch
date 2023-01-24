0c512480542a647cb3c1ed0ef401520ceb99ef65
alexey <klirichek@manticoresearch.com>
2023-01-24 19:26:38 +0700

    memcheck: run with clang-15

--- smoke.sh
+++ smoke.sh
@@ -8,7 +8,7 @@ export CTEST_CONFIGURATION_TYPE=RelWithDebInfo
 export CTEST_CMAKE_GENERATOR=Ninja
 export NO_STACK_CALCULATION=1
 export MANTICORE_THREADS_EX='2/1+2/1'
-export XTRA_FLAGS="CMAKE_C_COMPILER=/usr/bin/clang-13;CMAKE_CXX_COMPILER=/usr/bin/clang++-13"
+export XTRA_FLAGS="CMAKE_C_COMPILER=/usr/bin/clang-15;CMAKE_CXX_COMPILER=/usr/bin/clang++-15"
 #export MEMORYCHECK_COMMAND_OPTIONS='--tool=memcheck --leak-check=full --track-origins=yes --time-stamp=yes'
 time ctest -V -S ../misc/ctest/memcheck.cmake
 #export CTEST_CONFIGURATION_TYPE=Debug

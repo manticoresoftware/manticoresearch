9263f01eb45e12f73800732bb115b849d9d4c098
Sergey Nikolaev <prostuda@academ.org>
2023-01-30 20:47:31 +0700

    Fixed build_macosbrew.cmake, so FULL_SHARE_DIR points to a proper dir

--- cmake/builds/build_macosbrew.cmake
+++ cmake/builds/build_macosbrew.cmake
@@ -4,14 +4,23 @@ message ( STATUS "Installing via Homebrew" )
 
 if (NOT installed)
 	# start with short route - set all paths
+
+	# the following temporary replacement of CMAKE_INSTALL_PREFIX to HOMEBREW_PREFIX is needed mainly
+	# for FULL_SHARE_DIR to become just /opt/homebrew/share/ instead of smth like /opt/homebrew/Cellar/manticoresearch/5.0.3-2023012600-6ffa0d1/share
+	set ( CMAKE_INSTALL_PREFIX_ORIGINAL "${CMAKE_INSTALL_PREFIX}" )
+	set ( CMAKE_INSTALL_PREFIX "$ENV{HOMEBREW_PREFIX}" )
 	include ( GNUInstallDirs )
-	SET ( FULL_SHARE_DIR "${CMAKE_INSTALL_FULL_DATADIR}/manticore" )
-	SET ( LOCALDATADIR "${CMAKE_INSTALL_FULL_LOCALSTATEDIR}/manticore/data" )
+	set ( CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX_ORIGINAL}" )
+
+	set ( FULL_SHARE_DIR "${CMAKE_INSTALL_FULL_DATADIR}/manticore" )
+
 
 	# these guys came from homebrew formula
 	set ( CMAKE_INSTALL_FULL_SYSCONFDIR ${_SYSCONFDIR} )
 	set ( CMAKE_INSTALL_FULL_LOCALSTATEDIR ${_LOCALSTATEDIR} )
 	set ( CMAKE_INSTALL_FULL_RUNSTATEDIR ${_RUNSTATEDIR} )
+	# we have to use the external _LOCALSTATEDIR, otherwise the value may be incorrect (https://manticoresearch.slack.com/archives/C5EEXJG31/p1675084036163049)
+	set ( LOCALDATADIR "${_LOCALSTATEDIR}/manticore/data" )
 	set ( installed ON )
 endif ()
 

628b9ff724bf61eee6a3aa42471e785a409a6127
Sergey Nikolaev <prostuda@academ.org>
2023-01-30 10:33:50 +0700

    updated copyrights

--- src/netfetch.h
+++ src/netfetch.h
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2023, Manticore Software LTD (https://manticoresearch.com)
+// Copyright (c) 2022-2023, Manticore Software LTD (https://manticoresearch.com)
 // All rights reserved
 //
 // This program is free software; you can redistribute it and/or modify

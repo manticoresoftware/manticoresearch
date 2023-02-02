88ffedf8c5d481a38af869669ad1352aacfed232
Klim Todrik <djklim87@gmail.com>
2023-02-02 13:20:12 +0000

    Nsis

--- dist/build_dockers/nsis/deploy.sh
+++ dist/build_dockers/nsis/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build --no-cache -t registry.gitlab.com/manticoresearch/dev/build_nsis:0.2.0 .
-docker push registry.gitlab.com/manticoresearch/dev/build_nsis:0.2.0
+docker build --no-cache -t registry.gitlab.com/manticoresearch/dev/build_nsis:1.0.0 .
+docker push registry.gitlab.com/manticoresearch/dev/build_nsis:1.0.0
--- dist/build_dockers/nsis/nsis_src_parser.sh
+++ dist/build_dockers/nsis/nsis_src_parser.sh
@@ -5,27 +5,34 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 if [ -z "$CI_COMMIT_SHA" ]; then
-    echo -e "${RED}CI_COMMIT_SHA not passed. Exiting$NC \n"
-    exit 1
+  echo -e "${RED}CI_COMMIT_SHA not passed. Exiting$NC \n"
+  exit 1
 fi
+
+NSIS_BUILD_DIR="/tmp/nsis_build"
+
+cp dist/build_dockers/nsis/nsisscript.nsi $NSIS_BUILD_DIR
 
 CI_COMMIT_SHORT_SHA=${CI_COMMIT_SHA:0:9}
 
-echo $CI_COMMIT_SHORT_SHA
+MANTICORE_VERSION=$(cat src/sphinxversion.h.in | grep VERNUMBERS | cut -d'"' -f2)
 
-if [[ -z "$DESTINATION" || $DESTINATION != "release" ]]; then
-    DESTINATION="dev"
+IS_RELEASE_DIGIT=$(echo "$MANTICORE_VERSION" | cut -d. -f3)
+if [[ $((IS_RELEASE_DIGIT % 2)) -eq 0 ]]; then
+  DESTINATION="release_candidate"
+else
+  DESTINATION="dev"
 fi
 
-WIN_REPO="https://repo.manticoresearch.com/repository/manticoresearch_windows/$DESTINATION/x64/"
+echo "Commit: $CI_COMMIT_SHORT_SHA, Version: $MANTICORE_VERSION, Destination $DESTINATION"
 
+WIN_REPO="https://repo.manticoresearch.com/repository/manticoresearch_windows/$DESTINATION/x64/"
 
 REPO_CONTENT=$(curl $WIN_REPO 2>/dev/null)
 DEPS_PATH="deps.txt"
 BUDDY_COMMIT_SHA=$(cat $DEPS_PATH | grep buddy | cut -d" " -f4)
 EXECUTOR_COMMIT_SHA=$(cat $DEPS_PATH | grep executor | cut -d" " -f4)
 MCL_COMMIT_SHA=$(cat $DEPS_PATH | grep mcl | cut -d" " -f4)
-
 
 BUDDY_PACKAGE_NAME=$(echo "$REPO_CONTENT" | grep $BUDDY_COMMIT_SHA | grep buddy | tail -n 1 | cut -d'"' -f2)
 EXECUTOR_PACKAGE_NAME=$(echo "$REPO_CONTENT" | grep $EXECUTOR_COMMIT_SHA | grep executor | tail -n 1 | cut -d'"' -f2)
@@ -45,21 +52,17 @@ fi
 
 echo -e "${GREEN}Save src to temp files$NC"
 
-NSIS_BUILD_DIR="/tmp/nsis_build"
-
-echo -n "$WIN_REPO$MANTICORE_PACKAGE_NAME" > "${NSIS_BUILD_DIR}/manticore_src.txt"
-echo -n "$WIN_REPO$BUDDY_PACKAGE_NAME" > "${NSIS_BUILD_DIR}/buddy_src.txt"
-echo -n "$WIN_REPO$EXECUTOR_PACKAGE_NAME" > "${NSIS_BUILD_DIR}/executor_src.txt"
-echo -n "$WIN_REPO$MCL_PACKAGE_NAME" > "${NSIS_BUILD_DIR}/mcl_src.txt"
+echo -n "$WIN_REPO$MANTICORE_PACKAGE_NAME" >"${NSIS_BUILD_DIR}/manticore_src.txt"
+echo -n "$WIN_REPO$BUDDY_PACKAGE_NAME" >"${NSIS_BUILD_DIR}/buddy_src.txt"
+echo -n "$WIN_REPO$EXECUTOR_PACKAGE_NAME" >"${NSIS_BUILD_DIR}/executor_src.txt"
+echo -n "$WIN_REPO$MCL_PACKAGE_NAME" >"${NSIS_BUILD_DIR}/mcl_src.txt"
 
 echo -e "${GREEN}Run nsis build$NC"
 makensis "$NSIS_BUILD_DIR/nsisscript.nsi"
 
 COMMIT_DATE=$(git show -s --date=short --format=%cd $CI_COMMIT_SHORT_SHA | sed 's/-//g' | cut -c 3-8)
-MANTICORE_VERSION=$(cat src/sphinxversion.h.in | grep VERNUMBERS | cut -d'"' -f2)
 
 if [ -f "$NSIS_BUILD_DIR/manticore_installer.exe" ]; then
   echo "mv $NSIS_BUILD_DIR/manticore_installer.exe /builds/manticoresearch/dev/build/manticore-$MANTICORE_VERSION-$COMMIT_DATE-$CI_COMMIT_SHORT_SHA-x64.exe"
   mv $NSIS_BUILD_DIR/manticore_installer.exe /builds/manticoresearch/dev/build/manticore-$MANTICORE_VERSION-$COMMIT_DATE-$CI_COMMIT_SHORT_SHA-x64.exe
 fi
-

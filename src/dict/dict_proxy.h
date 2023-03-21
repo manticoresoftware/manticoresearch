2b88781edd5efcb9eb87ab1903a84383d9e02bb4
alexey <alexey@manticoresearch.com>
2023-03-21 15:34:39 +0700

    implement proxy dictionary cloning
    By default, we don't clone stateless dictionaries, but share one
    read-only copy instead. However, 'proxy' dictionary by mistake always
    report itself stateless, and so, they also share one instance across
    threads even when backend dictionary is not stateless. That leads to
    race and crash in some circumstances (one revealed - 'exact_words' +
    french stemmer + snippets).
    
    CHANGELOG: eliminated internal race leading to crashes when calling
    SNIPPETS() (github 910).
    
    That fixes #3044

--- src/dict/dict_proxy.h
+++ src/dict/dict_proxy.h
@@ -22,6 +22,12 @@ public:
 		: m_pDict { std::move ( pDict ) }
 	{}
 
+	// that is only one should be overridden
+	SphWordID_t GetWordID ( BYTE* pWord ) override { return m_pDict->GetWordID ( pWord ); }
+	SphWordID_t GetWordIDWithMarkers ( BYTE* pWord ) final { return m_pDict->GetWordIDWithMarkers ( pWord ); }
+	SphWordID_t GetWordIDNonStemmed ( BYTE* pWord ) final { return m_pDict->GetWordIDNonStemmed ( pWord ); }
+	SphWordID_t GetWordID ( const BYTE* pWord, int iLen, bool bFilterStops ) final { return m_pDict->GetWordID ( pWord, iLen, bFilterStops ); }
+	void ApplyStemmers ( BYTE* pWord ) const final { m_pDict->ApplyStemmers ( pWord ); }
 	void LoadStopwords ( const char* sFiles, const TokenizerRefPtr_c& pTokenizer, bool bStripFile ) final { m_pDict->LoadStopwords ( sFiles, pTokenizer, bStripFile ); }
 	void LoadStopwords ( const CSphVector<SphWordID_t>& dStopwords ) final { m_pDict->LoadStopwords ( dStopwords ); }
 	void WriteStopwords ( Writer_i & tWriter ) const final { m_pDict->WriteStopwords ( tWriter ); }
@@ -29,22 +35,40 @@ public:
 	bool LoadWordforms ( const StrVec_t& dFiles, const CSphEmbeddedFiles* pEmbedded, const TokenizerRefPtr_c& pTokenizer, const char* szIndex ) final { return m_pDict->LoadWordforms ( dFiles, pEmbedded, pTokenizer, szIndex ); }
 	void WriteWordforms ( Writer_i & tWriter ) const final { m_pDict->WriteWordforms ( tWriter ); }
 	void WriteWordforms ( JsonEscapedBuilder& tOut ) const final { m_pDict->WriteWordforms ( tOut ); }
+	const CSphWordforms* GetWordforms() final { return m_pDict->GetWordforms(); }
+	void DisableWordforms() final { m_pDict->DisableWordforms(); }
 	int SetMorphology ( const char* szMorph, CSphString& sMessage ) final { return m_pDict->SetMorphology ( szMorph, sMessage ); }
-
-	SphWordID_t GetWordID ( const BYTE* pWord, int iLen, bool bFilterStops ) final { return m_pDict->GetWordID ( pWord, iLen, bFilterStops ); }
-
-	// that is only one should be overridden
-	// SphWordID_t GetWordID ( BYTE* pWord ) override
-
-	SphWordID_t GetWordIDNonStemmed ( BYTE* pWord ) final { return m_pDict->GetWordIDNonStemmed ( pWord ); }
-	void Setup ( const CSphDictSettings& ) final {}
+	bool HasMorphology() const override{ return m_pDict->HasMorphology(); }
+	const CSphString& GetMorphDataFingerprint() const override { return m_pDict->GetMorphDataFingerprint(); }
+	void Setup ( const CSphDictSettings& ) final {} /// notice!
 	const CSphDictSettings& GetSettings() const final { return m_pDict->GetSettings(); }
 	const CSphVector<CSphSavedFile>& GetStopwordsFileInfos() const final { return m_pDict->GetStopwordsFileInfos(); }
 	const CSphVector<CSphSavedFile>& GetWordformsFileInfos() const final { return m_pDict->GetWordformsFileInfos(); }
 	const CSphMultiformContainer* GetMultiWordforms() const final { return m_pDict->GetMultiWordforms(); }
-	const CSphWordforms* GetWordforms() final { return m_pDict->GetWordforms(); }
-
 	bool IsStopWord ( const BYTE* pWord ) const final { return m_pDict->IsStopWord ( pWord ); }
+
+public:
+	void SetSkiplistBlockSize ( int iSize ) final { m_pDict->SetSkiplistBlockSize ( iSize ); }
+	void HitblockBegin() final { m_pDict->HitblockBegin(); }
+	void HitblockPatch ( CSphWordHit* pHits, int iHits ) const final { m_pDict->HitblockPatch ( pHits, iHits ); }
+	const char* HitblockGetKeyword ( SphWordID_t tWordID ) final { return m_pDict->HitblockGetKeyword ( tWordID ); }
+	int HitblockGetMemUse() final { return m_pDict->HitblockGetMemUse(); }
+	void HitblockReset() final { m_pDict->HitblockReset(); }
+
+public:
+	void DictBegin ( CSphAutofile& tTempDict, CSphAutofile& tDict, int iDictLimit ) final { m_pDict->DictBegin ( tTempDict, tDict, iDictLimit ); };
+	void DictEntry ( const DictEntry_t& tEntry ) final { m_pDict->DictEntry ( tEntry ); };
+	void DictEndEntries ( SphOffset_t iDoclistOffset ) final { m_pDict->DictEndEntries ( iDoclistOffset ); };
+	bool DictEnd ( DictHeader_t* pHeader, int iMemLimit, CSphString& sError ) final { return m_pDict->DictEnd ( pHeader, iMemLimit, sError ); };
+	bool DictIsError() const final { return m_pDict->DictIsError(); };
+
+public:
+	bool HasState() const final {
+		return m_pDict->HasState();
+	}
+	DictRefPtr_c Clone() const final {
+		return DictRefPtr_c { new DictProxy_c ( m_pDict->Clone() ) };
+	}
 	uint64_t GetSettingsFNV() const final { return m_pDict->GetSettingsFNV(); }
 
 protected:

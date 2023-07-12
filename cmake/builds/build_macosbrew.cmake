e5d665cec75471c0c33d14e6437a05df4db608c6
alexey <alexey@manticoresearch.com>
2023-07-12 18:26:55 +0700

    refactor recent changes to configuration
    remove HOMEBREW_PREFIX, as it is obvious from build for macos +
    cmake_system_processor.
    remove strange dancing over HARDCODED_PLUGIN_DIR, as it is obvious from
    CMAKE_INSTALL_FULL_LIBDRID + '/manticore'.
    
    That reverts 6d1634c1, 2e4eceb9, and partly 8e81d3e5 as redundant.

--- cmake/builds/build_macosbrew.cmake
+++ cmake/builds/build_macosbrew.cmake
@@ -5,20 +5,30 @@ message ( STATUS "Installing via Homebrew" )
 if (NOT installed)
 	# start with short route - set all paths
 
+	if ( CMAKE_SYSTEM_PROCESSOR STREQUAL arm64 )
+		set ( HOMEBREW_PREFIX "/opt/homebrew")
+	else()
+		set ( HOMEBREW_PREFIX "/usr/local" )
+	endif()
+
 	# the following temporary replacement of CMAKE_INSTALL_PREFIX to HOMEBREW_PREFIX is needed mainly
 	# for FULL_SHARE_DIR to become just /opt/homebrew/share/ instead of smth like /opt/homebrew/Cellar/manticoresearch/5.0.3-2023012600-6ffa0d1/share
 	set ( CMAKE_INSTALL_PREFIX_ORIGINAL "${CMAKE_INSTALL_PREFIX}" )
-	set ( CMAKE_INSTALL_PREFIX "$ENV{HOMEBREW_PREFIX}" )
+	set ( CMAKE_INSTALL_PREFIX "${HOMEBREW_PREFIX}" )
 	include ( GNUInstallDirs )
+	#fixup - CMAKE_INSTALL_DOCDIR is share/doc/MANTICORE, fixup to share/doc/manticore
+	set ( CMAKE_INSTALL_DOCDIR "${CMAKE_INSTALL_DATAROOTDIR}/doc/manticore" )
+	GNUInstallDirs_get_absolute_install_dir ( CMAKE_INSTALL_FULL_DOCDIR CMAKE_INSTALL_DOCDIR DOCDIR )
 	set ( CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX_ORIGINAL}" )
 
 	set ( FULL_SHARE_DIR "${CMAKE_INSTALL_FULL_DATADIR}/manticore" )
 
-	set ( CMAKE_INSTALL_FULL_SYSCONFDIR "$ENV{HOMEBREW_PREFIX}/etc" )
-	set ( CMAKE_INSTALL_FULL_LOCALSTATEDIR "$ENV{HOMEBREW_PREFIX}/var" )
-	set ( CMAKE_INSTALL_FULL_RUNSTATEDIR "$ENV{HOMEBREW_PREFIX}/var/run" )
+	# by default, etc, var, var/run with /opt/homebrew became /etc/opt/homebrew, /var/opt/homebrew and /var/run/opt/homebrew, so need to be fixed
+	set ( CMAKE_INSTALL_FULL_SYSCONFDIR "${HOMEBREW_PREFIX}/etc" )
+	set ( CMAKE_INSTALL_FULL_LOCALSTATEDIR "${HOMEBREW_PREFIX}/var" )
+	set ( CMAKE_INSTALL_FULL_RUNSTATEDIR "${HOMEBREW_PREFIX}/var/run" )
 
-	set ( LOCALDATADIR "$ENV{HOMEBREW_PREFIX}/var/manticore" )
+	set ( LOCALDATADIR "${HOMEBREW_PREFIX}/var/manticore" )
 	set ( SPLIT_SYMBOLS 1 )
 
 	# configure specific stuff
@@ -36,9 +46,7 @@ endif ()
 set ( CPACK_GENERATOR "TGZ" )
 message ( STATUS "Will create ${CPACK_GENERATOR} with build for MacOS" )
 
-#fixup - CMAKE_INSTALL_DOCDIR is share/doc/MANTICORE, fixup to share/doc/manticore
-set ( CMAKE_INSTALL_DOCDIR "${CMAKE_INSTALL_DATAROOTDIR}/doc/manticore" )
-GNUInstallDirs_get_absolute_install_dir ( CMAKE_INSTALL_FULL_DOCDIR CMAKE_INSTALL_DOCDIR DOCDIR )
+
 
 # generate config files
 configure_config ( manticore )

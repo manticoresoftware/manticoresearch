af49a5da3f4aca6d64fddd71cb7636151b375e59
Don Hardman <59067742+donhardman@users.noreply.github.com>
2023-07-19 15:12:15 +0700

    Introduce manticoresearch:test-kit image and use jammy instead of focal (#1254)
    Co-authored-by: Don Hardman <Don Hardman>

--- dist/build_dockers/nsis/nsis_src_parser.sh
+++ dist/build_dockers/nsis/nsis_src_parser.sh
@@ -26,7 +26,7 @@ fi
 
 DEPS_PATH="deps.txt"
 BUDDY_COMMIT_SHA=$(cat $DEPS_PATH | grep buddy | cut -d" " -f4)
-EXECUTOR_COMMIT_SHA=$(cat $DEPS_PATH | grep executor | cut -d" " -f4)
+EXECUTOR_COMMIT_SHA=$(cat $DEPS_PATH | grep executor-win | cut -d" " -f4)
 MCL_COMMIT_SHA=$(cat $DEPS_PATH | grep mcl | cut -d" " -f4)
 
 for DESTINATION in "${DESTINATION_REPOS[@]}"; do
@@ -43,7 +43,7 @@ for DESTINATION in "${DESTINATION_REPOS[@]}"; do
   REPO_CONTENT=$(curl $WIN_REPO 2>/dev/null)
 
   BUDDY_PACKAGE_NAME=$(echo "$REPO_CONTENT" | grep $BUDDY_COMMIT_SHA | grep buddy | tail -n 1 | cut -d'"' -f2)
-  EXECUTOR_PACKAGE_NAME=$(echo "$REPO_CONTENT" | grep $EXECUTOR_COMMIT_SHA | grep executor | tail -n 1 | cut -d'"' -f2)
+  EXECUTOR_PACKAGE_NAME=$(echo "$REPO_CONTENT" | grep $EXECUTOR_COMMIT_SHA | grep executor-win | tail -n 1 | cut -d'"' -f2)
   MCL_PACKAGE_NAME=$(echo "$REPO_CONTENT" | grep $MCL_COMMIT_SHA | grep columnar | grep libs.zip | tail -n 1 | cut -d'"' -f2)
   MANTICORE_PACKAGE_NAME=$(echo "$REPO_CONTENT" | grep $CI_COMMIT_SHORT_SHA | grep main | tail -n 1 | cut -d'"' -f2)
 

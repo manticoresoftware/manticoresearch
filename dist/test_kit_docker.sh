73f7872f380086470413fe6d663bdc437434ec01
Klim Todrik <djklim87@gmail.com>
2023-09-07 11:02:59 +0200

    test

--- dist/test_kit_docker.sh
+++ dist/test_kit_docker.sh
@@ -136,8 +136,8 @@ docker exec manticore-test-kit bash -c \
 docker exec manticore-test-kit bash -c \
 	"php -r \"copy('https://getcomposer.org/installer', 'composer-setup.php');\" && php -r \"if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;\" && php composer-setup.php && php -r \"unlink('composer-setup.php');\" && mv composer.phar /usr/bin/composer || true"
 
-img_url="ghcr.io/manticoresoftware/manticoresearch:test-kit-$BUILD_COMMIT"
-img_url_latest="ghcr.io/manticoresoftware/manticoresearch:test-kit-latest"
+img_url="ghcr.io/djklim87/manticoresearch:test-kit-$BUILD_COMMIT"
+img_url_latest="ghcr.io/djklim87/manticoresearch:test-kit-latest"
 
 # exporting the image, it also squashes all the layers into one
 docker export manticore-test-kit > manticore_test_kit.img

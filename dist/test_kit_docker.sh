12b6cca709a7818dadea1e91a905d9b139e78d4c
Sergey Nikolaev <prostuda@academ.org>
2023-09-26 22:39:40 +0700

    test

--- dist/test_kit_docker.sh
+++ dist/test_kit_docker.sh
@@ -144,7 +144,6 @@ docker exec manticore-test-kit bash -c \
 img_url="ghcr.io/manticoresoftware/manticoresearch:test-kit-$BUILD_COMMIT"
 img_url_latest="ghcr.io/manticoresoftware/manticoresearch:test-kit-latest"
 
-ls -la ..
 # exporting the image, it also squashes all the layers into one
 docker export manticore-test-kit > ../manticore_test_kit.img
 docker import ../manticore_test_kit.img $img_url

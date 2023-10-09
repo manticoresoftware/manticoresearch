8dd9a05515120a2cd2d7ccdf028d58fa136bf6f2
Sergey Nikolaev <prostuda@academ.org>
2023-10-09 21:42:08 +0700

    Fix to prev

--- dist/test_kit_docker.sh
+++ dist/test_kit_docker.sh
@@ -141,7 +141,7 @@ docker exec manticore-test-kit bash -c \
 docker exec manticore-test-kit bash -c \
 	"php -r \"copy('https://getcomposer.org/installer', 'composer-setup.php');\" && php -r \"if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;\" && php composer-setup.php && php -r \"unlink('composer-setup.php');\" && mv composer.phar /usr/bin/composer || true"
 
-[[ $GITHUB_REF_NAME == "master" ]] && branch="" || branch="-$GITHUB_REF_NAME"
+[[ $GITHUB_REF_NAME == "master" ]] && branch="" || branch="-${GITHUB_REF_NAME////-}"
 img_url="ghcr.io/manticoresoftware/manticoresearch:test-kit-${BUILD_COMMIT}${branch}"
 img_url_latest="ghcr.io/manticoresoftware/manticoresearch:test-kit-latest${branch}"
 echo "Going to push to $img_url and $img_url_latest if there's access to the registry"

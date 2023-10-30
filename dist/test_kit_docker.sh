4c8ca8eeed234899973ac18383c58a98e6fb9b37
Klim Todrik <djklim87@gmail.com>
2023-10-30 17:45:23 +0400

    Fix indexer and improve CI builds (#1491)
    * Restore additional searchd params in rpm/deb packages
    * Indexer Unit file fixes

--- dist/test_kit_docker.sh
+++ dist/test_kit_docker.sh
@@ -113,10 +113,10 @@ docker exec manticore-test-kit bash -c \
 docker exec manticore-test-kit bash -c \
 	"php -r \"copy('https://getcomposer.org/installer', 'composer-setup.php');\" && php -r \"if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;\" && php composer-setup.php && php -r \"unlink('composer-setup.php');\" && mv composer.phar /usr/bin/composer || true"
 
-img_url="ghcr.io/manticoresoftware/manticoresearch:test-kit-${BUILD_COMMIT}"
+img_url="ghcr.io/${REPO_OWNER}/manticoresearch:test-kit-${BUILD_COMMIT}"
 images=("$img_url")
 [[ $GITHUB_REF_NAME == "master" ]] \
-  && img_url_latest="ghcr.io/manticoresoftware/manticoresearch:test-kit-latest" \
+  && img_url_latest="ghcr.io/${REPO_OWNER}/manticoresearch:test-kit-latest" \
   && images+=("$img_url_latest") \
   || img_url_latest=""
 

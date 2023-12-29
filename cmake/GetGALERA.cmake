bd49f0c57a3c860786b89f54224f02165626d659
Sergey Nikolaev <prostuda@academ.org>
2023-12-29 22:59:07 +0700

    CI: Github workflow for packaging Galera (#1696)
    NOTE: macos is temporarily commented out and is to be enabled once we can pack macos tarballs

--- cmake/GetGALERA.cmake
+++ cmake/GetGALERA.cmake
@@ -68,3 +68,5 @@ execute_process ( COMMAND ${CMAKE_COMMAND} --build . WORKING_DIRECTORY ${CMAKE_C
 
 find_package ( galera REQUIRED )
 diagst ( galera::galera "library was built and saved" )
+
+# tmp line to trigger CI - v1

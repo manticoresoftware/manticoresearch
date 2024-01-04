4dac7d657de937cfe3c9b8a483baa70feef2e019
Sergey Nikolaev <prostuda@academ.org>
2024-01-04 17:17:33 +0700

    Update galera file paths, so they don't conflict with other galera packages (#1705)

--- galera_packaging/CommonDeb.cmake
+++ galera_packaging/CommonDeb.cmake
@@ -17,15 +17,11 @@ include ( GNUInstallDirs )
 
 install ( TARGETS galera LIBRARY DESTINATION ${CMAKE_INSTALL_DATADIR}/manticore/modules )
 
-#fixup - CMAKE_INSTALL_DOCDIR is share/doc/GALERA, fixup to share/doc/galera-4
-set ( CMAKE_INSTALL_DOCDIR "${CMAKE_INSTALL_DATAROOTDIR}/doc/galera-4" )
+#fixup - CMAKE_INSTALL_DOCDIR is share/doc/GALERA, fixup to share/doc/manticore-galera
+set ( CMAKE_INSTALL_DOCDIR "${CMAKE_INSTALL_DATAROOTDIR}/doc/manticore-galera" )
 GNUInstallDirs_get_absolute_install_dir ( CMAKE_INSTALL_FULL_DOCDIR CMAKE_INSTALL_DOCDIR DOCDIR )
 
-#/usr/share/doc/galera-4/AUTHORS - no changes (AUTHORS)
-#/usr/share/doc/galera-4/README.gz - no changes (README)
-#/usr/share/doc/galera-4/changelog.Debian.gz - no changes (packages/debian/changelog)
-#/usr/share/doc/galera-4/copyright - no changes (debian/copyright)
-# see https://github.com/manticoresoftware/dev/issues/416 for details
+# see https://github.com/manticoresoftware/dev/issues/416#issuecomment-1807560203 for details
 
 install ( FILES AUTHORS README packages/debian/changelog debian/copyright DESTINATION ${CMAKE_INSTALL_DOCDIR} )
 

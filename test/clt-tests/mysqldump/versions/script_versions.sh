b777ef205bc52010ed112f22a666911b0cbee0eb
Pavel_Shilin <work@pavelshilin.com>
2024-01-30 20:39:50 +0100

    Update test-supported-myqldump-versions.rec

--- test/clt-tests/mysqldump/versions/script_versions.sh
+++ test/clt-tests/mysqldump/versions/script_versions.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
 # Версии MariaDB и MySQL
-versions=("mariadb:latest" "mariadb:10.5" "mariadb:10.6")
-
-    # Создание сети
-    docker network create test_network
+versions=("mariadb:10.5" "mariadb:10.6" "mariadb:10.7" "mariadb:10.8" "mariadb:10.9" "mariadb:10.10" "mariadb:10.11" "mariadb:11.0" "mariadb:11.1" "mariadb:11.2" "mariadb:11.3" "mariadb:latest" "mysql:5.6" "mysql:5.7" "mysql:8.0" "mysql:8.2" "mysql:latest")
 
 # Перебор всех версий
 for version in "${versions[@]}"; do
     echo "Тестирование $version"
 
     # Запуск контейнера
+	docker pull -q $version > /dev/null
     docker run -d --network=test_network --name db-test -e MYSQL_ROOT_PASSWORD=my-secret-pw $version
+
 
     # Ожидание запуска
     sleep 15
 
     # Выполнение mysqldump
-    docker exec db-test mariadb -hmanticore -P9306 -e "show status";
+    docker exec db-test mariadb-dump -hmanticore -P9306 manticore > dump.sql
 
     # Проверка на наличие ошибок
-    if [ $? -eq 0 ]; then
-        echo "mysqldump $version выполнен успешно"
+    if [ -s dump.sql ]; then
+        echo "test $version выполнен успешно"
     else
-        echo "Ошибка при выполнении mysqldump для $version"
+        echo "Ошибка при выполнении test для $version"
     fi
 
-    # Остановка и удаление контейнера
-    docker stop db-test
-    docker rm db-test
+    # Остановка и удаление контейнера (заглушить вывод )
+    docker stop db-test > /dev/null
+    docker rm db-test > /dev/null
+	rm dump.sql
 done

ed9182eb888ee08879a227f6a490b5ff7e30b491
Ilya Kuznetsov <ilya@manticoresearch.com>
2024-02-19 20:51:50 +0100

    updated cmake script

--- cmake/GetxxHash.cmake
+++ cmake/GetxxHash.cmake
@@ -2,26 +2,25 @@ cmake_minimum_required ( VERSION 3.17 FATAL_ERROR )
 
 set ( XXH_REPO "https://github.com/manticoresoftware/xxHash" )
 set ( XXH_REV "cb21910" )
-set ( XXH_SRC_MD5 "b490d17568d2aaf9f28da664950c02c3" )
+set ( XXH_SRC_MD5 "b82466bd7156e407d1416d6c1c5d5821" )
 
 set ( XXH_GITHUB "${XXH_REPO}/archive/${XXH_REV}.zip" )
 set ( XXH_BUNDLE "${LIBS_BUNDLE}/xxHash-${XXH_REV}.zip" )
 
 include ( update_bundle )
 
-# try to find quietly (will work most time
-find_package ( xxhash QUIET CONFIG )
-return_if_target_found ( xxhash::xxhash "found ready (no need to build)" )
+# determine destination folder where we expect pre-built xxhash
+find_package ( xxHash QUIET CONFIG )
+return_if_target_found ( xxHash::xxHash "found ready (no need to build)" )
 
 # not found. Populate and prepare sources
-select_nearest_url ( XXH_PLACE xxhash ${XXH_BUNDLE} ${XXH_GITHUB} )
-fetch_and_check ( xxhash ${XXH_PLACE} ${XXH_SRC_MD5} XXH_SRC )
+select_nearest_url ( XXH_PLACE xxHash ${XXH_BUNDLE} ${XXH_GITHUB} )
+fetch_and_check ( xxHash ${XXH_PLACE} ${XXH_SRC_MD5} XXH_SRC )
 
 # build external project
-get_build ( XXH_BUILD xxhash )
-set ( XXHASH_BUILD_XXHSUM OFF CACHE BOOL "" FORCE )
-external_build ( xxhash XXH_SRC XXH_BUILD BUILD_SHARED_LIBS=0 )
+get_build ( XXH_BUILD xxHash )
+external_build ( xxHash XXH_SRC XXH_BUILD )
 
 # now it should find
-find_package ( xxhash REQUIRED CONFIG )
-return_if_target_found ( xxhash::xxhash "was built and saved" )
+find_package ( xxHash REQUIRED CONFIG )
+return_if_target_found ( xxHash::xxHash "was built and saved" )

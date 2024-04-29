ba732c6067ee8a7ff56fb904794ac276705e6b98
Pavel_Shilin <work@pavelshilin.com>
2024-04-29 11:38:16 +0200

    Update generate-records.sh

--- test/clt-tests/mysqldump/scripts/generate-records.sh
+++ test/clt-tests/mysqldump/scripts/generate-records.sh
@@ -4,6 +4,7 @@ srand(1024);
 
 $id=0;
 echo "create table t(id int, f text, a int, b float, j json, m multi, s string, e BOOL, d TIMESTAMP, v MULTI64, fv float_vector);\n";
+echo "insert into t (id) values (0);\n";
 for ($i = 0; $i < $argv[1]; $i++) {
   if ($i % 10000 === 0) {
     if ($i !== 0) {

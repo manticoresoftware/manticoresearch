6e08104b36f28d46b3128fc7547d2985c68fda48
Pavel_Shilin <work@pavelshilin.com>
2024-05-02 01:24:25 +0200

    Added checking for new data and new versions

--- test/clt-tests/mysqldump/scripts/generate-records.sh
+++ test/clt-tests/mysqldump/scripts/generate-records.sh
@@ -2,16 +2,22 @@
 <?php
 srand(1024);
 
-$id=0;
+$id=1;
 echo "create table t(id int, f text, a int, b float, j json, m multi, s string, e BOOL, d TIMESTAMP, v MULTI64, fv float_vector);\n";
+echo "insert into t(id) values (1);\n";
+echo "insert into t(id, f) values (2, '\'');\n";
+echo "insert into t(id, f) values (3, '\"');\n";
+echo "insert into t(id, s) values (4, '\'');\n";
+echo "insert into t(id, s) values (5, '\"');\n";
+echo "insert into t(id, j) values (6, '{\"a\": \"\'\", \"b\": \'\', \"c\": \"\'\"}');\n";
+echo "insert into t(id, j) values (7, '{\"a\": \'\"\', \"b\": \'\"\', \"c\": \'\"\'}');\n";
 
-for ($i = 1; $i < $argv[1]; $i++) {
+$id = 7;
+for ($i = 0; $i < $argv[1]; $i++) {
   if ($i % 10000 === 0) {
     if ($i !== 0) {
       echo ";\n";
     }
-    echo "insert into t (id) values (1);\n";
-	echo "insert into t (id, f, s) values (2, '', '', '');\n";
 
     echo 'insert into t (id, f, a, b, j, m, s, e, d, v, fv) values ';
   } else {

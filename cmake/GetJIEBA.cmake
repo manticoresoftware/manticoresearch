184e73e5e7c9f4f99a42f1c53e1e8709615aa40d
Ilya Kuznetsov <ilya@manticoresearch.com>
2024-09-28 18:54:02 +0200

    updated jieba rev

--- cmake/GetJIEBA.cmake
+++ cmake/GetJIEBA.cmake
@@ -9,8 +9,8 @@
 # See the License for more information.
 #=============================================================================
 set ( JIEBA_REPO "https://github.com/manticoresoftware/cppjieba" )
-set ( JIEBA_REV "25df9a49" )			 
-set ( JIEBA_SRC_MD5 "e876b4cbcde741c9ef7c0ee461dea5e6" )
+set ( JIEBA_REV "c4eda211" )
+set ( JIEBA_SRC_MD5 "28329fd68b17410210369fb43a2127a1" )
 
 set ( JIEBA_GITHUB "${JIEBA_REPO}/archive/${JIEBA_REV}.zip" )
 set ( JIEBA_BUNDLE "${LIBS_BUNDLE}/cppjieba-${XXH_REV}.zip" )

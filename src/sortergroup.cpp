9313163be44ff753d25ca79c2b781f7523ae3179
Aleksey N. Vinogradov <alexey@manticoresearch.com>
2024-10-09 20:15:22 +0700

    minor constexpr distinct

--- src/sortergroup.cpp
+++ src/sortergroup.cpp
@@ -528,8 +528,9 @@ protected:
 		}
 
 		// submit actual distinct value
-		if ( DISTINCT && m_bUpdateDistinct )
-			KBufferGroupSorter::template UpdateDistinct<GROUPED> ( tEntry, uGroupKey );
+		if constexpr ( DISTINCT )
+			if ( m_bUpdateDistinct )
+				KBufferGroupSorter::template UpdateDistinct<GROUPED> ( tEntry, uGroupKey );
 
 		return false; // since it is a dupe
 	}
@@ -590,8 +591,9 @@ protected:
 		{
 			tNew.SetAttr ( m_tLocGroupby, uGroupKey );
 			tNew.SetAttr ( tLocCount, 1 );
-			if ( DISTINCT && m_bUpdateDistinct )
-				tNew.SetAttr ( m_tLocDistinct, 0 );
+			if constexpr ( DISTINCT )
+				if ( m_bUpdateDistinct )
+					tNew.SetAttr ( m_tLocDistinct, 0 );
 
 			if ( pAttr )
 				UpdateGroupbyStr ( tNew, pAttr );
@@ -645,8 +647,9 @@ private:
 			CutWorst ( m_iLimit, true );
 		else
 		{
-			if ( DISTINCT && bCountDistinct )
-				CountDistinct();
+			if constexpr ( DISTINCT )
+				if ( bCountDistinct )
+					CountDistinct();
 
 			CalcAvg ( Avg_e::FINALIZE );
 			SortGroups();
@@ -665,8 +668,9 @@ private:
 	void CutWorst ( int iBound, bool bFinalize=false )
 	{
 		// prepare to partition - finalize distinct, avgs to provide smooth sorting
-		if ( DISTINCT && m_bSortByDistinct )
-			CountDistinct ();
+		if constexpr ( DISTINCT )
+			if ( m_bSortByDistinct )
+				CountDistinct ();
 
 		CalcAvg ( Avg_e::FINALIZE );
 
@@ -693,11 +697,12 @@ private:
 		if ( bFinalize )
 		{
 			SortGroups();
-			if ( DISTINCT && !m_bSortByDistinct ) // since they haven't counted at the top
-			{
-				RebuildHash(); // distinct uses m_hGroup2Match
-				CountDistinct();
-			}
+			if constexpr ( DISTINCT )
+				if ( !m_bSortByDistinct ) // since they haven't counted at the top
+				{
+					RebuildHash(); // distinct uses m_hGroup2Match
+					CountDistinct();
+				}
 		} else
 		{
 			// we've called CalcAvg ( Avg_e::FINALIZE ) before partitioning groups
@@ -1111,8 +1116,9 @@ protected:
 
 
 		// submit actual distinct value in all cases
-		if ( DISTINCT && m_bUpdateDistinct )
-			KBufferGroupSorter::template UpdateDistinct<GROUPED> ( tNew, uGroupKey );
+		if constexpr ( DISTINCT )
+			if ( m_bUpdateDistinct )
+				KBufferGroupSorter::template UpdateDistinct<GROUPED> ( tNew, uGroupKey );
 
 		if constexpr ( NOTIFICATIONS )
 			m_tJustPushed = RowTagged_t ( tNew );
@@ -1246,8 +1252,9 @@ private:
 
 		auto & tHeadMatch = m_dData[iHead];
 		// submit actual distinct value in all cases
-		if ( DISTINCT && m_bUpdateDistinct )
-			KBufferGroupSorter::template UpdateDistinct<GROUPED> ( tEntry, uGroupKey );
+		if constexpr ( DISTINCT )
+			if ( m_bUpdateDistinct )
+				KBufferGroupSorter::template UpdateDistinct<GROUPED> ( tEntry, uGroupKey );
 
 		// update group-wide counters
 		auto & tLocCount = m_tLocCount;
@@ -1843,8 +1850,9 @@ public:
 		if constexpr ( HAS_AGGREGATES )
 			dRhs.UpdateAggregates ( m_tData, false, true );
 
-		if ( !bCopyMeta && DISTINCT )
-			dRhs.UpdateDistinct ( m_tData );
+		if constexpr ( DISTINCT )
+			if ( !bCopyMeta  )
+				dRhs.UpdateDistinct ( m_tData );
 	}
 
 	void SetMerge ( bool bMerge ) override { m_bMerge = bMerge; }

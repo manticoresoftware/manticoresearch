c088d36a8a35a03e8ffebdae46185a4c637d024d
Pavel_Shilin <work@pavelshilin.com>
2024-10-17 09:47:26 +0200

    Updated load_names_attr.php and added tracking of team runtime while optimise is running

--- test/clt-tests/optimisation-and-upgrade/load_names_attr.php
+++ test/clt-tests/optimisation-and-upgrade/load_names_attr.php
@@ -1,48 +1,24 @@
 #!/usr/bin/php
 <?php
-print_r($argv);
+if (count($argv) < 5) die("Usage: ".__FILE__." <batch size> <concurrency> <docs> <multiplier> [rt_mem_limit] [initial_id]\n");
 
-if (count($argv) < 6) {
-    die("Usage: " . __FILE__ . " <batch size> <concurrency> <docs> <multiplier> <num_chunks>\n");
+// Default values for optional parameters
+$rt_mem_limit = $argv[5] ?? null;
+$initial_id = $argv[6] ?? 0;
+
+// Устанавливаем фиксированное значение для генерации случайных чисел
+srand(1);
+
+// This function ensures deterministic random name generation
+function get_name($names, $surnames, $namesCount, $surnamesCount, $index) {
+    return $names[$index % $namesCount] . ' ' . $surnames[$index % $surnamesCount];
 }
-
-$num_chunks = (int)$argv[5];
-$docs_per_chunk = (int)$argv[3] / $num_chunks;
-$c = 1;
-
-for ($chunk = 1; $chunk <= $num_chunks; $chunk++) {
-    $end_id = $c + $docs_per_chunk - 1;
-
-    echo "Inserting chunk $chunk (IDs $c to $end_id)...\n";
-
-    while ($c <= $end_id) {
-        $batch[] = "($c,'".$names[rand(0, $namesCount - 1)].' '.$surnames[rand(0, $surnamesCount - 1)]."', 'a')";
-        $c++;
-        if (floor($c/1000) == $c/1000) echo "\r".(($c - 1)/$docs_per_chunk*100)."%       ";
-        if (count($batch) == $argv[1]) {
-            $batches[] = $query_start.implode(',', $batch);
-            $batch = [];
-        }
-    }
-
-    echo "Flushing RAMCHUNK...\n";
-    if (!mysqli_query($all_links[0], "FLUSH RAMCHUNK;")) {
-        echo "Error during FLUSH RAMCHUNK: " . mysqli_error($all_links[0]) . "\n";
-    }
-}
-/*
-Requires:
-
-wget https://raw.githubusercontent.com/zeraye/names-surnames-list/master/male-names-list.txt > names.txt
-wget https://raw.githubusercontent.com/zeraye/names-surnames-list/master/female-names-list.txt >> names.txt
-wget https://raw.githubusercontent.com/zeraye/names-surnames-list/master/surnames-list.txt > surnames.txt
-*/
 
 // This function waits for an idle mysql connection for the $query, runs it and exits
 function process($query) {
     global $all_links;
     global $requests;
-    foreach ($all_links as $k=>$link) {
+    foreach ($all_links as $k => $link) {
         if (@$requests[$k]) continue;
         mysqli_query($link, $query, MYSQLI_ASYNC);
         @$requests[$k] = microtime(true);
@@ -55,9 +31,9 @@ function process($query) {
         }
         $count = @mysqli_poll($links, $errors, $reject, 0, 1000);
         if ($count > 0) {
-            foreach ($links as $j=>$link) {
+            foreach ($links as $j => $link) {
                 $res = @mysqli_reap_async_query($links[$j]);
-                foreach ($all_links as $i=>$link_orig) if ($all_links[$i] === $links[$j]) break;
+                foreach ($all_links as $i => $link_orig) if ($all_links[$i] === $links[$j]) break;
                 if ($link->error) {
                     echo "ERROR: {$link->error}\n";
                     if (!mysqli_ping($link)) {
@@ -72,7 +48,7 @@ function process($query) {
                     mysqli_free_result($res);
                 }
                 $requests[$i] = microtime(true);
-        mysqli_query($link, $query, MYSQLI_ASYNC); // making next query
+                mysqli_query($link, $query, MYSQLI_ASYNC); // making next query
                 return true;
             }
         };
@@ -82,52 +58,54 @@ function process($query) {
 
 $all_links = [];
 $requests = [];
-$c = 0;
-for ($i=0;$i<$argv[2];$i++) {
-  $m = @mysqli_connect('127.0.0.1', '', '', '', 9306);
-      if (mysqli_connect_error()) die("Cannot connect to Manticore\n");
-      $all_links[] = $m;
-  }
+$c = $initial_id;
+for ($i = 0; $i < $argv[2]; $i++) {
+    $m = @mysqli_connect('127.0.0.1', '', '', '', 9306);
+    if (mysqli_connect_error()) die("Cannot connect to Manticore\n");
+    $all_links[] = $m;
+}
 
 // init
-echo "Dropping existing table if it exists...\n";
-mysqli_query($all_links[0], "drop table if exists name");
 
-echo "Creating table 'name'...\n";
-mysqli_query($all_links[0], "create table name(username text, s string) min_infix_len='2' expand_keywords='1'");
-if (mysqli_errno($all_links[0])) {
-    die("Error creating table: " . mysqli_error($all_links[0]) . "\n");
-}
+// Проверяем начальный ID (шестой параметр скрипта)
+// Таблица создается только если начальный ID равен 1
+if ($argv[6] == 1) {
+    // Инициализируем переменную $create_table_query и добавляем условие для rt_mem_limit
+    $create_table_query = "CREATE TABLE IF NOT EXISTS name(username text, s string) min_infix_len='2' expand_keywords='1'";
 
-$result = mysqli_query($all_links[0], "SHOW TABLES LIKE 'name';");
-if (mysqli_num_rows($result) == 0) {
-    die("Error: Table 'name' does not exist after creation.\n");
+    // Проверяем, был ли передан параметр rt_mem_limit
+    if ($rt_mem_limit) {
+        $create_table_query .= " rt_mem_limit='{$rt_mem_limit}'";
+    }
+
+    // Выполняем запрос на создание таблицы
+    mysqli_query($all_links[0], $create_table_query) or die(mysqli_error($all_links[0]));
 }
-echo "Table 'name' created successfully.\n";
 
 $batch = [];
-$query_start = "insert into name(id, username, s) values ";
-$names = file('./test/clt-tests/optimisation-and-upgrade/names.txt', FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
+$query_start = "INSERT INTO name(id, username, s) VALUES ";
+$names = file('./test/clt-tests/optimisation-and-upgrade/names.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 $namesCount = count($names);
-$surnames = file('./test/clt-tests/optimisation-and-upgrade/surnames.txt', FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
+$surnames = file('./test/clt-tests/optimisation-and-upgrade/surnames.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 $surnamesCount = count($surnames);
 
 echo "preparing...\n";
 $error = false;
-$cache_file_name = '/tmp/'.md5($query_start).'_'.$argv[1].'_'.$argv[3];
-$c = 0;
+$cache_file_name = '/tmp/' . md5($query_start) . '_' . $argv[1] . '_' . $argv[3] . '_from_' . $initial_id;
+$c = $initial_id;
+
 if (!file_exists($cache_file_name)) {
     $batches = [];
-    while ($c < $argv[3]) {
-      $batch[] = "($c,'".$names[rand(0, $namesCount - 1)].' '.$surnames[rand(0, $surnamesCount - 1)]."', 'a')";
-      $c++;
-      if (floor($c/1000) == $c/1000) echo "\r".($c/$argv[3]*100)."%       ";
+    while ($c < $initial_id + $argv[3]) {
+        $batch[] = "($c,'" . get_name($names, $surnames, $namesCount, $surnamesCount, $c) . "', 'a')";
+        $c++;
+        if (floor(($c - $initial_id) / 1000) == ($c - $initial_id) / 1000) echo "\r" . (($c - $initial_id) / $argv[3] * 100) . "%       ";
         if (count($batch) == $argv[1]) {
-          $batches[] = $query_start.implode(',', $batch);
-          $batch = [];
+            $batches[] = $query_start . implode(',', $batch);
+            $batch = [];
         }
     }
-    if ($batch) $batches[] = $query_start.implode(',', $batch);
+    if ($batch) $batches[] = $query_start . implode(',', $batch);
     file_put_contents($cache_file_name, serialize($batches));
 } else {
     echo "found in cache\n";
@@ -135,7 +113,7 @@ if (!file_exists($cache_file_name)) {
 }
 
 $batchesMulti = [];
-for ($n=0;$n<$argv[4];$n++) $batchesMulti = array_merge($batchesMulti, $batches);
+for ($n = 0; $n < $argv[4]; $n++) $batchesMulti = array_merge($batchesMulti, $batches);
 $batches = $batchesMulti;
 
 echo "querying...\n";
@@ -143,16 +121,16 @@ echo "querying...\n";
 $t = microtime(true);
 
 foreach ($batches as $batch) {
-  if (!process($batch)) die("ERROR\n");
+    if (!process($batch)) die("ERROR\n");
 }
 
 // wait until all the workers finish
 do {
-  $links = $errors = $reject = array();
-  foreach ($all_links as $link)  $links[] = $errors[] = $reject[] = $link;
-  $count = @mysqli_poll($links, $errors, $reject, 0, 100);
+    $links = $errors = $reject = array();
+    foreach ($all_links as $link) $links[] = $errors[] = $reject[] = $link;
+    $count = @mysqli_poll($links, $errors, $reject, 0, 100);
 } while (count($all_links) != count($links) + count($errors) + count($reject));
 
 echo "finished inserting\n";
-echo "Total time: ".(microtime(true) - $t)."\n";
-echo round($argv[3] * $argv[4] / (microtime(true) - $t))." docs per sec\n";
+echo "Total time: " . (microtime(true) - $t) . "\n";
+echo round($argv[3] * $argv[4] / (microtime(true) - $t)) . " docs per sec\n";

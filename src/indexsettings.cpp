f331c8d4bb9c11a24ea3fce908d08873a48691f7
Ilya Kuznetsov <ilya@manticoresearch.com>
2024-11-03 19:10:56 +0100

    implemented option jieba_mode='...' for queries (also available for CALL KEYWORDS)

--- src/indexsettings.cpp
+++ src/indexsettings.cpp
@@ -1113,17 +1113,8 @@ bool CSphIndexSettings::Setup ( const CSphConfigSection & hIndex, const char * s
 
 	m_bJiebaHMM = hIndex.GetBool ( "jieba_hmm", true );
 	CSphString sJiebaMode = hIndex.GetStr ( "jieba_mode", "accurate" );
-	if ( sJiebaMode=="accurate" )
-		m_eJiebaMode = JiebaMode_e::ACCURATE;
-	else if ( sJiebaMode=="full" )
-		m_eJiebaMode = JiebaMode_e::FULL;
-	else if ( sJiebaMode=="search" )
-		m_eJiebaMode = JiebaMode_e::SEARCH;
-	else
-	{
-		sError.SetSprintf ( "Unknown jieba_mode value '%s'", sJiebaMode.cstr() );
+	if ( !StrToJiebaMode ( m_eJiebaMode , sJiebaMode, sError ) )
 		return false;
-	}
 
 	if ( m_eJiebaMode==JiebaMode_e::FULL && hIndex.Exists("jieba_hmm")  )
 		sWarning = "jieba_hmm has no effect when jieba_mode=full";
@@ -2956,7 +2947,7 @@ void LoadIndexSettingsJson ( bson::Bson_c tNode, CSphIndexSettings & tSettings )
 	tSettings.m_sHitlessFiles = String ( tNode.ChildByName ( "hitless_files" ) );
 	tSettings.m_eEngine = (AttrEngine_e)Int ( tNode.ChildByName ( "engine" ), (DWORD)AttrEngine_e::DEFAULT );
 	tSettings.m_eDefaultEngine = (AttrEngine_e)Int ( tNode.ChildByName ( "engine_default" ), (DWORD)AttrEngine_e::ROWWISE );
-	tSettings.m_eJiebaMode = (JiebaMode_e)Int ( tNode.ChildByName ( "jieba_mode" ), (DWORD)JiebaMode_e::ACCURATE );
+	tSettings.m_eJiebaMode = (JiebaMode_e)Int ( tNode.ChildByName ( "jieba_mode" ), (DWORD)JiebaMode_e::DEFAULT );
 	tSettings.m_bJiebaHMM = Bool ( tNode.ChildByName ( "jieba_hmm" ), true );
 }
 
@@ -3075,7 +3066,7 @@ void operator << ( JsonEscapedBuilder & tOut, const CSphIndexSettings & tSetting
 	tOut.NamedStringNonEmpty ( "hitless_files", tSettings.m_sHitlessFiles );
 	tOut.NamedValNonDefault ( "engine", (DWORD)tSettings.m_eEngine, (DWORD)AttrEngine_e::DEFAULT );
 	tOut.NamedValNonDefault ( "engine_default", (DWORD)tSettings.m_eDefaultEngine, (DWORD)AttrEngine_e::ROWWISE );
-	tOut.NamedValNonDefault ( "jieba_mode", (DWORD)tSettings.m_eJiebaMode, (DWORD)JiebaMode_e::ACCURATE );
+	tOut.NamedValNonDefault ( "jieba_mode", (DWORD)tSettings.m_eJiebaMode, (DWORD)JiebaMode_e::DEFAULT );
 	tOut.NamedValNonDefault ( "jieba_hmm", tSettings.m_bJiebaHMM, true );
 }
 

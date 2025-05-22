2b270fbd17ece3cadfa14a5d8ab0d852e218dfa1
Pavel_Shilin <pavel@manticoresearch.com>
2025-05-22 09:31:44 +0200

    Created test-integration-kafka-ms-copy.rec

--- test/clt-tests/integrations/kafka/import.sh
+++ test/clt-tests/integrations/kafka/import.sh
@@ -5,4 +5,8 @@ kafka_topic='my-data'
 json_file_path='/tmp/dump.json'
 
 
-/opt/bitnami/kafka/bin/kafka-console-producer.sh --broker-list "$bootstrap_servers" --topic "$kafka_topic" < "$json_file_path"
+/opt/bitnami/kafka/bin/kafka-console-producer.sh \
+  --broker-list "$bootstrap_servers" \
+  --property parse.key=true \
+  --property "key.separator=:" \
+  --topic "$kafka_topic" < "$json_file_path"

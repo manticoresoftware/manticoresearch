9074567f3fa25011621f245ccf8c4dbe33f798ec
Pavel_Shilin <pavel@manticoresearch.com>
2025-06-11 12:31:31 +0200

    Updated test-integration-kafka-partition-based-ingestion.rec

--- test/clt-tests/integrations/kafka/import.sh
+++ test/clt-tests/integrations/kafka/import.sh
@@ -5,8 +5,4 @@ kafka_topic='my-data'
 json_file_path='/tmp/dump.json'
 
 
-/opt/bitnami/kafka/bin/kafka-console-producer.sh \
-  --broker-list "$bootstrap_servers" \
-  --property parse.key=true \
-  --property "key.separator=:" \
-  --topic "$kafka_topic" < "$json_file_path"
+/opt/bitnami/kafka/bin/kafka-console-producer.sh --broker-list "$bootstrap_servers" --topic "$kafka_topic" < "$json_file_path"

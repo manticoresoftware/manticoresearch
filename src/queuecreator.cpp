690f29d4a1450b436b03a0752d6a1171ffd80978
Sergey Nikolaev <prostuda@academ.org>
2025-11-06 09:03:19 +0700

    fix: add validation for aggregate functions on text columns
    Related issue https://github.com/manticoresoftware/manticoresearch/issues/3844
    Related PR https://github.com/manticoresoftware/manticoresearch/pull/3883

--- src/queuecreator.cpp
+++ src/queuecreator.cpp
@@ -808,6 +808,20 @@ void QueueCreator_c::PropagateEvalStage ( CSphColumnInfo & tExprCol, StrVec_t & 
 
 bool QueueCreator_c::SetupAggregateExpr ( CSphColumnInfo & tExprCol, const CSphString & sExpr, DWORD uQueryPackedFactorFlags )
 {
+	// validate that MAX/MIN/SUM/AVG cannot be used on string/text columns
+	// This check must happen BEFORE the switch statement that may modify tExprCol.m_eAttrType
+	if ( tExprCol.m_eAggrFunc==SPH_AGGR_MAX || tExprCol.m_eAggrFunc==SPH_AGGR_MIN 
+		|| tExprCol.m_eAggrFunc==SPH_AGGR_SUM || tExprCol.m_eAggrFunc==SPH_AGGR_AVG )
+	{
+		if ( tExprCol.m_eAttrType==SPH_ATTR_STRING || tExprCol.m_eAttrType==SPH_ATTR_STRINGPTR )
+		{
+			const char * sFunc = ( tExprCol.m_eAggrFunc==SPH_AGGR_MAX ) ? "MAX" 
+				: ( tExprCol.m_eAggrFunc==SPH_AGGR_MIN ) ? "MIN"
+				: ( tExprCol.m_eAggrFunc==SPH_AGGR_SUM ) ? "SUM" : "AVG";
+			return Err ( "%s() cannot be used on text/string column '%s'", sFunc, sExpr.cstr() );
+		}
+	}
+
 	switch ( tExprCol.m_eAggrFunc )
 	{
 	case SPH_AGGR_AVG:

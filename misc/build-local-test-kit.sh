4fb748d8dd7879deb086708ec3ae42bfa5ee8601
Sergey Nikolaev <prostuda@academ.org>
2025-12-10 14:26:41 +0700

    chore: enhance local test kit script with error handling and additional file copies
    - Added error handling for Docker build failure.
    - Included cleanup of cache directory when using the --clean parameter.
    - Copied new shared libraries from the columnar build to the output directory.
    - Updated Dockerfile generation to copy additional binaries and libraries.

--- misc/build-local-test-kit.sh
+++ misc/build-local-test-kit.sh
@@ -1,10 +1,11 @@
 # Handle --clean parameter
 if [ "$1" = "--clean" ]; then
 	rm -rf ../build
+        rm -fr ../cache/linux-x86_64/mcl/*
 fi
 
 mkdir -p ../build
-docker run -it --rm \
+if ! docker run --platform linux/amd64 -it --rm \
 -v $(pwd)/../cache:/cache \
 -e CACHEB="/cache" \
 -e DIAGNOSTIC=1 \
@@ -21,18 +22,28 @@ docker run -it --rm \
 -e HOMEBREW_PREFIX="" \
 -e PACK_GALERA=0 \
 -e UNITY_BUILD=1 \
+-v $(pwd)/../../columnar/:/columnar \
 -v $(pwd)/../:/manticore_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa \
-manticoresearch/external_toolchain:vcpkg331_20250114 bash -c 'cd /manticore_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/build && cmake -DDISTR=jammy .. -DCMAKE_BUILD_TYPE=Debug && export CMAKE_TOOLCHAIN_FILE=$(pwd)/dist/build_dockers/cross/linux.cmake && cmake --build .'
+manticoresearch/external_toolchain:vcpkg331_20250114 bash -c 'echo "SOURCE_DIR /columnar" > /manticore_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/local_columnar_src.txt && cd /manticore_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/build && cmake -DDISTR=jammy .. -DCMAKE_BUILD_TYPE=Debug && export CMAKE_TOOLCHAIN_FILE=$(pwd)/dist/build_dockers/cross/linux.cmake && cmake --build .'; then
+	echo "Error: Docker build failed. Aborting."
+	exit 1
+fi
 
 # Build Docker image with compiled binaries
+cp ../../columnar/build/knn/lib_manticore_knn.so ../build/lib_manticore_knn.so
+cp ../../columnar/build/knn/embeddings/release/lib_manticore_knn_embeddings.so ../build/lib_manticore_knn_embeddings.so
+
 if [ -f ../build/src/searchd ] && [ -f ../build/src/indexer ] && [ -f ../build/src/indextool ]; then
 	cat > ../build/Dockerfile <<EOF
 FROM ghcr.io/manticoresoftware/manticoresearch:test-kit-latest
-COPY src/searchd /usr/bin/searchd
-COPY src/indexer /usr/bin/indexer
-COPY src/indextool /usr/bin/indextool
+COPY . /manticore/
+COPY build/src/searchd /usr/bin/searchd
+COPY build/src/indexer /usr/bin/indexer
+COPY build/src/indextool /usr/bin/indextool
+COPY build/lib_manticore_knn.so /usr/share/manticore/modules/lib_manticore_knn.so
+COPY build/lib_manticore_knn_embeddings.so /usr/share/manticore/modules/lib_manticore_knn_embeddings.so
 EOF
-	docker build --load -t test-kit ../build
+	docker build --load -t test-kit -f ../build/Dockerfile ..
 	rm -f ../build/Dockerfile
 	echo "Docker image 'test-kit' created successfully"
 else

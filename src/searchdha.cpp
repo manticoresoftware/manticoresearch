a134a9f05d182dcdab6d040caa4994fdcc759ccc
Aleksey N. Vinogradov <alexey@manticoresearch.com>
2025-12-19 12:44:56 +0700

    narrow case of remove socket from epoll
    Ensure, only epoll flow, and only for closing socket is immediately
    removed from epoll.

--- src/searchdha.cpp
+++ src/searchdha.cpp
@@ -3713,10 +3713,12 @@ public:
 		} else
 			sphLogDebugv ( "- %d Change task (task %p), fd=%d (%d) " INT64_FMT "Us -> " INT64_FMT "Us", pConnection->m_iStoreTag, pTask, pTask->m_ifd, pTask->m_iStoredfd, pTask->m_iTimeoutTimeUS, iTimeoutUS );
 
-		if ( !pConnection->InNetLoop () )
-			AddToQueue ( pTask, false );
-		else
+		const bool bRemoveClosingFromEpoll = POLLING_EPOLL && (iTimeoutUS<0) && pConnection->InNetLoop ();
+
+		if ( bRemoveClosingFromEpoll )
 			events_change_io (pTask);
+		else
+			AddToQueue ( pTask, pConnection->InNetLoop () );
 	}
 
 	void DisableWrite ( AgentConn_t * pConnection )

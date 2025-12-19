f55fc1d5cfcf2b94b52e96c727531c7d841a73d9
Stas <tomat1979@gmail.com>
2025-12-19 17:59:08 +0100

    switched to github source for libstemmer; fixed #2990

--- cmake/GetSTEMMER.cmake
+++ cmake/GetSTEMMER.cmake
@@ -15,9 +15,9 @@
 # If no file found, it will try to fetch it from
 # https://snowballstem.org/dist/libstemmer_c.tgz
 
-set ( STEMMER_REMOTE "https://snowballstem.org/dist/libstemmer_c.tgz" )
+set ( STEMMER_REMOTE "https://github.com/tomatolog/snowball/archive/refs/tags/v3.0.2.tar.gz" )
 set ( STEMMER_BUNDLEZIP "${LIBS_BUNDLE}/libstemmer_c.tgz" )
-set ( STEMMER_SRC_MD5 "f8288a861db7c97dc4750020c7c7aa6f" )
+set ( STEMMER_SRC_MD5 "abe9b77f95ae3931ce688ba03158383b" )
 
 cmake_minimum_required ( VERSION 3.17 FATAL_ERROR )
 include ( update_bundle )

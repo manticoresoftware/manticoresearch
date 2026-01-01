2d26045d544dda068bcc06de79f9796f5a15236a
Aleksey N. Vinogradov <alexey@manticoresearch.com>
2026-01-01 17:37:01 +0700

    chore: copyright 2026

--- api/sphinxapi.php
+++ api/sphinxapi.php
@@ -7,7 +7,7 @@
 //
 // Copyright (c) 2001-2016, Andrew Aksyonoff
 // Copyright (c) 2008-2016, Sphinx Technologies Inc
-// Copyright (c) 2019-2025, Manticore Software LTD (https://manticoresearch.com)
+// Copyright (c) 2019-2026, Manticore Software LTD (https://manticoresearch.com)
 // All rights reserved
 //
 // This program is free software; you can redistribute it and/or modify

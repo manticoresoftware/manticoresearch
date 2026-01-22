3cb4b1d8c028540c2a16341886e118e7e661972e
nick <nick@manticoresearch.com>
2026-01-22 13:45:00 +0700

    refactor: optimize MAD calculation in TDigest by directly utilizing deviation tracking
    This commit further refines the Median Absolute Deviation (MAD) calculation in the TDigest implementation by removing unnecessary checks and directly leveraging the TDigest class for deviation tracking. The changes enhance the clarity and efficiency of the computation process while ensuring accurate MAD results.

--- src/sphinxjsonquery.cpp
+++ src/sphinxjsonquery.cpp
@@ -25,7 +25,6 @@
 
 #include <cstring>
 #include <cmath>
-#include <limits>
 
 static const char * g_szAll = "_all";
 static const char * g_szHighlight = "_@highlight_";
@@ -2430,48 +2429,14 @@ static bool CalcMadFromDigest ( const TDigest_c & tDigest, double & fMad )
 	CSphVector<TDigestCentroid_t> dCentroids;
 	tDigest.Export ( dCentroids );
 
-	if ( dCentroids.IsEmpty() )
-		return false;
-
-	int64_t iTotalWeight = 0;
+	TDigest_c tDeviation ( tDigest.GetCompression() );
 	for ( const auto & tCentroid : dCentroids )
-		iTotalWeight += tCentroid.m_iCount;
-
-	const double fTarget = 0.5 * (double)iTotalWeight;
-	double fAccumulated = 0.0;
-
-	int iRight = 0;
-	while ( iRight<dCentroids.GetLength() && dCentroids[iRight].m_fMean<fMedian )
-		++iRight;
-	int iLeft = iRight-1;
-
-	while ( iLeft>=0 || iRight<dCentroids.GetLength() )
 	{
-		double fLeftDev = ( iLeft>=0 ) ? ( fMedian - dCentroids[iLeft].m_fMean ) : std::numeric_limits<double>::infinity();
-		double fRightDev = ( iRight<dCentroids.GetLength() ) ? ( dCentroids[iRight].m_fMean - fMedian ) : std::numeric_limits<double>::infinity();
-
-		if ( fLeftDev<=fRightDev )
-		{
-			fAccumulated += (double)dCentroids[iLeft].m_iCount;
-			if ( fAccumulated>=fTarget )
-			{
-				fMad = fLeftDev;
-				return true;
-			}
-			--iLeft;
-		} else
-		{
-			fAccumulated += (double)dCentroids[iRight].m_iCount;
-			if ( fAccumulated>=fTarget )
-			{
-				fMad = fRightDev;
-				return true;
-			}
-			++iRight;
-		}
+		double fDeviation = std::fabs ( tCentroid.m_fMean - fMedian );
+		tDeviation.Add ( fDeviation, tCentroid.m_iCount );
 	}
 
-	fMad = 0.0;
+	fMad = tDeviation.Quantile ( 0.5 );
 	return true;
 }
 

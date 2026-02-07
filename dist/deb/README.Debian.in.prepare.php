6ed2b621322406b2f68acc2f086fd96f593b91be
Sergey Nikolaev <prostuda@academ.org>
2026-02-07 12:37:15 +0700

    docs: minor update in README.Debian.in.prepare.php and man pages

--- dist/deb/README.Debian.in.prepare.php
+++ dist/deb/README.Debian.in.prepare.php
@@ -11,4 +11,8 @@ $text = preg_replace('/\[(.*?)\]\(.*?\)/', '$1', $text);
 // Replace the title
 $text = str_replace("# Introduction", "# Manticore", $text);
 
+$text .= "## System Limits
+Manticore Search requires increased memory mapping limits for large datasets. The installation sets vm.max_map_count=262144 in /etc/sysctl.d/70-manticore.conf. Verify with 'sysctl vm.max_map_count'.
+";
+
 file_put_contents('README.Debian.in', $text);

eaecd6fae4b0ac34034e7bcde1ff049c48b95b80
Aleksey N. Vinogradov <alexey@manticoresearch.com>
2026-04-21 13:22:23 +0700

    benchmarks for fnv64 flavours

--- src/std/fnv64.cpp
+++ src/std/fnv64.cpp
@@ -19,6 +19,21 @@ uint64_t sphFNV64 ( const void * s )
 	return sphFNV64cont ( s, SPH_FNV64_SEED );
 }
 
+uint64_t sphFNV64_old_for_bench ( const void * s, int iLen, uint64_t uPrev )
+{
+	const BYTE * p = (const BYTE*)s;
+	uint64_t hval = uPrev;
+	for ( ; iLen>0; iLen-- )
+	{
+		// xor the bottom with the current octet
+		hval ^= (uint64_t)*p++;
+
+		// multiply by the 64 bit FNV magic prime mod 2^64
+		hval += (hval << 1) + (hval << 4) + (hval << 5) + (hval << 7) + (hval << 8) + (hval << 40); // gcc optimization
+	}
+	return hval;
+}
+
 
 uint64_t sphFNV64 ( const void * s, int iLen, uint64_t uPrev )
 {

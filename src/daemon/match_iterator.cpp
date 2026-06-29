cb4821c8c680edfe40c8054b8222cb6ead622023
Sergey Nikolaev <prostuda@academ.org>
2026-06-29 11:41:35 +0700

    fix: merge UUID agent results by internal docid
    Use the schema @id locator when MatchIterator sorts and dedupes merged result sets. UUID agent replies append the internal numeric @id dynamically, so reading row offset 0 can collapse distinct remote UUID rows during distributed HTTP/JSON result merging.
    
    Test: php test/ubertest.php t --no-demo --strict-verbose --cwd -b /Users/sn/manticore_uuid/build/src/ -t /tmp/mcore-test517.JX5ycr/ -tt /tmp/mcore-test517.JX5ycr/ 517

--- src/daemon/match_iterator.cpp
+++ src/daemon/match_iterator.cpp
@@ -18,8 +18,14 @@
 class MatchTagSortAccessor_c
 {
 	const VecTraits_T<CSphMatch> & m_dTagOrder;
+	CSphAttrLocator m_tDocIDLocator;
+	bool m_bUseDocIDLocator = false;
 public:
-	explicit MatchTagSortAccessor_c ( const VecTraits_T<CSphMatch> & dTagOrder) : m_dTagOrder ( dTagOrder ) {}
+	MatchTagSortAccessor_c ( const VecTraits_T<CSphMatch> & dTagOrder, const CSphAttrLocator & tDocIDLocator, bool bUseDocIDLocator )
+		: m_dTagOrder ( dTagOrder )
+		, m_tDocIDLocator ( tDocIDLocator )
+		, m_bUseDocIDLocator ( bUseDocIDLocator )
+	{}
 	using T = CSphMatch;
 	using MEDIAN_TYPE = int;
 	static MEDIAN_TYPE Key ( T * a ) { return a->m_iTag; }
@@ -27,12 +33,19 @@ public:
 	static T * Add ( T * p, int i ) noexcept { return p+i; }
 	static int Sub ( T * b, T * a ) noexcept { return (int)(b-a); }
 	static void CopyKey ( MEDIAN_TYPE * pMed, CSphMatch * pVal ) noexcept { *pMed = Key ( pVal ); }
+	DocID_t GetDocID ( const CSphMatch & tMatch ) const noexcept { return m_bUseDocIDLocator ? (DocID_t)tMatch.GetAttr ( m_tDocIDLocator ) : sphGetDocID ( tMatch.m_pDynamic ); }
 
 	bool IsLess ( int a, int b ) const noexcept
 	{
-		return sphGetDocID ( m_dTagOrder[a].m_pDynamic )<sphGetDocID ( m_dTagOrder[b].m_pDynamic );
+		return GetDocID ( m_dTagOrder[a] )<GetDocID ( m_dTagOrder[b] );
 	}
 };
+
+
+DocID_t MatchIterator_c::GetDocID ( const CSphMatch & tMatch ) const noexcept
+{
+	return m_bUseDocIDLocator ? (DocID_t)tMatch.GetAttr ( m_tDocIDLocator ) : sphGetDocID ( tMatch.m_pDynamic );
+}
 
 // use space after end of matches to store indexes, WORD per match
 bool MatchIterator_c::MaybeUseWordOrder ( const CSphSwapVector<CSphMatch>& dMatches ) const noexcept
@@ -48,8 +61,8 @@ bool MatchIterator_c::MaybeUseWordOrder ( const CSphSwapVector<CSphMatch>& dMatc
 	VecTraits_T dOrder = { (WORD *) dMatches.end (), m_iLimit };
 	ARRAY_CONSTFOREACH( i, dOrder )
 		dOrder[i] = i;
-	dOrder.Sort ( Lesser ( [&dMatches] ( WORD a, WORD b ) {
-		return sphGetDocID ( dMatches[a].m_pDynamic )<sphGetDocID ( dMatches[b].m_pDynamic );
+	dOrder.Sort ( Lesser ( [this, &dMatches] ( WORD a, WORD b ) {
+		return GetDocID ( dMatches[a] )<GetDocID ( dMatches[b] );
 	} ) );
 	return true;
 }
@@ -68,8 +81,8 @@ bool MatchIterator_c::MaybeUseDwordOrder ( const CSphSwapVector<CSphMatch>& dMat
 	VecTraits_T dOrder = { (DWORD *) dMatches.end (), m_iLimit };
 	for( DWORD i=0, uLen=dOrder.GetLength(); i<uLen; ++i )
 		dOrder[i] = i;
-	dOrder.Sort ( Lesser ( [&dMatches] ( DWORD a, DWORD b ) {
-		return sphGetDocID ( dMatches[a].m_pDynamic )<sphGetDocID ( dMatches[b].m_pDynamic );
+	dOrder.Sort ( Lesser ( [this, &dMatches] ( DWORD a, DWORD b ) {
+		return GetDocID ( dMatches[a] )<GetDocID ( dMatches[b] );
 	} ) );
 	return true;
 }
@@ -80,7 +93,7 @@ void MatchIterator_c::UseTags ( VecTraits_T<CSphMatch> & dOrder ) noexcept
 	ARRAY_CONSTFOREACH( i, dOrder )
 		dOrder[i].m_iTag = i;
 
-	MatchTagSortAccessor_c tOrder ( dOrder );
+	MatchTagSortAccessor_c tOrder ( dOrder, m_tDocIDLocator, m_bUseDocIDLocator );
 	sphSort ( dOrder.Begin (), dOrder.GetLength (), tOrder, tOrder );
 	m_bTailClean = true;
 }
@@ -91,7 +104,13 @@ MatchIterator_c::MatchIterator_c ( OneResultset_t & tResult )
 	auto& dMatches = tResult.m_dMatches;
 	m_iLimit = dMatches.GetLength();
 
-	assert ( m_tResult.m_tSchema.GetAttr ( sphGetDocidName() ) || m_tResult.m_tSchema.GetAttr ( sphGetUuidDocidName() ) );
+	const CSphColumnInfo * pDocID = m_tResult.m_tSchema.GetAttr ( sphGetDocidName() );
+	assert ( pDocID || m_tResult.m_tSchema.GetAttr ( sphGetUuidDocidName() ) );
+	if ( pDocID )
+	{
+		m_tDocIDLocator = pDocID->m_tLocator;
+		m_bUseDocIDLocator = true;
+	}
 
 	if ( MaybeUseWordOrder ( dMatches ) )
 		m_fnOrder = [pData = (WORD *) m_tResult.m_dMatches.end ()] ( int i ) { return pData[i]; };
@@ -106,7 +125,7 @@ MatchIterator_c::MatchIterator_c ( OneResultset_t & tResult )
 	m_iRawIdx = 0;
 	m_iIdx = m_fnOrder(0);
 
-	m_tDocID = sphGetDocID ( m_tResult.m_dMatches[m_iIdx].m_pDynamic );
+	m_tDocID = GetDocID ( m_tResult.m_dMatches[m_iIdx] );
 }
 
 MatchIterator_c::~MatchIterator_c ()
@@ -128,6 +147,6 @@ bool MatchIterator_c::Step () noexcept
 	if ( m_iRawIdx>=m_iLimit )
 		return false;
 	m_iIdx = m_fnOrder ( m_iRawIdx );
-	m_tDocID = sphGetDocID ( m_tResult.m_dMatches[m_iIdx].m_pDynamic );
+	m_tDocID = GetDocID ( m_tResult.m_dMatches[m_iIdx] );
 	return true;
 }

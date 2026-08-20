6a7e2b86e7c3f06a34c084768d1cac21e03030b9
Stas <stas@manticoresearch.com>
2026-08-20 12:31:53 +0200

    fixed files banner (#4832)

--- src/api_reply_stream.h
+++ src/api_reply_stream.h
@@ -1,5 +1,6 @@
 //
-// Copyright (c) 2017-2026, Manticore Software LTD (https://manticoresearch.com)
+// Copyright (c) 2026, Manticore Software LTD (https://manticoresearch.com)
+// All rights reserved
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License. You should have
